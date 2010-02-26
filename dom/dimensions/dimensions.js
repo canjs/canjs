2280d79a081148a0143a4f3da563314135c26fbb
Pinhook <contact@pinhooklabs.com>
2010-02-26 00:24:59 -0600

    dimensions allows set and get outer width

---
+++ dom/dimensions/dimensions.js
@@ -0,0 +1,34 @@
+steal.plugins('jquery').then(function($){
+	var oldHeight = $.fn.outerHeight,
+		oldWidth = $.fn.outerWidth;
+	$.fn.outerHeight = function(v){
+		if(typeof v == 'number'){
+			//subtract border and padding
+			var val = 0, el = this[0]
+			$.each(['Top','Bottom'], function(){
+				val -= parseFloat(jQuery.curCSS( el, "padding" + this, true)) || 0;
+				val -= parseFloat(jQuery.curCSS( el, "border" + this + "Width", true)) || 0;
+			})
+			
+			this.height(v+val)
+		}else{
+			return oldHeight.call(this, v)
+		}
+	}
+	
+	
+	$.fn.oldWidth = function(v){
+		if(typeof v == 'number'){
+			//subtract border and padding
+			var val = 0, el = this[0]
+			$.each(['Left','Right'], function(){
+				val -= parseFloat(jQuery.curCSS( el, "padding" + this, true)) || 0;
+				val -= parseFloat(jQuery.curCSS( el, "border" + this + "Width", true)) || 0;
+			})
+			
+			this.width(v+val)
+		}else{
+			return oldWidth.call(this, v)
+		}
+	}
+})
