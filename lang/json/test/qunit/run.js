37720dbb90653ea7e41cd2d0ca720885c11242fa
Pinhook <contact@pinhooklabs.com>
2010-03-08 16:58:03 -0600

    changed to env.js

--- lang/json/test/qunit/run.js
+++ lang/json/test/qunit/run.js
@@ -1,2 +1,2 @@
-load('steal/rhino/env.rhino.js');
+load('steal/rhino/env.js');
 Envjs('json/test/qunit/qunit.html', {scriptTypes : {"text/javascript" : true,"text/envjs" : true}, fireLoad: true, logLevel: 2});
