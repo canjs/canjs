0abc7c8a76aded5fdf6b654d27355f1cf93064c4
Pinhook <contact@pinhooklabs.com>
2010-03-26 14:35:49 -0500

    fixed resize

--- event/resize/resize.js
+++ event/resize/resize.js
@@ -1,17 +1,19 @@
 steal.apps('jquery').then(function($){
+	var resizeCount = 0, windowWidth, windowHeight;
+	
 	$.event.special["resize"] = {
 		add: function(handleObj){
 			//jQuery.event.add( this, handleObj.origType, jQuery.extend({}, handleObj, {handler: liveHandler}) );
 			
 			var origHandler = handleObj.handler;
 			handleObj.origHandler = origHandler;
-			var resizeCount = 0, windowWidth, windowHeight;
+			
 			handleObj.handler = function(ev, data){
 			    if(resizeCount === 0){
 			        windowWidth = $(window).width();
 				    windowHeight = $(window).height();
 			    }								
-			    if(this !== window || resizeCount === 0){
+			    if((this !== window) || resizeCount === 0){
 				     resizeCount++;
 			         handleObj.origHandler.call(this, ev, data);
 					 resizeCount--;
@@ -24,7 +26,9 @@ steal.apps('jquery').then(function($){
 			}
 		},
 		
-		setup: function(){return true;}
+		setup: function(){
+			return this !== window;
+		}
 	}
 })
 
