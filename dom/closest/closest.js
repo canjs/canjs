1b131f7ededa53bf2e3b8709ad0a62aa1cc01607
Brian Moschel <brian.moschel@gmail.com>
2010-04-12 10:20:36 -0500

    fixing closest to work with strings

--- dom/closest/closest.js
+++ dom/closest/closest.js
@@ -3,14 +3,16 @@ steal.then(function(){
 	var oldClosest = jQuery.fn.closest;
 	jQuery.fn.closest = function(selectors, context){
 		var rooted = {}, res, result, thing, i, j, selector, matchEl, 
-			rootedIsEmpty = true, selector, nbrParents;
-		if(typeof selectors == "string") selectors = [selectors];
-		$.each(selectors, function(i, selector){
+			rootedIsEmpty = true, selector, nbrParents, selectorsArr = selectors;
+		if(typeof selectors == "string") selectorsArr = [selectors];
+		
+		$.each(selectorsArr, function(i, selector){
 		    if(selector.indexOf(">") == 0 ){
 				if(selector.indexOf(" ") != -1){
 					throw " closest does not work with > followed by spaces!"
 				}
-				rooted[( selectors[i] = selector.substr(1)  )] = selector;
+				rooted[( selectorsArr[i] = selector.substr(1)  )] = selector;
+				if(typeof selectors == "string") selectors = selector.substr(1);
 				rootedIsEmpty = false;
 			}
 		})
