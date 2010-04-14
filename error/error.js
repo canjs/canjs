d5167ef9386ec4ba3fe6879f5dadf24700b500a8
Brian Moschel <brian.moschel@gmail.com>
2010-04-14 02:38:52 -0500

    cleaning up error

--- error/error.js
+++ error/error.js
@@ -1,5 +1,8 @@
 steal.plugins('jquery/class', 'jquery').then(function(){
 
+	var seconds_remaining;
+	var timer;
+			
 	$.ApplicationError = {
 		textarea_text: "type description here",
 		textarea_title: "Damn It!",
@@ -23,56 +26,20 @@ steal.plugins('jquery/class', 'jquery').then(function(){
 		config: function(params) {
 			$.extend($.ApplicationError, params);
 		},
-		create_containing_div: function(){
-			var div = document.createElement('div');
-			div.id = '_application_error';
-			div.style.position = $.browser.mozilla ? 'fixed' : 'absolute';
-			div.style.bottom = '0px';
-			div.style.left = '0px';
-			div.style.margin = '0px';
-			return div;
-		},
-		create_title: function(){
-			var title = document.createElement('div');
-			title.style.backgroundImage = 'url('+steal.root.path+'/jquery/error/background.png)';
-			title.style.backgroundAttachment = 'scroll';
-			title.style.backgroundRepeat = 'repeat-x';
-			title.style.backgroundPosition = 'center top';
-			title.style.font = 'bold 10pt verdana';
-			title.style.color ='white';
-			title.style.padding='0px 5px 0px 10px';
-			title.innerHTML+= "<a style='float:right; width: 50px;text-decoration:underline; color: Red; "+
-								"padding-left: 25px; font-size:8pt; cursor: pointer' onclick='$.ApplicationError.send()'>Close</a> "+
-								"<span id='_error_seconds' style='float:right; font-size:10pt;'></span>"+this.textarea_title;
-			return title;
-		},
-		create_form: function(callback){
-			var form = document.createElement('form');
-			var leftmargin = $.browser.msie ? 5 : 10;
-			form.id = '_error_form';
-			form.onsubmit = callback;
-			form.innerHTML ="<div style='float: left; width: 300px;margin-left:"+leftmargin+"px;'>"+this.prompt_text+"</div>"+
-			    "<input type='submit' value='Send' style='font-size: 10pt; float:right; margin: 17px 5px 0px 0px; width:60px;padding:5px;'/>"+
-				"<textarea style='width: 335px; color: gray;' rows='"+($.browser.mozilla ? 2 : 3)+"' name='description' id='_error_text' "+
-				"onfocus='$.ApplicationError.text_area_focus();' "+
-				"onblur='$.ApplicationError.text_area_blur();' >"+this.textarea_text+"</textarea>";
-			form.style.padding = '0px';
-			form.style.font = 'normal 8pt verdana';
-			form.style.margin = '0px';
-			form.style.backgroundColor = '#FAE8CD';
-			return form;
-		},
 		create_send_function: function(error){
-			$.ApplicationError.send = function(event){
-				var params = {error: {}}, description;
+			$.ApplicationError.send = function(ev){
+				var params = {error: {}}, 
+					description = $('#_error_text');
 				params.error.subject = error.subject;
-				if((description = $('#_error_text')[0])){error['User Description'] = description.value;}
+				if(description.length)
+					error['User Description'] = description.value;
 				if($.ApplicationError.prompt_user) {
 					$.ApplicationError.pause_count_down();
 					document.body.removeChild($('#_application_error')[0]);
 				}
 				params.error.content = $.ApplicationError.generate_content(error);
-				$.ApplicationError.kill_event(event);
+				ev.stopPropagation();
+				ev.preventDefault();
 				params.user_crypted_key = APPLICATION_KEY;
 				params.referer = location.href;
 				params._method = "POST";
@@ -85,42 +52,95 @@ steal.plugins('jquery/class', 'jquery').then(function(){
 		},
 		create_dom: function(error){
 			if($('#_application_error').length) return; 
-			var div = $.ApplicationError.create_containing_div();
-			document.body.appendChild(div);
-			div.appendChild($.ApplicationError.create_title());
-			div.appendChild($.ApplicationError.create_form($.ApplicationError.send));
-			this.set_width();
-			
-			var seconds_remaining;
-			var timer;
-			
-			$.ApplicationError.count_down = function(){
-				seconds_remaining --;
-				$('#_error_seconds')[0].innerHTML = 'This will close in '+seconds_remaining+' seconds.';
-				if(seconds_remaining == 0){
-					$.ApplicationError.pause_count_down();
-					$.ApplicationError.send();
-				}
-			};
-			$.ApplicationError.start_count_down = function(){
-				seconds_remaining = this.close_time;
-				$('#_error_seconds')[0].innerHTML = 'This will close in '+seconds_remaining+' seconds.';
-				timer = setInterval($.ApplicationError.count_down, 1000);
-			};
-			$.ApplicationError.pause_count_down = function(){
-				clearInterval(timer);
-				timer = null;
+			var div = $('<div />')
+				.attr('id', '_application_error')
+				.css({
+					position: $.browser.mozilla ? 'fixed' : 'absolute',
+					bottom: '0px',
+					left: '0px', 
+					margin: '0px'
+				})
+			var title = $('<div />')
+				.css({
+					background: 'url('+steal.root.path+'/jquery/error/background.png) scroll repeat-x center top',
+					font: 'bold 10pt verdana',
+					color: 'white',
+					padding: '0px 5px 0px 10px'
+				})
 				
-				$('#_error_seconds')[0].innerHTML = '';
-			};
-			$.ApplicationError.start_count_down();
-		},
-		prompt_and_send: function(error){
-			this.create_send_function(error);
-			if($.ApplicationError.prompt_user == true)
-				this.create_dom(error);
-			else
-				this.send();
+			var a = $('<a />')
+				.css({
+					'float': 'right',
+					width: 50, 
+					'text-decoration': 'underline',
+					color: 'Red',
+					'padding-left': 25,
+					'font-size': '8pt',
+					cursor: 'pointer'
+				})
+				.click($.ApplicationError.send)
+				.html('Close')
+			var span = $('<span />')
+				.attr('id', '_error_seconds')
+				.css({
+					'float': 'right',
+					'font-size': '10pt'
+				})
+				.after(this.textarea_title);
+				
+			title.append(a).append(span).append('<span>'+this.textarea_title+'</span>');
+			
+			var form = $('<form />')
+				.attr('id', '_error_form')
+				.submit($.ApplicationError.send)
+				.css({
+					padding: 0,
+					margin: 0,
+					font: 'normal 8pt verdana',
+					backgroundColor: '#FAE8CD'
+				});
+			
+			var form_div = $('<div />')
+				.css({
+					'float': 'left',
+					width: 300,
+					'margin-left': $.browser.msie ? 5 : 10,
+				})
+				.html(this.prompt_text)
+			var input = $('<input />')
+				.attr({
+					type: 'submit',
+					value: 'Send'
+				})
+				.css({
+					'font-size': '10pt',
+					'float': 'right',
+					margin: '17px 5px 0px 0px',
+					width: 60,
+					padding: 5
+				})
+			var textarea = $('<textarea />')
+				.css({
+					width: 335,
+					color: 'gray'
+				})
+				.attr({
+					rows: $.browser.mozilla ? 2 : 3,
+					name: 'description',
+					id: '_error_text'
+				})
+				.focus(function(){
+					var area = $('#_error_text');
+					if(area.val() == $.ApplicationError.textarea_text) area.val('');
+					area.css('color', 'black');
+					$.ApplicationError.pause_count_down();
+				})
+				.html(this.textarea_text)
+			form.append(form_div).append(input.after(textarea));
+			
+			div.append(title).append(form);
+			$(document.body).append(div);
+			this.set_width();
 		},
 		notify: function(e){
 			e = $.ApplicationError.transform_error(e);
@@ -131,23 +151,19 @@ steal.plugins('jquery/class', 'jquery').then(function(){
 			});
 			if(Error && new Error().stack) e.Stack = new Error().stack;
 			if(!e.subject) e.subject = 'ApplicationError on: '+window.location.href;
-			$.ApplicationError.prompt_and_send(e);
+			this.create_send_function(e);
+			if($.ApplicationError.prompt_user == true)
+				this.create_dom(e);
+			else
+				this.send();
 			return false;
 		},
-		text_area_focus: function(){
-			var area = document.getElementById('_error_text');
-			if(area.value == this.textarea_text) area.value = '';
-			area.style.color = 'black';
-			$.ApplicationError.pause_count_down();
-		},
-		text_area_blur: function(){
-		},
 		set_width: function(){
-			var cont, width;
-			if(!(cont = $('#_application_error')[0] )) return;
-			width = document.body.clientWidth;
-			cont.style.width = width+'px';
-			$('#_error_text')[0].style.width = (width-400)+'px';
+			var cont = $('#_application_error'), width;
+			if(!cont.length) return;
+			width = $(document.body).width();
+			cont.width(width);
+			$('#_error_text').width(width-400);
 		},
 		transform_error: function(error){
 			if(typeof error == 'string'){
@@ -165,23 +181,27 @@ steal.plugins('jquery/class', 'jquery').then(function(){
 			}
 			return error;
 		},
-		kill_event: function(event) {
-		    if(! event) return;
-		    event.cancelBubble = true;
-		    if (event.stopPropagation)  event.stopPropagation(); 
-		    if (event.preventDefault)  event.preventDefault();
+		count_down: function(){
+			seconds_remaining --;
+			$('#_error_seconds').html('This will close in '+seconds_remaining+' seconds.');
+			if(seconds_remaining == 0){
+				$.ApplicationError.pause_count_down();
+				$.ApplicationError.send();
+			}
+		},
+		start_count_down: function(){
+			seconds_remaining = this.close_time;
+			$('#_error_seconds').html('This will close in '+seconds_remaining+' seconds.');
+			timer = setInterval($.ApplicationError.count_down, 1000);
+		},
+		pause_count_down: function(){
+			clearInterval(timer);
+			timer = null;
+			$('#_error_seconds').html('');
 		}
 	};
 	
-	$.error_handler = function(msg, url, l){
-		var e = {
-			message: msg,
-			fileName: url,
-			lineNumber: l
-		};
-		$.ApplicationError.notify(e);
-		return false;
-	};
+	$.ApplicationError.start_count_down();
 	
 	var oldCB = jQuery.Class.callback;
 	jQuery.Class.callback = function(f_names){
@@ -190,7 +210,6 @@ steal.plugins('jquery/class', 'jquery').then(function(){
 				try{
 					return oldCBFunc.apply(this, arguments);
 				}catch(e){
-					$.ApplicationError.kill_event(params.event);
 					e = $.ApplicationError.transform_error(e);
 					
 					$.extend(e,{
@@ -205,10 +224,14 @@ steal.plugins('jquery/class', 'jquery').then(function(){
 		return newCBFunc;
 	}
 	
-	if(window.attachEvent) {
-		window.attachEvent("onresize", $.ApplicationError.set_width);
-	}else{
-		window.addEventListener('resize', $.ApplicationError.set_width, false);
-	}
-	window.onerror = $.error_handler;
+	$(window).resize($.ApplicationError.set_width)
+	$(window).error(function(msg, url, l){
+		var e = {
+			message: msg,
+			fileName: url,
+			lineNumber: l
+		};
+		$.ApplicationError.notify(e);
+		return false;
+	})
 })
