8497d4110f033ca79565c3e684242d8b6fa2b8e5
Jeffery To <jeffery.to@gmail.com>
2010-04-28 17:04:07 +0800

    test cases for fixes

--- controller/test/qunit/controller_test.js
+++ controller/test/qunit/controller_test.js
@@ -51,6 +51,53 @@ test("subscribe testing works", function(){
 	//OpenAjax.hub.publish("a.b",{})
 	//equals(clicks,2, "Clicks stopped")
 	//equals(subscribes,2, "Subscribes stopped")
-	
-	
+})
+
+module("document and main controllers")
+test("events", function(){
+	var a = $("<div id='test'><span/></div>").appendTo($("#qunit-test-area")),
+		a_inner = a.find('span'),
+		b = $("<div><span/></div>").appendTo($("#qunit-test-area")),
+		b_inner = b.find('span'),
+		doc_outer_clicks = 0,
+		doc_inner_clicks = 0,
+		main_outer_clicks = 0,
+		main_inner_clicks = 0;
+
+	$.Controller.extend("TestController", { onDocument: true }, {
+		click : function(){
+			doc_outer_clicks++;
+		},
+		"span click" : function(){
+			doc_inner_clicks++;
+		}
+	})
+
+	a_inner.trigger("click");
+	equals(doc_outer_clicks,1,"document controller handled (no-selector) click inside listening element");
+	equals(doc_inner_clicks,1,"document controller handled (selector) click inside listening element");
+
+	b_inner.trigger("click");
+	equals(doc_outer_clicks,1,"document controller ignored (no-selector) click outside listening element");
+	equals(doc_inner_clicks,1,"document controller ignored (selector) click outside listening element");
+
+	$(document.documentElement).controller('test').destroy();
+
+	$.Controller.extend("MainController", { onDocument: true }, {
+		click : function(){
+			main_outer_clicks++;
+		},
+		"span click" : function(){
+			main_inner_clicks++;
+		}
+	})
+
+	b_inner.trigger("click");
+	equals(main_outer_clicks,1,"main controller handled (no-selector) click");
+	equals(main_inner_clicks,1,"main controller handled (selector) click");
+
+	$(document.documentElement).controller('main').destroy();
+
+	a.remove();
+	b.remove();
 })
