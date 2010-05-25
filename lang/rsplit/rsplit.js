0b0ad3ca973423ad0a7d7583c9dae8c23be716b9
Jeffery To <jeffery.to@gmail.com>
2010-05-25 17:33:49 +0800

    cleaned up $.String.rsplit() a bit

--- lang/rsplit/rsplit.js
+++ lang/rsplit/rsplit.js
@@ -1,13 +1,12 @@
 steal.plugins('jquery/lang').then(function($){
 	jQuery.Native.extend('String',{
 		rsplit : function(string, regex) {
-			var result = regex.exec(string),retArr = new Array(), first_idx, last_idx, first_bit;
+			var result = regex.exec(string),retArr = [], first_idx, last_idx;
 			while (result != null)
 			{
 				first_idx = result.index; last_idx = regex.lastIndex;
-				if ((first_idx) != 0)
+				if (first_idx != 0)
 				{
-					first_bit = string.substring(0,first_idx);
 					retArr.push(string.substring(0,first_idx));
 					string = string.slice(first_idx);
 				}		
@@ -15,7 +14,7 @@ steal.plugins('jquery/lang').then(function($){
 				string = string.slice(result[0].length);
 				result = regex.exec(string);	
 			}
-			if (! string == '')
+			if (string != '')
 			{
 				retArr.push(string);
 			}
