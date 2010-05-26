d1bbc37421a424a2d58ea3acc13ba972cfc1ccda
Brian Moschel <brian.moschel@gmail.com>
2010-05-26 00:19:06 -0500

    adding compressed files to dist

--- build.js
+++ build.js
@@ -1,5 +1,12 @@
+load('steal/file/file.js')
+
 var i, fileName, cmd, 
 	plugins = [
+	"class", 
+	"controller",
+	"event/default",
+	"event/destroyed",
+	"event/drag",
 	{plugin: "event/drag/limit", exclude: ["jquery/lang/vector/vector.js", "jquery/event/livehack/livehack.js", "jquery/event/drag/drag.js"]},
 	{plugin: "event/drag/scroll", exclude: ["jquery/dom/within/within.js", "jquery/dom/compare/compare.js", "jquery/event/drop/drop.js","jquery/lang/vector/vector.js", "jquery/event/livehack/livehack.js", "jquery/event/drag/drag.js"]},
 	"event/drop",
@@ -15,7 +22,7 @@ var i, fileName, cmd,
 ]
 
 
-var plugin, exclude;
+var plugin, exclude, fileDest;
 for(i=0; i<plugins.length; i++){
 	plugin = plugins[i];
 	exclude = [];
@@ -24,9 +31,19 @@ for(i=0; i<plugins.length; i++){
 		exclude = plugins[i].exclude;
 	}
 	fileName = "jquery."+plugin.replace(/\//g, ".")+".js";
-	print("***"+fileName)
-	cmd = "js steal/scripts/pluginify.js jquery/"+plugin+" -destination jquery/dist/"+fileName;
+	fileDest = "jquery/dist/"+fileName
+	cmd = "js steal/scripts/pluginify.js jquery/"+plugin+" -destination "+fileDest;
 	if(exclude.length)
 		cmd += " -exclude "+exclude;
 	runCommand(	"cmd", "/C", cmd)
+	
+	// compress 
+	var outBaos = new java.io.ByteArrayOutputStream();
+	var output = new java.io.PrintStream(outBaos);
+	runCommand("java", "-jar", "steal/rhino/compiler.jar", "--compilation_level",
+    	"SIMPLE_OPTIMIZATIONS", "--warning_level","QUIET",  "--js", fileDest, {output: output});
+	
+    var minFileDest = fileDest.replace(".js", ".min.js")
+	new steal.File(minFileDest).save(outBaos.toString());
+	print("***"+fileName+" pluginified and compressed")
 }
