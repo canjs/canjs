d10881e76fbc139dc95ff6f9e4dc8fd561646614
Jeffery To <jeffery.to@gmail.com>
2010-05-29 20:56:32 +0800

    updated json plugin to v2.2

--- lang/json/json.js
+++ lang/json/json.js
@@ -1,152 +1,132 @@
 /*
  * jQuery JSON Plugin
- * version: 1.0 (2008-04-17)
+ * version: 2.1 (2009-08-14)
  *
  * This document is licensed as free software under the terms of the
  * MIT License: http://www.opensource.org/licenses/mit-license.php
  *
- * Brantley Harris technically wrote this plugin, but it is based somewhat
- * on the JSON.org website's http://www.json.org/json2.js, which proclaims:
+ * Brantley Harris wrote this plugin. It is based somewhat on the JSON.org 
+ * website's http://www.json.org/json2.js, which proclaims:
  * "NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.", a sentiment that
- * I uphold.  I really just cleaned it up.
+ * I uphold.
  *
- * It is also based heavily on MochiKit's serializeJSON, which is 
- * copywrited 2005 by Bob Ippolito.
+ * It is also influenced heavily by MochiKit's serializeJSON, which is 
+ * copyrighted 2005 by Bob Ippolito.
  */
  
-(function($) {   
-    function toIntegersAtLease(n) 
-    // Format integers to have at least two digits.
-    {    
-        return n < 10 ? '0' + n : n;
-    }
+(function($) {
+    /** jQuery.toJSON( json-serializble )
+        Converts the given argument into a JSON respresentation.
 
-    Date.prototype.toJSON = function(date)
-    // Yes, it polutes the Date namespace, but we'll allow it here, as
-    // it's damned usefull.
+        If an object has a "toJSON" function, that will be used to get the representation.
+        Non-integer/string keys are skipped in the object, as are keys that point to a function.
+
+        json-serializble:
+            The *thing* to be converted.
+     **/
+    $.toJSON = function(o)
     {
-        return this.getUTCFullYear()   + '-' +
-             toIntegersAtLease(this.getUTCMonth()) + '-' +
-             toIntegersAtLease(this.getUTCDate());
-    };
-
-    var escapeable = /["\\\x00-\x1f\x7f-\x9f]/g;
-    var meta = {    // table of character substitutions
-            '\b': '\\b',
-            '\t': '\\t',
-            '\n': '\\n',
-            '\f': '\\f',
-            '\r': '\\r',
-            '"' : '\\"',
-            '\\': '\\\\'
-        };
+        if (typeof(JSON) == 'object' && JSON.stringify)
+            return JSON.stringify(o);
         
-    $.quoteString = function(string)
-    // Places quotes around a string, inteligently.
-    // If the string contains no control characters, no quote characters, and no
-    // backslash characters, then we can safely slap some quotes around it.
-    // Otherwise we must also replace the offending characters with safe escape
-    // sequences.
-    {
-        if (escapeable.test(string))
-        {
-            return '"' + string.replace(escapeable, function (a) 
-            {
-                var c = meta[a];
-                if (typeof c === 'string') {
-                    return c;
-                }
-                c = a.charCodeAt();
-                return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
-            }) + '"';
-        }
-        return '"' + string + '"';
-    };
-    
-    $.toJSON = function(o, compact, names, indent)
-    {
         var type = typeof(o);
-        
-        if (type == "undefined")
-            return "undefined";
-        else if (type == "number" || type == "boolean")
-            return o + "";
-        else if (o === null)
+    
+        if (o === null)
             return "null";
+    
+        if (type == "undefined")
+            return undefined;
         
-        // Is it a string?
-        if (type == "string") 
-        {
+        if (type == "number" || type == "boolean")
+            return o + "";
+    
+        if (type == "string")
             return $.quoteString(o);
-        }
-        
-        // Does it have a .toJSON function?
-        if (type == "object" && typeof o.toJSON == "function") 
-            return o.toJSON(compact);
-        
-        // Is it an array?
-        if($.isArray(o))
+    
+        if (type == 'object')
         {
-            var ret = [];
-            for (var i = 0; i < o.length; i++) {
-                ret.push( $.toJSON(o[i], compact, names) );
+            if (typeof o.toJSON == "function") 
+                return $.toJSON( o.toJSON() );
+            
+            if (o.constructor === Date)
+            {
+                var month = o.getUTCMonth() + 1;
+                if (month < 10) month = '0' + month;
+
+                var day = o.getUTCDate();
+                if (day < 10) day = '0' + day;
+
+                var year = o.getUTCFullYear();
+                
+                var hours = o.getUTCHours();
+                if (hours < 10) hours = '0' + hours;
+                
+                var minutes = o.getUTCMinutes();
+                if (minutes < 10) minutes = '0' + minutes;
+                
+                var seconds = o.getUTCSeconds();
+                if (seconds < 10) seconds = '0' + seconds;
+                
+                var milli = o.getUTCMilliseconds();
+                if (milli < 100) milli = '0' + milli;
+                if (milli < 10) milli = '0' + milli;
+
+                return '"' + year + '-' + month + '-' + day + 'T' +
+                             hours + ':' + minutes + ':' + seconds + 
+                             '.' + milli + 'Z"'; 
             }
-            if (compact)
+
+            if (o.constructor === Array) 
+            {
+                var ret = [];
+                for (var i = 0; i < o.length; i++)
+                    ret.push( $.toJSON(o[i]) || "null" );
+
                 return "[" + ret.join(",") + "]";
-            else
-                return "[" + ret.join(", ") + "]";
-        }
-        
-        // If it's a function, we have to warn somebody!
-        if (type == "function") {
-            throw new TypeError("Unable to convert object of type 'function' to json.");
-        }
-        
-        // It's probably an object, then.
-        var ret = [];
-        for (var k in o) {
-            if(names && ($.inArray(k, names) === -1) ){
-				continue;
-			}
-			
-			var name;
-            type = typeof(k);
-            
-            if (type == "number")
-                name = '"' + k + '"';
-            else if (type == "string")
-                name = $.quoteString(k);
-            else
-                continue;  //skip non-string or number keys
-            
-            var val = $.toJSON(o[k], compact, names);
-            if (typeof(val) != "string") {
-                // skip non-serializable values
-                continue;
             }
+        
+            var pairs = [];
+            for (var k in o) {
+                var name;
+                var type = typeof k;
+
+                if (type == "number")
+                    name = '"' + k + '"';
+                else if (type == "string")
+                    name = $.quoteString(k);
+                else
+                    continue;  //skip non-string or number keys
             
-            if (compact)
-                ret.push(name + ":" + val);
-            else
-                ret.push(name + ": " + val);
+                if (typeof o[k] == "function") 
+                    continue;  //skip pairs where the value is a function.
+            
+                var val = $.toJSON(o[k]);
+            
+                pairs.push(name + ":" + val);
+            }
+
+            return "{" + pairs.join(", ") + "}";
         }
-        return "{" + ret.join(", \n") + "}";
     };
-    
-    $.compactJSON = function(o)
-    {
-        return $.toJSON(o, true);
-    };
-    
+
+    /** jQuery.evalJSON(src)
+        Evaluates a given piece of json source.
+     **/
     $.evalJSON = function(src)
-    // Evals JSON that we know to be safe.
     {
+        if (typeof(JSON) == 'object' && JSON.parse)
+            return JSON.parse(src);
         return eval("(" + src + ")");
     };
     
+    /** jQuery.secureEvalJSON(src)
+        Evals JSON in a way that is *more* secure.
+    **/
     $.secureEvalJSON = function(src)
-    // Evals JSON in a way that is *more* secure.
     {
+        if (typeof(JSON) == 'object' && JSON.parse)
+            return JSON.parse(src);
+        
         var filtered = src;
         filtered = filtered.replace(/\\["\\\/bfnrtu]/g, '@');
         filtered = filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
@@ -156,5 +136,43 @@
             return eval("(" + src + ")");
         else
             throw new SyntaxError("Error parsing JSON, source is not valid.");
+    };
+
+    /** jQuery.quoteString(string)
+        Returns a string-repr of a string, escaping quotes intelligently.  
+        Mostly a support function for toJSON.
+    
+        Examples:
+            >>> jQuery.quoteString("apple")
+            "apple"
+        
+            >>> jQuery.quoteString('"Where are we going?", she asked.')
+            "\"Where are we going?\", she asked."
+     **/
+    $.quoteString = function(string)
+    {
+        if (string.match(_escapeable))
+        {
+            return '"' + string.replace(_escapeable, function (a) 
+            {
+                var c = _meta[a];
+                if (typeof c === 'string') return c;
+                c = a.charCodeAt();
+                return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+            }) + '"';
+        }
+        return '"' + string + '"';
+    };
+    
+    var _escapeable = /["\\\x00-\x1f\x7f-\x9f]/g;
+    
+    var _meta = {
+        '\b': '\\b',
+        '\t': '\\t',
+        '\n': '\\n',
+        '\f': '\\f',
+        '\r': '\\r',
+        '"' : '\\"',
+        '\\': '\\\\'
     };
 })(jQuery);
