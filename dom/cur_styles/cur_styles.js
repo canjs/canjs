23e00d59edeb2dfb397e21c98136375ca69694d2
Pinhook <contact@pinhooklabs.com>
2010-06-09 19:32:05 -0500

    cur styles update

--- dom/cur_styles/cur_styles.js
+++ dom/cur_styles/cur_styles.js
@@ -4,15 +4,15 @@ var getComputedStyle = document.defaultView && document.defaultView.getComputedS
 	rupper = /([A-Z])/g,
 	rdashAlpha = /-([a-z])/ig,
 	fcamelCase = function(all, letter) {
-	    return letter.toUpperCase();
+		return letter.toUpperCase();
 	},
 	getStyle = function(elem) {
-	    if (getComputedStyle) {
-	        return getComputedStyle(elem, null);
-	    }
-	    else if (elem.currentStyle) {
-	        return elem.currentStyle
-	    }
+		if (getComputedStyle) {
+			return getComputedStyle(elem, null);
+		}
+		else if (elem.currentStyle) {
+			return elem.currentStyle
+		}
 	},
 	rfloat = /float/i,
 	rnumpx = /^-?\d+(?:px)?$/i,
@@ -28,54 +28,57 @@ var getComputedStyle = document.defaultView && document.defaultView.getComputedS
  * @return {Object} an object of style:value pairs.  Style names are camelCase.
  */
 $.curStyles = function(el, styles) {
-    var currentS = getStyle(el), 
+	if(!el){
+		return null;
+	}
+	var currentS = getStyle(el), 
 				   oldName, 
 				   val, 
 				   style = el.style,
 				   results = {},
 				   i=0,
 				   name;
-    
+	
 	for(; i < styles.length; i++){
 		name = styles[i];
-        oldName = name.replace(rdashAlpha, fcamelCase);
+		oldName = name.replace(rdashAlpha, fcamelCase);
 		
 		if ( rfloat.test( name ) ) {
 			name = jQuery.support.cssFloat ? "float" : "styleFloat";
 			oldName = "cssFloat"
 		}
 		
-        if (getComputedStyle) {
-            name = name.replace(rupper, "-$1").toLowerCase();
-            val = currentS.getPropertyValue(name);
+		if (getComputedStyle) {
+			name = name.replace(rupper, "-$1").toLowerCase();
+			val = currentS.getPropertyValue(name);
 			if ( name === "opacity" && val === "" ) {
 				val = "1";
 			}
 			results[oldName] = val;
-        } else {
-            var camelCase = name.replace(rdashAlpha, fcamelCase);
-            results[oldName] = currentS[name] || currentS[camelCase];
+		} else {
+			var camelCase = name.replace(rdashAlpha, fcamelCase);
+			results[oldName] = currentS[name] || currentS[camelCase];
 
 
-            if (!rnumpx.test(results[oldName]) && rnum.test(results[oldName])) { //convert to px
-                // Remember the original values
-                var left = style.left, 
+			if (!rnumpx.test(results[oldName]) && rnum.test(results[oldName])) { //convert to px
+				// Remember the original values
+				var left = style.left, 
 					rsLeft = el.runtimeStyle.left;
 
-                // Put in the new values to get a computed value out
-                el.runtimeStyle.left = el.currentStyle.left;
-                style.left = camelCase === "fontSize" ? "1em" : (results[oldName] || 0);
-                results[oldName] = style.pixelLeft + "px";
+				// Put in the new values to get a computed value out
+				el.runtimeStyle.left = el.currentStyle.left;
+				style.left = camelCase === "fontSize" ? "1em" : (results[oldName] || 0);
+				results[oldName] = style.pixelLeft + "px";
 
-                // Revert the changed values
-                style.left = left;
-                el.runtimeStyle.left = rsLeft;
-            }
+				// Revert the changed values
+				style.left = left;
+				el.runtimeStyle.left = rsLeft;
+			}
 
-        }
-    }
+		}
+	}
 	
-    return results;
+	return results;
 };
 /**
  *  @add jQuery.fn
