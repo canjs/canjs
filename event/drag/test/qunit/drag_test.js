936b0141066475694742f57c0a04dda629476ef0
Brian Moschel <brian.moschel@gmail.com>
2010-06-10 22:22:34 -0500

    cleaning up test, not done yet

--- event/drag/test/qunit/drag_test.js
+++ event/drag/test/qunit/drag_test.js
@@ -1,22 +1,32 @@
-module("dom compare")
-asyncTest("Compare cases", function(){
-	$(document.body).append("<div id='drag' style='border:1px solid green;width:20px;height:20px;position:absolute;left:0px;top:0px;background-color:green;'></div>"+
-			"<div id='drop' style='border:1px solid yellow;width:20px;height:20px;position:absolute;left:30px;top:30px;background-color:yellow;'></div>");
+module("jquery/event/drag")
+test("dragging an element", function(){
+	var div = $("<div>"+
+			"<div id='drag' style='width:20px;height:20px;position:absolute;left:0px;top:0px;'></div>"+
+			"<div id='drop' style='width:20px;height:20px;position:absolute;left:30px;top:30px;'></div>"+
+			"</div>");
 	
-	$('#drag').live("draginit", function(){})
+	div.appendTo($("#qunit-test-area"))
+	$('#drag')
+		.live("draginit", function(){
+			$(this).addClass("draginit")
+		})
+		.live("dragmove", function(){
+			$(this).addClass("draginit")
+		})
+		.live("dragend", function(){
+			$(this).addClass("draginit")
+		})
 		.live("dragover", function(){
-			$(this).css('background-color', 'black').addClass("dragover")
+			$(this).addClass("dragover")
+		})
+		.live("dragout", function(){
+			$(this).addClass("dragover")
 		})
 	$('#drop').live("dropover", function(){ 
-		$(this).css('background-color', 'blue').addClass("dropover")
+		$(this).addClass("dropover")
 	})
-	  
-	new Synthetic("drag", {duration: 1}).send($("#drag")[0], $("#drop")[0]);
-	setTimeout(function(){
-		ok($("#drop").hasClass("dropover"), "Drop worked correctly")
-		ok($("#drag").hasClass("dragover"), "Drag worked correctly")
-		$("#drag").remove();
-		$("#drop").remove();
-		start()
-	}, 1500);
+	new Synthetic("drag", {to: "#drop"}).send($("#drag")[0]);
+	ok($("#drop").hasClass("dropover"), "Drop worked correctly")
+	ok($("#drag").hasClass("dragover"), "Drag worked correctly")
+	div.remove();
 })
