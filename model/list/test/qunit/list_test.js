27b238b8b06f2ccabea26da240e82568b5fd8f69
Justin Meyer <justinbmeyer@gmail.com>
2010-06-27 11:46:10 -0500

    list stuff

---
+++ model/list/test/qunit/list_test.js
@@ -0,0 +1,24 @@
+module("jquery/model/list")
+
+test("hookup with list", function(){
+	$.Model.extend("Person")
+	
+	$.Model.List.extend("Person.List",{
+		destroy : function(){
+			equals(this.length, 20,  "Got 20 people")
+		}
+	});
+	
+	
+	var div = $("<div>")
+	
+	for(var i =0; i < 20 ; i ++){
+		var child = $("<div>");
+		var p = new Person({foo: "bar"+i, id: i});
+		p.hookup( child[0] );
+		div.append(child)
+	}
+	var models = div.children().models();
+	ok(models.Class === Person.List, "correct type");
+	models.destroy();
+})
---
+++ model/list/test/qunit/run.js
@@ -0,0 +1,2 @@
+load('steal/rhino/env.js');
+Envjs('associations/test/qunit/qunit.html', {scriptTypes : {"text/javascript" : true,"text/envjs" : true}, fireLoad: true, logLevel: 2});
