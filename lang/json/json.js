ec9b91531abaf7754ce79cbd96b034356ec06382
Brian Moschel <brian.moschel@gmail.com>
2010-07-09 01:52:00 -0500

    json can be pluginified

--- lang/json/json.js
+++ lang/json/json.js
@@ -13,7 +13,7 @@
  * It is also influenced heavily by MochiKit's serializeJSON, which is 
  * copyrighted 2005 by Bob Ippolito.
  */
- 
+ steal.then(function(){
 (function($) {
     /** jQuery.toJSON( json-serializble )
         Converts the given argument into a JSON respresentation.
@@ -198,3 +198,4 @@
         '\\': '\\\\'
     };
 })(jQuery);
+})
