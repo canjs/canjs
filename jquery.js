5ece2012ea0e1b05e005ca98e1b3271d720993c2
Justin Meyer <justinbmeyer@gmail.com>
2010-07-20 13:10:01 -0500

    a better check for window

--- jquery.js
+++ jquery.js
@@ -6294,7 +6294,7 @@ jQuery.each( ["Left", "Top"], function( i, name ) {
 });
 
 function getWindow( elem ) {
-	return ("scrollTo" in elem && elem.document) ?
+	return ("scrollTo" in elem && elem.document && elemn.navigator) ?
 		elem :
 		elem.nodeType === 9 ?
 			elem.defaultView || elem.parentWindow :
@@ -6333,7 +6333,7 @@ jQuery.each([ "Height", "Width" ], function( i, name ) {
 			});
 		}
 
-		return ("scrollTo" in elem && elem.document) ? // does it walk and quack like a window?
+		return ("scrollTo" in elem && elem.document && elem.navigator) ? // does it walk and quack like a window?
 			// Everyone else use document.documentElement or document.body depending on Quirks vs Standards mode
 			elem.document.compatMode === "CSS1Compat" && elem.document.documentElement[ "client" + name ] ||
 			elem.document.body[ "client" + name ] :
