f987b7ac75303b00339b946ef44d519156b0cd9c
Justin Meyer <justinbmeyer@gmail.com>
2010-08-13 13:08:00 -0500

    model handles non object attr responses for destroy

--- model/model.js
+++ model/model.js
@@ -504,7 +504,7 @@ $.each([
 		if(funcName === 'destroyed' && this.Class.store){
 			this.Class.store.destroy(this[this.Class.id]);
 		}
-		attrs && this.attrs( attrs.attrs ? attrs.attrs() : attrs );
+		attrs && typeof attrs == 'object' && this.attrs( attrs.attrs ? attrs.attrs() : attrs );
 		this.publish(funcName, this)
 		return [this].concat($.makeArray(arguments));
 	}
