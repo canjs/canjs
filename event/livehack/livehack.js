c03673fb43ba22a3657f9a26d22f7456b0698864
ollinaie <ollinaie@gmx.net>
2011-01-03 23:54:20 +0100

    cleaned / linted

--- event/livehack/livehack.js
+++ event/livehack/livehack.js
@@ -1,31 +1,29 @@
-steal.plugins('jquery/event').then(function(){
+steal.plugins('jquery/event').then(function() {
 
 	var event = jQuery.event,
-		
+
 		//helper that finds handlers by type and calls back a function, this is basically handle
-		findHelper = function(events, types, callback){
-			for( var t =0; t< types.length; t++ ) {
-				var type = types[t], 
-					typeHandlers,
-					all = type.indexOf(".") < 0,
-					namespaces,
-					namespace; 
-				if ( !all ) {
+		findHelper = function( events, types, callback ) {
+			var t, type, typeHandlers, all, h, handle, namespaces, namespace;
+			for ( t = 0; t < types.length; t++ ) {
+				type = types[t];
+				all = type.indexOf(".") < 0;
+				if (!all ) {
 					namespaces = type.split(".");
 					type = namespaces.shift();
 					namespace = new RegExp("(^|\\.)" + namespaces.slice(0).sort().join("\\.(?:.*\\.)?") + "(\\.|$)");
 				}
-				typeHandlers = ( events[type] || [] ).slice(0);
-				
-				for( var h = 0; h <typeHandlers.length; h++ ) {
-					var handle = typeHandlers[h];
-					if( !handle.selector && (all || namespace.test( handle.namespace ))  ){
+				typeHandlers = (events[type] || []).slice(0);
+
+				for ( h = 0; h < typeHandlers.length; h++ ) {
+					handle = typeHandlers[h];
+					if (!handle.selector && (all || namespace.test(handle.namespace)) ) {
 						callback(type, handle.origHandler || handle.handler);
 					}
 				}
 			}
-		}
-	
+		};
+
 	/**
 	 * Finds event handlers of a given type on an element.
 	 * @param {HTMLElement} el
@@ -33,135 +31,135 @@ steal.plugins('jquery/event').then(function(){
 	 * @param {String} [selector] optional selector
 	 * @return {Array} an array of event handlers
 	 */
-	event.find  = function(el, types, selector){
-		var events = $.data(el, "events"), 
-			handlers = [];
+	event.find = function( el, types, selector ) {
+		var events = $.data(el, "events"),
+			handlers = [],
+			t, liver, live;
 
-		if( !events ) {
+		if (!events ) {
 			return handlers;
 		}
-		
-		if( selector ) {
-			if (!events.live) { 
+
+		if ( selector ) {
+			if (!events.live ) {
 				return [];
 			}
-			var live = events.live;
+			live = events.live;
 
-			for ( var t = 0; t < live.length; t++ ) {
-				var liver = live[t];
-				if(  liver.selector === selector &&  $.inArray(liver.origType, types  ) !== -1 ) {
+			for ( t = 0; t < live.length; t++ ) {
+				liver = live[t];
+				if ( liver.selector === selector && $.inArray(liver.origType, types) !== -1 ) {
 					handlers.push(liver.origHandler || liver.handler);
 				}
 			}
-		}else{
+		} else {
 			// basically re-create handler's logic
-			findHelper(events, types, function(type, handler){
+			findHelper(events, types, function( type, handler ) {
 				handlers.push(handler);
-			})
+			});
 		}
 		return handlers;
-	}
+	};
 	/**
 	 * Finds 
 	 * @param {HTMLElement} el
 	 * @param {Array} types
 	 */
-	event.findBySelector = function(el, types){
-		var events = $.data(el, "events"), 
-			selectors = {}, 
+	event.findBySelector = function( el, types ) {
+		var events = $.data(el, "events"),
+			selectors = {},
 			//adds a handler for a given selector and event
-			add = function(selector, event, handler){
-				var select = selectors[selector] ||  (selectors[selector] = {}),
+			add = function( selector, event, handler ) {
+				var select = selectors[selector] || (selectors[selector] = {}),
 					events = select[event] || (select[event] = []);
 				events.push(handler);
 			};
 
-		if ( !events ) {
+		if (!events ) {
 			return selectors;
 		}
 		//first check live:
-		$.each( events.live||[] , function(i, live) {
-			if( $.inArray(live.origType, types  ) !== -1 ) {
-				add( live.selector, live.origType, live.origHandler || live.handler );
+		$.each(events.live || [], function( i, live ) {
+			if ( $.inArray(live.origType, types) !== -1 ) {
+				add(live.selector, live.origType, live.origHandler || live.handler);
 			}
-		})
+		});
 		//then check straight binds
-		
-		findHelper(events, types, function(type, handler){
+		findHelper(events, types, function( type, handler ) {
 			add("", type, handler);
-		})
-		
+		});
+
 		return selectors;
-	}
-	$.fn.respondsTo = function(events){
-		if(!this.length){
+	};
+	$.fn.respondsTo = function( events ) {
+		if (!this.length ) {
 			return false;
-		}else{
+		} else {
 			//add default ?
 			return event.find(this[0], $.isArray(events) ? events : [events]).length > 0;
 		}
-	}
-	$.fn.triggerHandled = function(event, data){
-		event = ( typeof event == "string" ? $.Event(event) : event);
+	};
+	$.fn.triggerHandled = function( event, data ) {
+		event = (typeof event == "string" ? $.Event(event) : event);
 		this.trigger(event, data);
 		return event.handled;
-	}
+	};
 	/**
 	 * Only attaches one event handler for all types ...
 	 * @param {Array} types llist of types that will delegate here
 	 * @param {Object} startingEvent the first event to start listening to
 	 * @param {Object} onFirst a function to call 
 	 */
-	event.setupHelper = function(types, startingEvent, onFirst){
-		if(!onFirst) {
+	event.setupHelper = function( types, startingEvent, onFirst ) {
+		if (!onFirst ) {
 			onFirst = startingEvent;
 			startingEvent = null;
 		}
-		var add = function(handleObj){
-			
-			var selector = handleObj.selector || "";
-			if (selector) {
-				var bySelector = event.find(this, types, selector);
-				if (!bySelector.length) {
-					$(this).delegate(selector,startingEvent, onFirst );
+		var add = function( handleObj ) {
+
+			var bySelector, selector = handleObj.selector || "";
+			if ( selector ) {
+				bySelector = event.find(this, types, selector);
+				if (!bySelector.length ) {
+					$(this).delegate(selector, startingEvent, onFirst);
 				}
 			}
 			else {
 				//var bySelector = event.find(this, types, selector);
-				if(!event.find(this, types, selector).length){
+				if (!event.find(this, types, selector).length ) {
 					event.add(this, startingEvent, onFirst, {
 						selector: selector,
 						delegate: this
 					});
 				}
-				
+
 			}
-			
-		}
-		var remove = function(handleObj){
-			var selector = handleObj.selector || "";
-			if (selector) {
-				var bySelector = event.find(this, types, selector);
-				if (!bySelector.length) {
-					$(this).undelegate(selector,startingEvent, onFirst );
+
+		},
+			remove = function( handleObj ) {
+				var bySelector, selector = handleObj.selector || "";
+				if ( selector ) {
+					bySelector = event.find(this, types, selector);
+					if (!bySelector.length ) {
+						$(this).undelegate(selector, startingEvent, onFirst);
+					}
 				}
-			}
-			else {
-				if (!event.find(this, types, selector).length) {
-					event.remove(this, startingEvent, onFirst, {
-						selector: selector,
-						delegate: this
-					});
+				else {
+					if (!event.find(this, types, selector).length ) {
+						event.remove(this, startingEvent, onFirst, {
+							selector: selector,
+							delegate: this
+						});
+					}
 				}
-			}
-		}
-		$.each(types, function(){
+			};
+		$.each(types, function() {
 			event.special[this] = {
-				add:  add,
+				add: add,
 				remove: remove,
 				setup: function() {},
 				teardown: function() {}
 			};
 		});
-	}
-})
+	};
+});
