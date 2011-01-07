0dc4eedafd1666a4990eebb5121fae27e6187599
Pinhook <contact@pinhooklabs.com>
2011-01-07 16:20:07 -0600

    initial 2.0 changes

--- controller/view/test/qunit/controller_view_test.js
+++ controller/view/test/qunit/controller_view_test.js
@@ -1,15 +1,22 @@
-module("jquery/controller/view")
-test("this.view", function(){
+steal.plugins('jquery/controller/view','jquery/view/micro','funcunit/qunit')  //load qunit
+ .then(function(){
 	
-	$.Controller.extend("jquery.Controller.View.Test.Qunit",{
-		init: function() {
-			this.element.html(this.view())
-		}
-	})
-	jQuery.View.ext = ".micro";
-	$("#qunit-test-area").append("<div id='cont_view'/>");
+	module("jquery/controller/view");
 	
-	new jquery.Controller.View.Test.Qunit( $('#cont_view') );
+	test("this.view", function(){
+		
+		$.Controller.extend("jquery.Controller.View.Test.Qunit",{
+			init: function() {
+				this.element.html(this.view())
+			}
+		})
+		jQuery.View.ext = ".micro";
+		$("#qunit-test-area").append("<div id='cont_view'/>");
+		
+		new jquery.Controller.View.Test.Qunit( $('#cont_view') );
+		
+		ok(/Hello World/i.test($('#cont_view').text()),"view rendered")
+	});
 	
-	ok(/Hello World/i.test($('#cont_view').text()),"view rendered")
 });
+
--- event/drag/test/qunit/qunit.js
+++
@@ -1,4 +0,0 @@
-steal
- .plugins("jquery/event/drop",'funcunit/synthetic')  //load your app
- .plugins('funcunit/qunit' )  //load qunit
- .then("drag_test")
