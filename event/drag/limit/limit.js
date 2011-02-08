082bc6051d737168a9fd8243293d196144f49947
Pinhook <contact@pinhooklabs.com>
2011-02-07 20:01:36 -0600

    limit and step take center param, this should probably work something like positionable

--- event/drag/limit/limit.js
+++ event/drag/limit/limit.js
@@ -12,11 +12,11 @@ steal.plugins('jquery/event/drag', 'jquery/dom/cur_styles').then(function( $ ) {
 	 * @download  http://jmvcsite.heroku.com/pluginify?plugins[]=jquery/event/event/drag/limit/limit.js
 	 * limits the drag to a containing element
 	 * @param {jQuery} container
-	 * @param {Object} [options] can set the limit to the center of the object.  Can be 
-	 *   'center', 'width' or 'both'
+	 * @param {Object} [center] can set the limit to the center of the object.  Can be 
+	 *   'x', 'y' or 'both'
 	 * @return {$.Drag}
 	 */
-	.limit = function( container, options ) {
+	.limit = function( container, center ) {
 		//on draws ... make sure this happens
 		var styles = container.curStyles('borderTopWidth', 'paddingTop', 'borderLeftWidth', 'paddingLeft'),
 			paddingBorder = new $.Vector(
@@ -25,7 +25,7 @@ steal.plugins('jquery/event/drag', 'jquery/dom/cur_styles').then(function( $ ) {
 		this._limit = {
 			offset: container.offsetv().plus(paddingBorder),
 			size: container.dimensionsv(),
-			options : options
+			center : center
 		};
 		return this;
 	};
@@ -34,14 +34,15 @@ steal.plugins('jquery/event/drag', 'jquery/dom/cur_styles').then(function( $ ) {
 	$.Drag.prototype.position = function( offsetPositionv ) {
 		//adjust required_css_position accordingly
 		if ( this._limit ) {
-			var center = this._limit.options,
+			var limit = this._limit,
+				center = limit.center && limit.center.toLowerCase(),
 				movingSize = this.movingElement.dimensionsv('outer'),
-				halfHeight = center && center != 'width' ? movingSize.height() / 2 : 0,
-				halfWidth = center && center != 'height' ? movingSize.width() / 2 : 0,
-				lot = this._limit.offset.top(),
-				lof = this._limit.offset.left(),
-				height = this._limit.size.height(),
-				width = this._limit.size.width();
+				halfHeight = center && center != 'x' ? movingSize.height() / 2 : 0,
+				halfWidth = center && center != 'y' ? movingSize.width() / 2 : 0,
+				lot = limit.offset.top(),
+				lof = limit.offset.left(),
+				height = limit.size.height(),
+				width = limit.size.width();
 
 			//check if we are out of bounds ...
 			//above
