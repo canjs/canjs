879adc3a6eea8b5b6427bec525cd13a0ee7f12e2
Pinhook <contact@pinhooklabs.com>
2011-04-20 23:53:02 -0500

    moving styles

--- test/qunit/qunit.js
+++ test/qunit/qunit.js
@@ -1,15 +1,18 @@
 //we probably have to have this only describing where the tests are
 steal('//jquery/class/class_test',
+	  '//jquery/controller/controller_test',
+	  '//jquery/dom/compare/compare_test',
+	  '//jquery/dom/cur_styles/cur_styles_test',
 	  '//jquery/lang/lang_test',
 	  '//jquery/dom/fixture/fixture_test',
 	  '//jquery/event/drag/drag_test',
 	  '//jquery/event/key/key_test',
 	  '//jquery/tie/tie_test').plugins(	
-'jquery/controller/test/qunit',
+
 'jquery/controller/view/test/qunit',
 
-'jquery/dom/compare/test/qunit',
-'jquery/dom/cur_styles/test/qunit',
+
+
 'jquery/dom/dimensions/test/qunit',
 
 'jquery/dom/form_params/test/qunit',
