1235d15b16d59012bb27e673b284d1b1cc3290e1
Pinhook <contact@pinhooklabs.com>
2011-05-04 21:49:53 -0500

    cleaned

--- event/resize/resize.js
+++ event/resize/resize.js
@@ -1,20 +1,20 @@
-steal.plugins('jquery/event').then(function($){
+steal.plugins('jquery/event').then(function( $ ) {
 	/**
 	 * @add jQuery.event.special
 	 */
 	var resizers = $(),
-		resizeCount = 0, 
+		resizeCount = 0,
 		// bind on the window window resizes to happen
 		win = $(window),
-		windowWidth = 0, 
-		windowHeight = 0, 
+		windowWidth = 0,
+		windowHeight = 0,
 		timer;
-		
-	$(function(){
-		windowWidth = win.width(); 
+
+	$(function() {
+		windowWidth = win.width();
 		windowHeight = win.height();
 	})
-	
+
 	/**
 	 * @attribute resize
 	 * @parent specialevents
@@ -56,84 +56,80 @@ steal.plugins('jquery/event').then(function($){
 		setup: function( handleObj ) {
 			// add and sort the resizers array
 			// don't add window because it can't be compared easily
-			if(this !== window ){
+			if ( this !== window ) {
 				resizers.push(this);
 				$.unique(resizers);
 			}
 			// returns false if the window
 			return this !== window;
 		},
-		teardown : function(){
+		teardown: function() {
 			// we shouldn't have to sort
-			resizers = resizers.not( this );
-			
+			resizers = resizers.not(this);
+
 			// returns false if the window
 			return this !== window;
 		},
 		add: function( handleObj ) {
 			// increment the number of resizer elements
 			//$.data(this, "jquery.dom.resizers", ++$.data(this, "jquery.dom.resizers") );
-			
 			var origHandler = handleObj.handler;
 			handleObj.origHandler = origHandler;
 
-			handleObj.handler = function(ev, data){							
-			    var isWindow = this === window;
-				
+			handleObj.handler = function( ev, data ) {
+				var isWindow = this === window;
+
 				// if we are the window and a real resize has happened
 				// then we check if the dimensions actually changed
 				// if they did, we will wait a brief timeout and 
 				// trigger resize on the window
 				// this is for IE, to prevent window resize 'infinate' loop issues
-				if(isWindow && ev.originalEvent) {
+				if ( isWindow && ev.originalEvent ) {
 					var width = win.width(),
 						height = win.height();
-					
-					
-					if ( (width != windowWidth || height != windowHeight) ) {
+
+
+					if ((width != windowWidth || height != windowHeight)) {
 						//update the new dimensions
 						windowWidth = width;
 						windowHeight = height;
 						clearTimeout(timer)
-						timer = setTimeout(function(){
+						timer = setTimeout(function() {
 							win.triggerHandler("resize");
 						}, 1);
-						
+
 					}
 					return;
 				}
-				
+
 				// if this is the first handler for this event ...
-				if(resizeCount === 0){
+				if ( resizeCount === 0 ) {
 					// prevent others from doing what we are about to do
 					resizeCount++;
-					
+
 					//trigger all this element's handlers
-					$.event.handle.call( this, ev );
-					if(ev.isPropagationStopped()){
+					$.event.handle.call(this, ev);
+					if ( ev.isPropagationStopped() ) {
 						resizeCount--;
 						return;
 					}
-					
+
 					// get all other elements within this element that listen to resize
 					// and trigger their resize events
 					var index = resizers.index(this),
 						length = resizers.length,
-						child,
-						sub;
+						child, sub;
 
 					// if index == -1 it's the window
-					while(++index < length && 
-							(child = resizers[index]) && 
-							(isWindow || $.contains(this, child) )){
-						
+					while (++index < length && (child = resizers[index]) && (isWindow || $.contains(this, child)) ) {
+
 						// call the event
-						$.event.handle.call( child, ev );
-						
-						if(ev.isPropagationStopped()){
+						$.event.handle.call(child, ev);
+
+						if ( ev.isPropagationStopped() ) {
 							// move index until the item is not in the current child
-							while(++index < length && (sub = resizers[index])){
-								if( !$.contains(child, sub) ){
+							while (++index < length && (sub = resizers[index]) ) {
+								if (!$.contains(child, sub) ) {
 									// set index back one
 									index--;
 									break
@@ -141,18 +137,17 @@ steal.plugins('jquery/event').then(function($){
 							}
 						}
 					}
-					
+
 					// prevent others from responding
 					ev.stopImmediatePropagation();
 					resizeCount--;
 				} else {
 					handleObj.origHandler.call(this, ev, data);
-				}	
+				}
 			}
 		}
 	};
-	
-	// automatically bind on these
-	$([document, window]).bind('resize', function(){})
-})
 
+	// automatically bind on these
+	$([document, window]).bind('resize', function() {})
+})
