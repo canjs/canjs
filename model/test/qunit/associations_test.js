6e3b111f2acf2fd79989638fcb54d66bd0593c2a
Michael Malamud <mike.malamud@gmail.com>
2011-07-28 20:53:54 -0400

    Added test for serializing a Model.List during save

--- model/test/qunit/associations_test.js
+++ model/test/qunit/associations_test.js
@@ -11,7 +11,20 @@ module("jquery/model/associations",{
 				person : "MyTest.Person.model",
 				loans : "MyTest.Loan.models",
 				issues : "MyTest.Issue.models"
-			}
+			},
+			
+			update : function(id, attrs, success, error){
+				return $.ajax({
+					url : "/people/"+id,
+					data : attrs,
+					type : 'post',
+					dataType : "json",
+					fixture: function(){
+						return [{loansAttr: attrs.loans}]
+					},
+					success : success
+				})
+			}			
 		},
 		{});
 	}
@@ -48,4 +61,38 @@ test("associations work", function(){
 	equals(c.loans.length, 2);
 	
 	equals(c.loans[0].Class, MyTest.Loan);
+})
+
+
+test("Model.List association serialize on save", function(){
+	var c = new MyTest.Customer({
+		id: 5,
+		person : {
+			id: 1,
+			name: "thecountofzero"
+		},
+		issues : [],
+		loans : [
+			{
+				amount : 1000,
+				id: 2
+			},
+			{
+				amount : 19999,
+				id: 3
+			}
+		]
+	}),
+	cSave = c.save();
+	
+	stop();
+	cSave.then(function(customer){
+		start()
+		ok(customer.loansAttr._namespace === undefined, "_namespace does not exist");
+		ok(customer.loansAttr._data === undefined, "_data does not exist");
+		ok(customer.loansAttr._use_call === undefined, "_use_call does not exist");
+		ok(customer.loansAttr._changed === undefined, "_changed does not exist");
+		
+	});
+	
 })
