5da1fc18899d19fac23b7c1feca899ab10fbf414
Justin Meyer <justinbmeyer@gmail.com>
2011-12-03 14:05:43 -0600

    micro jmvc

---
+++ class/super/super_test.js
@@ -0,0 +1,10 @@
+steal('funcunit/qunit','./super',function(){
+
+module("super");
+
+test("super testing works", function(){
+	ok(true,"an assert is run");
+});
+
+
+});
---
+++ lang/observe/setter/setter.js
@@ -0,0 +1,31 @@
+steal('jquery/lang/observe',function($){
+
+
+var classize = $.String.classize,
+	proto =  $.Observe.prototype,
+	old = proto.__set;
+
+proto.__set = function(prop, value, current, success, error){
+	// check if there's a setter
+	var cap = classize(prop),
+		setName = "set" + cap,
+		errorCallback = function( errors ) {
+			var stub = error && error.call(self, errors);
+			trigger(self, "error." + attribute, errors);
+		},
+		self = this;
+	// if we have a setter
+	if ( this[setName] &&
+		// call the setter, if returned value is undefined,
+		// this means the setter is async so we 
+		// do not call update property and return right away
+		( value = this[setName](value, 
+			function(){ old.call(self,prop, value, current, success, errorCallback) },
+			errorCallback ) ) === undefined ) {
+		return;
+	}
+	old.call(self,prop, value, current, success, errorCallback);
+	return this;
+};
+
+});
