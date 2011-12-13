808c62481926c2783a75b67f561c817b1de4f08a
Justin Meyer <justinbmeyer@gmail.com>
2011-12-13 13:43:27 -0600

    model loads right

--- model/model.js
+++ model/model.js
@@ -1,5 +1,5 @@
 // get observe and all of it's plugins
 steal('jquery/lang/observe/setter',
-	  'jquery/lang/observe/attributes'
-	).then('./model_core')
-	  .then('./elements/elements.js')
+      'jquery/lang/observe/attributes')
+.then('./model_core')
+.then('./elements/elements.js')
