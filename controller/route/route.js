53581c482b9567a558bddfdbb3deb4b859159e7f
Justin Meyer <justinbmeyer@gmail.com>
2011-12-19 01:12:00 -0600

    adding route changes from micro branch

--- controller/route/route.js
+++ controller/route/route.js
@@ -8,7 +8,16 @@ steal('jquery/dom/route','jquery/controller', function(){
 	 * @param {Object} selector
 	 * @param {Object} cb
 	 */
-	jQuery.Controller.processors.route = function(el, event, selector, cb){
-	
+	jQuery.Controller.processors.route = function(el, event, selector, funcName, controller){
+		$.route(selector||"")
+		var check = function(){
+			if($.route.attr('route') === (selector||"")){
+				controller[funcName]($.route.attr())
+			}
+		}
+		$.route.bind('route',check);
+		return function(){
+			$.route.unbind('route',check)
+		}
 	}
 })
