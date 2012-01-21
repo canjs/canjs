301910b7b284ce134dafe4a53d605103afa3be77
Justin Meyer <justinbmeyer@gmail.com>
2012-01-20 18:02:05 -0600

    moved out steal

---
+++ view/view_steal.js
@@ -0,0 +1,38 @@
+steal("./view",function( $ ) {
+
+
+	$.extend($.View, {
+		register: function( info ) {
+			this.types["." + info.suffix] = info;
+
+			if ( window.steal ) {
+				steal.type(info.suffix + " view js", function( options, success, error ) {
+					var type = $view.types["." + options.type],
+						id = toId(options.rootSrc+'');
+
+					options.text = type.script(id, options.text)
+					success();
+				})
+			}
+		},
+		registerScript: function( type, id, src ) {
+			return "$.View.preload('" + id + "'," + $view.types["." + type].script(id, src) + ");";
+		},
+		preload: function( id, renderer ) {
+			$view.cached[id] = function( data, helpers ) {
+				return renderer.call(data, data, helpers);
+			};
+		}
+
+	});
+	if ( window.steal ) {
+		steal.type("view js", function( options, success, error ) {
+			var type = $view.types["." + options.type],
+				id = toId(options.rootSrc+'');
+
+			options.text = "steal('" + (type.plugin || "jquery/view/" + options.type) + "').then(function($){" + "$.View.preload('" + id + "'," + options.text + ");\n})";
+			success();
+		})
+	}
+
+});
