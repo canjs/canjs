66d0798b575ca30c8440f55612773312a2c7e5b5
Justin Meyer <justinbmeyer@gmail.com>
2012-01-23 14:07:24 -0600

    some more work on models

--- model/test/qunit/qunit.js
+++
@@ -1,5 +0,0 @@
-//we probably have to have this only describing where the tests are
-steal("jquery/model","jquery/dom/fixture")  //load your app
- .then('funcunit/qunit')  //load qunit
- .then("./model_test.js","./associations_test.js")
- 
