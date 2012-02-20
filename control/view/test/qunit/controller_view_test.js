f1737c16e62cb927ca854b12f7c3d2f682359d01
Austin McDaniel <amcdaniel2@gmail.com>
2012-02-20 16:56:33 -0600

    Updated 'can/control/view' to work with Can and all tests passing

--- control/view/test/qunit/controller_view_test.js
+++ control/view/test/qunit/controller_view_test.js
@@ -1,37 +1,37 @@
-steal('jquery/controller/view','jquery/view/micro','funcunit/qunit')  //load qunit
+steal('can/control/view','can/view/micro','funcunit/qunit')  //load qunit
  .then(function(){
 	
-	module("jquery/controller/view");
+	module("can/control/view");
 	
 	test("this.view", function(){
 		
-		Can.Control.extend("jquery.Controller.View.Test.Qunit",{
+		Can.Control.extend("Can.Control.View.Test.Qunit",{
 			init: function() {
 				this.element.html(this.view('init'))
 			}
 		})
-		jQuery.View.ext = ".micro";
+		Can.view.ext = ".micro";
 		$("#qunit-test-area").append("<div id='cont_view'/>");
 		
-		new jquery.Controller.View.Test.Qunit( $('#cont_view') );
+		new Can.Control.View.Test.Qunit( $('#cont_view') );
 		
 		ok(/Hello World/i.test($('#cont_view').text()),"view rendered")
 	});
 	
 	test("test.suffix.doubling", function(){
 		
-		Can.Control.extend("jquery.Controller.View.Test.Qunit",{
+		Can.Control.extend("Can.Control.View.Test.Qunit",{
 			init: function() {
 				this.element.html(this.view('init.micro'))
 			}
 		})
 		
-		jQuery.View.ext = ".ejs"; // Reset view extension to default
-		equal(".ejs", jQuery.View.ext); 
+		Can.view.ext = ".ejs"; // Reset view extension to default
+		equal(".ejs", Can.view.ext); 
 		
 		$("#qunit-test-area").append("<div id='suffix_test_cont_view'/>");
 		
-		new jquery.Controller.View.Test.Qunit( $('#suffix_test_cont_view') );
+		new Can.Control.View.Test.Qunit( $('#suffix_test_cont_view') );
 		
 		ok(/Hello World/i.test($('#suffix_test_cont_view').text()),"view rendered")
 	});
@@ -43,7 +43,7 @@ steal('jquery/controller/view','jquery/view/micro','funcunit/qunit')  //load qun
 		//equals(path, "//myproject/views/foo/bar/init.ejs", "view path is correct")
 		
 		Can.Control.extend("Myproject.Controllers.FooBar");
-		path = jQuery.Controller._calculatePosition(Myproject.Controllers.FooBar, "init.ejs", "init")
+		path = Can.Control._calculatePosition(Myproject.Controllers.FooBar, "init.ejs", "init")
 		equals(path, "//myproject/views/foo_bar/init.ejs", "view path is correct")
 	})
 });
