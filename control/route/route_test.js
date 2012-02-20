1e286827cd60c19399ba32dc5600046bd078993d
Austin McDaniel <amcdaniel2@gmail.com>
2012-02-20 16:23:12 -0600

    Fixed route test page and added test for routes

--- control/route/route_test.js
+++ control/route/route_test.js
@@ -2,8 +2,26 @@ steal('funcunit/qunit','./route',function(){
 
 module("route");
 
-test("route testing works", function(){
-	ok(true,"an assert is run");
+test("routes changed", function(){
+	//setup controller
+	Can.Control("Router",{
+		"foo/:bar route" : function(){
+			ok('route updated to foo/:bar')
+		},
+		"route" : function(){
+			ok('route updated to empty')
+		}
+	})
+
+	//append some anchors
+	Can.append( Can.$("#qunit-test-area"), '<a id="foo" href="#!foo/bar">foo/bar</a><a id="empty" href="#!">empty</a>')
+	
+	//init controller
+	new Router(window);
+	
+	//trigger change events
+	Can.trigger( Can.$('#foo'), 'click');
+	Can.trigger( Can.$('#empty'), 'click');
 });
 
 
