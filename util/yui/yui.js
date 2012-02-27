7c742e6c7c4b6ed7942587d8f3cce9ee1ceb14a5
Andy Kant <andy@andykant.com>
2012-02-26 23:38:14 -0600

    Updated YUI implementation (ajax and data methods). Fixed a typo in mootools.

--- util/yui/yui.js
+++ util/yui/yui.js
@@ -14,6 +14,7 @@ for (var i = 0; i < yui.length; i++) {
 steal("http://yui.yahooapis.com/3.4.1/build/yui-base/yui-base-min.js",
 	"../event.js")
 	.then.apply(steal, yui).then(
+// steal({ src: "http://yui.yahooapis.com/combo?3.4.1/build/yui-base/yui-base-min.js&3.4.1/build/oop/oop-min.js&3.4.1/build/event-custom-base/event-custom-base-min.js&3.4.1/build/features/features-min.js&3.4.1/build/dom-core/dom-core-min.js&3.4.1/build/dom-base/dom-base-min.js&3.4.1/build/selector-native/selector-native-min.js&3.4.1/build/selector/selector-min.js&3.4.1/build/node-core/node-core-min.js&3.4.1/build/node-base/node-base-min.js&3.4.1/build/event-base/event-base-min.js&3.4.1/build/event-delegate/event-delegate-min.js&3.4.1/build/node-event-delegate/node-event-delegate-min.js&3.4.1/build/pluginhost-base/pluginhost-base-min.js&3.4.1/build/pluginhost-config/pluginhost-config-min.js&3.4.1/build/node-pluginhost/node-pluginhost-min.js&3.4.1/build/dom-style/dom-style-min.js&3.4.1/build/dom-screen/dom-screen-min.js&3.4.1/build/node-screen/node-screen-min.js&3.4.1/build/node-style/node-style-min.js&3.4.1/build/querystring-stringify-simple/querystring-stringify-simple-min.js&3.4.1/build/io-base/io-base-min.js&3.4.1/build/querystring-parse-simple/querystring-parse-simple-min.js", type: "js" }, "../event.js").then(
 		
 	function(){
 		
@@ -95,6 +96,88 @@ steal("http://yui.yahooapis.com/3.4.1/build/yui-base/yui-base-min.js",
 		wrapped.each(function(node){
 			node.append(html)
 		});
+	}
+	
+	Can.data = function(wrapped, key, value){
+		if(value === undefined){
+			return wrapped[0].getData(key)
+		} else {
+			return wrapped.setData(key, value)
+		}
+	}
+	Can.remove = function(wrapped){
+		return wrapped.filter(function(node){ 
+			if(node.nodeType !== 1){
+				node.parentNode.removeChild(node);
+			} else {
+				return true;
+			}
+		}).destroy();
+	}
+	// destroyed method
+	var destroy = Element.prototype.destroy;
+	Element.prototype.destroy = function(){
+		Can.trigger(this,"destroyed",[],false)
+		var elems = this.getElementsByTagName("*");
+		for ( var i = 0, elem; (elem = elems[i]) !== undefined; i++ ) {
+			Can.trigger(elem,"destroyed",[],false);
+		}
+		destroy.apply(this, arguments)
+	}
+	
+	// Ajax
+	var optionsMap = {
+		type:"method",
+		success : undefined,
+		error: undefined
+	}
+	var updateDeferred = function(xhr, d){
+		for(var prop in xhr){
+			if(typeof d[prop] == 'function'){
+				d[prop] = function(){
+					xhr[prop].apply(xhr, arguments)
+				}
+			} else {
+				d[prop] = prop[xhr]
+			}
+		}
+	}
+	Can.ajax = function(options){
+		var d = Can.Deferred(),
+			requestOptions = Can.extend({}, options);
+			// map jQuery options to mootools options
+		
+		for(var option in optionsMap){
+			if(requestOptions[option] !== undefined){
+				requestOptions[optionsMap[option]] = requestOptions[option];
+				delete requestOptions[option]
+			}
+		}
+
+		var success = options.success,
+			error = options.error;
+		
+		requestOptions.on = {
+			success: function(transactionid, response, arguments) {
+				var data = response.responseText;
+				if(options.dataType ==='json'){
+					data = eval("("+data+")")
+				}
+				updateDeferred(request.io, d);
+				d.resolve(data,"success",request.io);
+				success && success(data,"success",request.io);
+			},
+			failure: function(transactionid, response, arguments) {
+				updateDeferred(request.io, d);
+				d.reject(request.io,"error");
+				error(request.io,"error");
+			}
+		};
+		
+		var request = new Y.io(requestOptions.url, requestOptions);
+		updateDeferred(request.io, d);
+		return d;
+			
 	}
 	
 	// Events
