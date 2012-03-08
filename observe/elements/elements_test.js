f23bc95f3aae77f81d19d2083b4aaa968590cc67
Austin McDaniel <amcdaniel2@gmail.com>
2012-03-08 11:03:08 -0600

    Moved 'can/model/elements/' to 'can/observe/elements' and ported to canjs, updated tests and added to plugin test suite

--- observe/elements/elements_test.js
+++ observe/elements/elements_test.js
@@ -1,32 +1,27 @@
-steal('funcunit/qunit','./elements',function(){
+steal('funcunit/qunit', 'can/observe/elements',function(){
 
-module("elements");
+module("can/obvserve/elements");
 
-test("elements testing works", function(){
-	ok(true,"an assert is run");
-});
-
-
-test("hookup and model", function(){
-	can.Model('Person')
-	var div = $(document.createElement('div'))
+test("hookup and obvserve", function(){
+	can.Observe('Person')
+	var div = can.$(document.createElement('div'))
 	var p = new Person({foo: "bar2", id: 5});
-	p.hookup( div[0] );
+	p.hookup( div );
 	ok(div.hasClass("person"), "has person");
 	ok(div.hasClass("person_5"), "has person_5");
-	equals(p, div.model(),"gets model" )
+	equals(div.model(), p,"gets model" )
 })
 
 // test that models returns an array of unique instances
-test("unique models", function(){
-	var div1 = $("<div/>")
-	var div2 = $("<div/>")
-	var div3 = $("<div/>")
+test("unique obvserves", function(){
+	var div1 = can.$("<div/>")
+	var div2 = can.$("<div/>")
+	var div3 = can.$("<div/>")
 	var p = new Person({foo: "bar2", id: 5});
 	var p2 = new Person({foo: "bar3", id: 4});
-	p.hookup( div1[0] );
-	p.hookup( div2[0] );
-	p2.hookup( div3[0] );
+	p.hookup( div1 );
+	p.hookup( div2 );
+	p2.hookup( div3 );
 	var models = div1.add(div2).add(div3).models();
 	equals(p, models[0], "gets models" )
 	equals(p2, models[1], "gets models" )
@@ -34,7 +29,7 @@ test("unique models", function(){
 })
 
 test("identity should replace spaces with underscores", function(){
-	can.Model("Task",{},{});
+	can.Observe("Task",{},{});
 	t = new Task({
 		id: "id with spaces"
 	});
@@ -43,11 +38,11 @@ test("identity should replace spaces with underscores", function(){
 
 
 test("hookup and elements", function(){
-	can.Model('Escaper',{
+	can.Observe('Escaper',{
 		escapeIdentity : true
 	},{});
 	
-	var ul = $('<ul><li></li></ul>'),
+	var ul = can.$('<ul><li></li></ul>'),
 		li = ul.find('li');
 	
 	var esc = new Escaper({id: " some crazy #/ %ing stuff"});
