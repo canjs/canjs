7a1e79c0f3cc0f7a1c15aecb19354e6a6b50975f
Justin Meyer <justinbmeyer@gmail.com>
2012-03-29 01:57:23 -0500

    some docs and the demos ready

--- route/route.js
+++ route/route.js
@@ -35,165 +35,7 @@ steal('can/observe', 'can/util/string/deparam', function() {
 		each = can.each,
 		extend = can.extend;
 
-	/**
-	 * @class can.route
-	 * @inherits can.Observe
-	 * @plugin can/route
-	 * @parent index
-	 * 
-	 * can.route helps manage browser history (and
-	 * client state) by
-	 * synchronizing the window.location.hash with
-	 * an [can.Control].
-	 * 
-	 * ## Background Information
-	 * 
-	 * To support the browser's back button and bookmarking
-	 * in an Ajax application, most applications use
-	 * the <code>window.location.hash</code>.  By
-	 * changing the hash (via a link or JavaScript), 
-	 * one is able to add to the browser's history 
-	 * without changing the page.  The [jQuery.event.special.hashchange event] allows
-	 * you to listen to when the hash is changed.
-	 * 
-	 * Combined, this provides the basics needed to
-	 * create history enabled Ajax websites.  However,
-	 * jQuery.Route addresses several other needs such as:
-	 * 
-	 *   - Pretty Routes
-	 *   - Keeping routes independent of application code
-	 *   - Listening to specific parts of the history changing
-	 *   - Setup / Teardown of widgets.
-	 * 
-	 * ## How it works
-	 * 
-	 * <code>can.route</code> is a [can.Control can.Observe] that represents the
-	 * <code>window.location.hash</code> as an 
-	 * object.  For example, if the hash looks like:
-	 * 
-	 *     #!type=videos&id=5
-	 *     
-	 * the data in <code>can.route</code> would look like:
-	 * 
-	 *     { type: 'videos', id: 5 }
-	 * 
-	 * 
-	 * can.route keeps the state of the hash in-sync with the data in
-	 * can.route.
-	 * 
-	 * ## can.Observe
-	 * 
-	 * can.route is a [can.Control can.Observe]. Understanding
-	 * can.Observe is essential for using can.route correctly.
-	 * 
-	 * You can
-	 * listen to changes in an Observe with bind and
-	 * delegate and change can.route's properties with 
-	 * attr and attrs.
-	 * 
-	 * ### Listening to changes in an Observable
-	 * 
-	 * Listen to changes in history 
-	 * by [can.Control.prototype.bind bind]ing to
-	 * changes in <code>can.route</code> like:
-	 * 
-	 *     can.route.bind('change', function(ev, attr, how, newVal, oldVal) {
-	 *     
-	 *     })
-	 * 
-     *  - attr - the name of the changed attribute
-     *  - how - the type of Observe change event (add, set or remove)
-     *  - newVal/oldVal - the new and old values of the attribute
-     * 
-	 * You can also listen to specific changes 
-	 * with [can.Control.prototype.delegate delegate]:
-	 * 
-	 *     can.route.delegate('id','change', function(){ ... })
-	 * 
-	 * Observe lets you listen to the following events:
-	 * 
-	 *  - change - any change to the object
-	 *  - add - a property is added
-	 *  - set - a property value is added or changed
-	 *  - remove - a property is removed
-	 * 
-	 * Listening for <code>add</code> is useful for widget setup
-	 * behavior, <code>remove</code> is useful for teardown.
-	 * 
-	 * ### Updating an observable
-	 * 
-	 * Create changes in the route data like:
-	 * 
-	 *     can.route.attr('type','images');
-	 * 
-	 * Or change multiple properties at once with
-	 * [can.Control.prototype.attrs attrs]:
-	 * 
-	 *     can.route.attr({type: 'pages', id: 5}, true)
-	 * 
-	 * When you make changes to can.route, they will automatically
-	 * change the <code>hash</code>.
-	 * 
-	 * ## Creating a Route
-	 * 
-	 * Use <code>can.route(url, defaults)</code> to create a 
-	 * route. A route is a mapping from a url to 
-	 * an object (that is the can.route's state).
-	 * 
-	 * If no routes are added, or no route is matched, 
-	 * can.route's data is updated with the [jQuery.String.deparam deparamed]
-	 * hash.
-	 * 
-	 *     location.hash = "#!type=videos";
-	 *     // can.route -> {type : "videos"}
-	 *     
-	 * Once routes are added and the hash changes,
-	 * can.route looks for matching routes and uses them
-	 * to update can.route's data.
-	 * 
-	 *     can.route( "content/:type" );
-	 *     location.hash = "#!content/images";
-	 *     // can.route -> {type : "images"}
-	 *     
-	 * Default values can also be added:
-	 * 
-	 *     can.route("content/:type",{type: "videos" });
-	 *     location.hash = "#!content/"
-	 *     // can.route -> {type : "videos"}
-	 *     
-	 * ## Delay setting can.route
-	 * 
-	 * By default, <code>can.route</code> sets its initial data
-	 * on document ready.  Sometimes, you want to wait to set 
-	 * this data.  To wait, call:
-	 * 
-	 *     can.route.ready(false);
-	 * 
-	 * and when ready, call:
-	 * 
-	 *     can.route.ready(true);
-	 * 
-	 * ## Changing the route.
-	 * 
-	 * Typically, you never want to set <code>location.hash</code>
-	 * directly.  Instead, you can change properties on <code>can.route</code>
-	 * like:
-	 * 
-	 *     can.route.attr('type', 'videos')
-	 *     
-	 * This will automatically look up the appropriate 
-	 * route and update the hash.
-	 * 
-	 * Often, you want to create links.  <code>can.route</code> provides
-	 * the [jQuery.route.link] and [jQuery.route.url] helpers to make this 
-	 * easy:
-	 * 
-	 *     can.route.link("Videos", {type: 'videos'})
-	 * 
-	 * @param {String} url the fragment identifier to match.  
-	 * @param {Object} [defaults] an object of default values
-	 * @return {jQuery.route}
-	 */
+
 	can.route = function( url, defaults ) {
         // Extract the variable names and replace with regEx that will match an atual URL with values.
 		var names = [],
@@ -223,6 +65,7 @@ steal('can/observe', 'can/util/string/deparam', function() {
 
 	extend(can.route, {
 		/**
+		 * @function can.route.param
 		 * Parameterizes the raw JS object representation provided in data.
 		 * If a route matching the provided data is found that URL is built
          * from the data. Any remaining data is added at the end of the
@@ -279,6 +122,8 @@ steal('can/observe', 'can/util/string/deparam', function() {
 			return can.isEmptyObject(data) ? "" : "&" + can.param(data);
 		},
 		/**
+		 * @function can.route.param
+		 * 
 		 * Populate the JS data object from a given URL.
 		 * 
 		 * @param {Object} url
@@ -350,6 +195,8 @@ steal('can/observe', 'can/util/string/deparam', function() {
          */
 		routes: {},
 		/**
+		 * @function can.route.param
+		 * @parent can.route
 		 * Indicates that all routes have been added and sets can.route.data
 		 * based upon the routes and the current hash.
 		 * 
@@ -372,6 +219,9 @@ steal('can/observe', 'can/util/string/deparam', function() {
 			return can.route;
 		},
 		/**
+		 * @function can.route.url
+		 * @parent can.route
+		 * 
 		 * Returns a url from the options
 		 * @param {Object} options
 		 * @param {Boolean} merge true if the options should be merged with the current options
@@ -384,7 +234,11 @@ steal('can/observe', 'can/util/string/deparam', function() {
 			return "#!" + can.route.param(options)
 		},
 		/**
+		 * @function can.route.link
+		 * @parent can.route
+		 * 
 		 * Returns a link
+		 * 
 		 * @param {Object} name The text of the link.
 		 * @param {Object} options The route options (variables)
 		 * @param {Object} props Properties of the &lt;a&gt; other than href.
@@ -397,7 +251,11 @@ steal('can/observe', 'can/util/string/deparam', function() {
 			}, props)) + ">" + name + "</a>";
 		},
 		/**
+		 * @function can.route.current
+		 * @parent can.route
+		 * 
 		 * Returns true if the options represent the current page.
+		 * 
 		 * @param {Object} options
          * @return {Boolean}
 		 */
