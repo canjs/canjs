94dbf51ef5ef24fb4ac9f5e7e9ccdf9cb600d793
David Luecke <daff@neyeon.com>
2012-03-30 09:52:23 -0600

    More Control plugin docs and examples

--- construct/proxy/proxy.js
+++ construct/proxy/proxy.js
@@ -2,67 +2,7 @@ steal('can/construct',function($){
 var isFunction = can.isFunction,
 	isArray = can.isArray,
 	makeArray = can.makeArray,
-/**
- * @page can.Construct.proxy
- * @parent can.Construct
- * @plugin can/construct/plugin
- * 
- * Adds a static and prototype proxy method that are
- * useful for creating callback functions that have
- * `this` set correctly.
- * 
- * 
- * Returns a callback function for a function on this Class.
- * Proxy ensures that 'this' is set appropriately.  
- * @codestart
- * can.Construct("MyClass",{
- *     getData: function() {
- *         this.showing = null;
- *         $.get("data.json",this.proxy('gotData'),'json')
- *     },
- *     gotData: function( data ) {
- *         this.showing = data;
- *     }
- * },{});
- * MyClass.showData();
- * @codeend
- * <h2>Currying Arguments</h2>
- * Additional arguments to proxy will fill in arguments on the returning function.
- * @codestart
- * can.Construct("MyClass",{
- *    getData: function( <b>callback</b> ) {
- *      $.get("data.json",this.proxy('process',<b>callback</b>),'json');
- *    },
- *    process: function( <b>callback</b>, jsonData ) { //callback is added as first argument
- *        jsonData.processed = true;
- *        callback(jsonData);
- *    }
- * },{});
- * MyClass.getData(showDataFunc)
- * @codeend
- * <h2>Nesting Functions</h2>
- * Proxy can take an array of functions to call as 
- * the first argument.  When the returned callback function
- * is called each function in the array is passed the return value of the prior function.  This is often used
- * to eliminate currying initial arguments.
- * @codestart
- * can.Construct("MyClass",{
- *    getData: function( callback ) {
- *      //calls process, then callback with value from process
- *      $.get("data.json",this.proxy(['process2',callback]),'json') 
- *    },
- *    process2: function( type,jsonData ) {
- *        jsonData.processed = true;
- *        return [jsonData];
- *    }
- * },{});
- * MyClass.getData(showDataFunc);
- * @codeend
- * @param {String|Array} fname If a string, it represents the function to be called.  
- * If it is an array, it will call each function in order and pass the return value of the prior function to the
- * next function.
- * @return {Function} the callback function.
- */
+
 proxy = function( funcs ) {
 
 			//args that should be curried
