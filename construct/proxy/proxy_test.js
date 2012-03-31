444f39ff417c238644a848ba53fcc4455c7f7403
Brian Moschel <brian.moschel@gmail.com>
2012-03-31 18:08:15 -0400

    all plugin tests pass in dist

--- construct/proxy/proxy_test.js
+++ construct/proxy/proxy_test.js
@@ -1,5 +1,7 @@
 (function(){
 
+var isDist = (/dist/.test(location.href));
+
 module("can/construct/proxy");
 
 
@@ -23,23 +25,25 @@ test("proxy", function(){
 	cb2();
 });
 
-test("proxy error", 1,function(){
-	can.Construct("Car",{
-		show: function( value ) {
-			equals(curVal, value)
-		}
-	},{
-		show: function( value ) {
-			
+// this won't work in dist mode (this functionality is removed)
+if(!isDist){
+	test("proxy error", 1,function(){
+		can.Construct("Car",{
+			show: function( value ) {
+				equals(curVal, value)
+			}
+		},{
+			show: function( value ) {
+				
+			}
+		})
+		try{
+			Car.proxy('huh');
+			ok(false, "I should have errored")
+		}catch(e){
+			ok(true, "Error was thrown")
 		}
 	})
-	try{
-		Car.proxy('huh');
-		ok(false, "I should have errored")
-	}catch(e){
-		ok(true, "Error was thrown")
-	}
-})
-
+}
 
 })();
