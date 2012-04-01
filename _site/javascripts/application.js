36b0c7da11381d93a1b030e892f0ce606b1cde91
retro <konjevic@gmail.com>
2012-03-31 19:40:19 -0700

    Add twitter and blog buttons

--- _site/javascripts/application.js
+++ _site/javascripts/application.js
@@ -284,3 +284,15 @@ if($('.lt-ie7, .lt-ie8, .lt-ie9').length == 0){
 } else {
 	$('#wrapper').menu('html,body');
 }
+
+
+// google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-2302003-11']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
