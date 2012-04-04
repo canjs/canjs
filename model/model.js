c93ae5478eea7fdb88fa6fc03211d81c8d4ca3bd
Cohuman Dev <dev@cohuman.com>
2012-04-04 11:21:59 -0700

    findOne and findAll in can/model don't work with can/construct/super. The context (thisArg) is set incorrectly

--- model/model.js
+++ model/model.js
@@ -498,8 +498,8 @@ steal('can/observe',function(){
 					// Increment requests.
 					self._reqs++;
 					// Make the request.
-					return pipe( old.call(self,params),
-						self, 
+					return pipe( old.call(this,params),
+						this,
 						method ).then(success,error).then(clean, clean);
 				}
 				
