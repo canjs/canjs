1044bf6f82c3d25c477fc34907100a94a8046745
Cohuman Dev <dev@cohuman.com>
2012-04-04 11:21:59 -0700

    findOne and findAll in can/model don't work with can/construct/super. The context (thisArg) is set incorrectly

--- construct/super/super_test.js
+++ construct/super/super_test.js
@@ -41,7 +41,30 @@ test("static super", function(){
 	
 	equal(Second.raise(2), 4)
 	
-})
+});
+
+test("findAll super", function(){
+
+  var Parent = can.Model({
+    findAll: function(){
+      equal(this.shortName, 'child');
+      return new can.Deferred();
+    },
+    shortName : 'parent'
+  },{});
+
+  var Child = Parent({
+    findAll: function(){
+      return this._super();
+    },
+     shortName : 'child'
+  },{});
+
+  stop();
+  expect(1);
+  Child.findAll({});
+  start();
+});
 
 /* Not sure I want to fix this yet.
 test("Super in derived when parent doesn't have init", function(){
