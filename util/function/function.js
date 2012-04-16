271928784a327bd05aaf2f6589b44bceb4367679
Ralph Holzmann <ralphholzmann@gmail.com>
2012-04-16 09:07:12 -0500

    Adding context and arguments passthrough.

--- util/function/function.js
+++ util/function/function.js
@@ -2,29 +2,34 @@ steal("can/util", function() {
 
   can.extend( can, {
 
-    debounce : function( fn, time ) {
+    debounce : function( fn, time, context ) {
 
       var timeout;
 
       return function() {
+
+        var args = arguments;
+
         clearTimeout( timeout );
         timeout = setTimeout(function() {
-          fn();
+          fn.apply( context, args );
         }, time );
       }
 
     },
 
-    throttle : function( fn, time ) {
+    throttle : function( fn, time, context ) {
 
       var run;
 
       return function() {
 
+        var args = arguments;
+
         if ( ! run ) {
           run = true;
           setTimeout(function() {
-            fn();
+            fn.apply( context, args );
             run = false;
           }, time );
         }
