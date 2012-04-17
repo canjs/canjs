b548104029ef0b3e8b0f54fa51b443606c96522a
Justin Meyer <justinbmeyer@gmail.com>
2012-04-17 12:33:17 -0500

    merging view steal

--- view/view.js
+++ view/view.js
@@ -426,4 +426,42 @@ steal("can/util")
 			return can.isArray(resolved) && resolved[1] === 'success' ? resolved[0] : resolved
 		};
 	
+	
+	if ( window.steal ) {
+		steal.type("view js", function( options, success, error ) {
+			var type = can.view.types["." + options.type],
+				id = toId(options.rootSrc);
+
+			options.text = "steal('" + (type.plugin || "jquery/view/" + options.type) + "').then(function($){" + "can.view.preload('" + id + "'," + options.text + ");\n})";
+			success();
+		})
+	}
+
+
+	$.extend(can.view, {
+		register: function( info ) {
+			this.types["." + info.suffix] = info;
+
+			if ( window.steal ) {
+				steal.type(info.suffix + " view js", function( options, success, error ) {
+					var type = can.view.types["." + options.type],
+						id = toId(options.rootSrc+'');
+
+					options.text = type.script(id, options.text)
+					success();
+				})
+			}
+		},
+		registerScript: function( type, id, src ) {
+			return "can.view.preload('" + id + "'," + $view.types["." + type].script(id, src) + ");";
+		},
+		preload: function( id, renderer ) {
+			can.view.cached[id] = function( data, helpers ) {
+				return renderer.call(data, data, helpers);
+			};
+		}
+
+	});
+	
+	
 });
