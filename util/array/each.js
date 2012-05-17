c3016bc9d7075e5a31cc37576d944d9734457307
Ralph Holzmann <ralphholzmann@gmail.com>
2012-05-17 15:41:03 -0500

    Undefined check in can.each.

--- util/array/each.js
+++ util/array/each.js
@@ -1,16 +1,18 @@
 steal(function(){
 	can.each = function(elements, callback) {
 		var i = 0, key;
-		if (typeof  elements.length == 'number' && elements.pop) {
-			elements.attr && elements.attr('length');
-			for(var len = elements.length; i < len; i++) {
-				if(callback(elements[i], i, elements) === false) return elements;
-			}
-		} else {
-			for(key in elements) {
-				if(callback(elements[key], key) === false) return elements;
-			}
-		}
+    if ( elements ) {
+      if (typeof  elements.length == 'number' && elements.pop) {
+        elements.attr && elements.attr('length');
+        for(var len = elements.length; i < len; i++) {
+          if(callback(elements[i], i, elements) === false) return elements;
+        }
+      } else {
+        for(key in elements) {
+          if(callback(elements[key], key) === false) return elements;
+        }
+      }
+    }
 		return elements;
 	}
 })
