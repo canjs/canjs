824805e0386839ddd2d53d17e8ac61587840cea1
Ralph Holzmann <ralphholzmann@gmail.com>
2012-05-22 14:24:04 -0500

    Fixing the make script.

--- util/make.js
+++ util/make.js
@@ -156,7 +156,7 @@ steal('steal/build/pluginify', function() {
 
 		steal.build.pluginify("can/" + input + ".js", {
 			out: "can/dist/edge/can." + output + ".js",
-			global: "this.can, this",
+			global: "this.can",
 			onefunc: true,
 			compress: false,
 			skipCallbacks: true,
@@ -172,7 +172,7 @@ steal('steal/build/pluginify', function() {
 		
 		steal.build.pluginify("can/" + input + ".js", {
 			out: "can/dist/edge/can." + output + ".js",
-			global: "this.can, this",
+			global: "this.can",
 			onefunc: true,
 			exclude: [
 				'can/util/jquery/jquery.1.7.1.js',
