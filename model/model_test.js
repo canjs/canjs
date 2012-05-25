235097a46e45329d63da9b6d28a6c284c1b2a157
Justin Meyer <justinbmeyer@gmail.com>
2012-05-25 14:06:32 -0500

    making it easier to overwrite model behavior

--- model/model_test.js
+++ model/model_test.js
@@ -641,4 +641,80 @@ test("templated destroy", function(){
 	new MyModel({id: 5}).destroy(function(){
 		start();
 	})
+});
+
+test("overwrite makeFindAll", function(){
+	
+	var store = {};
+	
+	var LocalModel = can.Model({
+		makeFindOne : function(findOne){
+			return function(params, success, error){
+				var def = new can.Deferred(),
+					data = store[params.id];
+				def.then(success, error)
+				// make the ajax request right away
+				var findOneDeferred = findOne(params);
+				
+				if(data){
+					var instance=  this.model(data);
+					findOneDeferred.then(function(data){
+						instance.updated(data)
+					}, function(){
+						can.trigger(instance,"error", data)
+					});
+					def.resolve(instance)
+				} else {
+					findOneDeferred.then(can.proxy(function(data){
+						var instance=  this.model(data);
+						store[instance[this.id]] = data;
+						def.resolve(instance)
+					}, this), function(data){
+						def.reject(data)
+					})
+				}
+				return def;
+			}
+		}
+	},{
+		updated : function(attrs){
+			can.Model.prototype.updated.apply(this, arguments);
+			store[this[this.constructor.id]] = this.serialize();
+		}
+	});
+	
+	
+	can.fixture("/food/{id}", function(settings){
+		
+		return count == 0 ? {
+			id: settings.data.id,
+			name : "hot dog"
+		} : {
+			id: settings.data.id,
+			name : "ice water"
+		}
+	})
+	var Food = LocalModel({
+		findOne : "/food/{id}"
+	},{});
+	stop();
+	var count = 0;
+	Food.findOne({id: 1}, function(food){
+		count = 1;
+		ok(true, "empty findOne called back")
+		food.bind("name", function(){
+			ok(true, "name changed");
+			equal(count, 2, "after last find one")
+			equals(this.name, "ice water");
+			start();
+		})
+		
+		Food.findOne({id: 1}, function(food2){
+			count = 2;
+			ok(food2 === food, "same instances")
+			equals(food2.name, "hot dog")
+		})
+	})
 })
+
+
