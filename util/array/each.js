317d75ccbf7e60dfa5251cb6f7552effa1020539
Ralph Holzmann <ralphholzmann@gmail.com>
2012-06-02 16:06:14 -0500

    Indentation fix.

--- util/array/each.js
+++ util/array/each.js
@@ -1,17 +1,18 @@
-steal(function(){
-	can.each = function( elements, callback, context ) {
-		var i = 0, key;
-		if ( elements ) {
-			if ( typeof  elements.length == 'number' && elements.pop ) {
+steal(function () {
+	can.each = function (elements, callback, context) {
+		var i = 0,
+		    key;
+		if (elements) {
+			if (typeof elements.length == 'number' && elements.pop) {
 				elements.attr && elements.attr('length');
-				for ( var len = elements.length; i < len; i++ ) {
-					if ( callback.call( context, elements[i], i, elements ) === false ) {
+				for (var len = elements.length; i < len; i++) {
+					if (callback.call(context || elements[i], elements[i], i, elements) === false) {
 						break;
 					}
 				}
 			} else {
-				for ( key in elements ) {
-					if ( callback.call( context, elements[key], key ) === false ) {
+				for (key in elements) {
+					if (callback.call(context || elements[i], elements[key], key, elements) === false) {
 						break;
 					}
 				}
