bbd2ad5e38df90f0ebcc09a20f7ea216fe20bd72
Ralph Holzmann <ralphholzmann@gmail.com>
2012-06-02 16:02:49 -0500

    Allow context to be passed as a third argument to can.each.

--- util/array/each.js
+++ util/array/each.js
@@ -1,18 +1,22 @@
 steal(function(){
-	can.each = function(elements, callback) {
+	can.each = function( elements, callback, context ) {
 		var i = 0, key;
-    if ( elements ) {
-      if (typeof  elements.length == 'number' && elements.pop) {
-        elements.attr && elements.attr('length');
-        for(var len = elements.length; i < len; i++) {
-          if(callback(elements[i], i, elements) === false) return elements;
-        }
-      } else {
-        for(key in elements) {
-          if(callback(elements[key], key) === false) return elements;
-        }
-      }
-    }
+		if ( elements ) {
+			if ( typeof  elements.length == 'number' && elements.pop ) {
+				elements.attr && elements.attr('length');
+				for ( var len = elements.length; i < len; i++ ) {
+					if ( callback.call( context, elements[i], i, elements ) === false ) {
+						break;
+					}
+				}
+			} else {
+				for ( key in elements ) {
+					if ( callback.call( context, elements[key], key ) === false ) {
+						break;
+					}
+				}
+			}
+		}
 		return elements;
 	}
-})
+});
