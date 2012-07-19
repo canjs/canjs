4995e75296489b30d4fda6c65c06aaf35a9c06f7
Justin Meyer <justinbmeyer@gmail.com>
2012-07-19 10:28:31 -0500

    zepto tests work

--- util/exports.js
+++ util/exports.js
@@ -1,4 +1,4 @@
-steal('can/util',function(can) {
+steal('can/util', function( can ) {
 
 	// Register as an AMD module if supported, otherwise attach to the window
 	if (false && typeof define === "function" && define.amd ) {
