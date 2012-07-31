61ec8886b8c78c3b02d079fffea7437399976028
Brian Moschel <brian@jupiterjs.com>
2012-07-31 17:13:33 -0400

    "Updating edge"

--- release/edge/can.jquery.js
+++ release/edge/can.jquery.js
@@ -44,6 +44,61 @@ module['can/util/array/each.js'] = (function (can) {
 		return elements;
 	}
 })(module["can/util/can.js"]);
+module['can/util/jquery/jquery.js'] = (function($, can) {
+	// jquery.js
+	// ---------
+	// _jQuery node list._
+	$.extend( can, jQuery, {
+		trigger: function( obj, event, args ) {
+			obj.trigger ?
+				obj.trigger( event, args ) :
+				$.event.trigger( event, args, obj, true );
+		},
+		addEvent: function(ev, cb){
+			$([this]).bind(ev, cb);
+			return this;
+		},
+		removeEvent: function(ev, cb){
+			$([this]).unbind(ev, cb);
+			return this;
+		},
+		// jquery caches fragments, we always needs a new one
+		buildFragment : function(result, element){
+			var ret = $.buildFragment([result],[element]);
+			return ret.cacheable ? $.clone(ret.fragment) : ret.fragment;
+		},
+		$: jQuery,
+		each: can.each
+	});
+
+	// Wrap binding functions.
+	$.each(['bind','unbind','undelegate','delegate'],function(i,func){
+		can[func] = function(){
+			var t = this[func] ? this : $([this]);
+			t[func].apply(t, arguments);
+			return this;
+		};
+	});
+
+	// Wrap modifier functions.
+	$.each(["append","filter","addClass","remove","data","get"], function(i,name){
+		can[name] = function(wrapped){
+			return wrapped[name].apply(wrapped, can.makeArray(arguments).slice(1));
+		};
+	});
+
+	// Memory safe destruction.
+	var oldClean = $.cleanData;
+
+	$.cleanData = function( elems ) {
+		$.each( elems, function( i, elem ) {
+			can.trigger(elem,"destroyed",[],false);
+		});
+		oldClean(elems);
+	};
+
+	return can;
+})(module["jquery"], module["can/util/can.js"], module["jquery"], module["can/util/preamble.js"], module["can/util/array/each.js"]);
 module['can/util/string/string.js'] = (function(can) {
 	// ##string.js
 	// _Miscellaneous string utility functions._  
