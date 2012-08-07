29ef726c0cafc3dad6c2afa64925dbe8aa5f979f
David Luecke <daff@neyeon.com>
2012-08-07 16:40:50 -0600

    View returns renderer function

--- view/view.js
+++ view/view.js
@@ -20,7 +20,7 @@ steal("can/util", function( can ) {
 		}
 		
 		// Convert it into a dom frag.
-		return $view.frag(result);
+		return can.isFunction(result) ? result : $view.frag(result);
 	};
 
 	can.extend( $view, {
@@ -291,7 +291,7 @@ steal("can/util", function( can ) {
 					response = deferred;
 					// And fire callback with the rendered result.
 					deferred.then(function( renderer ) {
-						callback(renderer(data, helpers))
+						callback(data !== undefined ? renderer(data, helpers) : renderer)
 					})
 				} else {
 					// if the deferred is resolved, call the cached renderer instead
@@ -309,12 +309,11 @@ steal("can/util", function( can ) {
 						// Otherwise, the deferred is complete, so
 						// set response to the result of the rendering.
 						deferred.then(function( renderer ) {
-							response = renderer(data, helpers);
+							response = data !== undefined ? renderer(data, helpers) : renderer;
 						});
 					}
 					
 				}
-	
 				return response;
 			}
 		}
--- view/view_test.js
+++ view/view_test.js
@@ -182,4 +182,11 @@ steal('can/view', function(can) {
 
 		ok( /fool sometimes/.test(div.innerHTML) , 'has fool sometimes'+div.innerHTML);
 	})
+
+	test("return renderer", function() {
+		can.view.ejs('renderer_test', "This is a <%= test %>");
+		var renderer = can.view('renderer_test');
+		ok(can.isFunction(renderer), 'Renderer is a function');
+		equal(renderer({ test : 'working test' }), 'This is a working test', 'Renderer rendered');
+	})
 });
