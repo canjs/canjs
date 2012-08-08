127488da5a37b7901246f03a8ef10a69f5508ac6
David Luecke <daff@neyeon.com>
2012-08-08 16:18:18 -0600

    control/modifier build

--- util/can-all.js
+++ util/can-all.js
@@ -1,4 +1,4 @@
 steal('can/util', 'can/construct/proxy', 'can/construct/super', 'can/control', 'can/control/plugin',
-	'can/control/modifier.js', 'can/view/modifiers', 'can/model', 'can/view/ejs', 'can/route', function(can) {
+	'can/control/modifier', 'can/view/modifiers', 'can/model', 'can/view/ejs', 'can/route', function(can) {
 	return can;
 });
