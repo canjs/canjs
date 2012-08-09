99ca547bb5b5377ecd83db90b57d74c4b75683b3
David Luecke <daff@neyeon.com>
2012-08-09 13:48:57 -0600

    Revert view renderer change

--- view/view_test.js
+++ view/view_test.js
@@ -182,12 +182,4 @@ steal('can/view', function(can) {
 
 		ok( /fool sometimes/.test(div.innerHTML) , 'has fool sometimes'+div.innerHTML);
 	})
-
-	test("return renderer", function() {
-		can.view.ejs('renderer_test', "This is a <%= test %>");
-		var renderer = can.view('renderer_test');
-		ok(can.isFunction(renderer), 'Renderer is a function');
-		equal(renderer({ test : 'working test' }), 'This is a working test', 'Renderer rendered');
-		can.view("//can/view/test/qunit/template.ejs");
-	})
 });
