b3e8568bcbe616e4fbd9e2bc554e7b2d1f81e423
Justin Meyer <justinbmeyer@gmail.com>
2012-08-15 18:02:52 -0500

    Update observe/observe.js
    some comments for get

--- observe/observe.js
+++ observe/observe.js
@@ -418,9 +418,21 @@ steal('can/util','can/construct', function(can, Construct) {
 		},
 		// Reads a property from the `object`.
 		_get: function( attr ) {
+			// break up the attr (`"foo.bar"`) into `["foo","bar"]`
 			var parts = attrParts(attr),
+				// get the value of the first attr name (`"foo"`)
 				current = this.__get(parts.shift());
-			return parts.length ? current ? current._get(parts) : undefined : current;
+			// if there are other attributes to read
+			return parts.length ? 
+				// and current has a value
+				current ?
+					// lookup the remaining attrs on current
+					current._get(parts) : 
+					// or if there's no current, return undefined
+					undefined 	
+				: 
+				// if there are no more parts, return current
+				current;
 		},
 		// Reads a property directly if an `attr` is provided, otherwise
 		// returns the "real" data object itself.
