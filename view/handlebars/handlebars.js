64bd2d68beb4d9aa1b49a04dd228dc594919ba5e
Austin McDaniel <amcdaniel2@gmail.com>
2012-08-30 20:31:57 -0500

    handlebar updates

--- view/handlebars/handlebars.js
+++ view/handlebars/handlebars.js
@@ -2,8 +2,27 @@ steal('can/view/handlebars/handlebars-1.0.0.beta.6.js',
       'can/observe/compute', 
       'can/view', 
       'can/view/scanner.js')
-.then(function(can){
+.then(function(){
 
-	
+/**
+ * These are the tokens for the scanner.
+ */
+can.view.tokens = {
+    tLeft: "{{$", // Template  ---- Not supported
+    tRight: "$}}", // Right Template  ---- Not supported
+    rLeft: "{{{", // Return
+    reLeft: "{{", // Return Escaped
+    cmntLeft: "{{#", // Comment ---- Not supported
+    left: "{{", // Run
+    right: "}}", // Right -> All have same,
+    rRight: "}}}"
+};
+
+can.hb = function(template, attrs){
+    var template = Handlebars.compile(template);
+    template = template(attrs)
+    //can.view.hookups[2](can.$("[data-view-id='2']"))
+    return can.view._scan(template).out;
+};
 
 });
