e383d5031a6ca1697b4125a0136aec7998031b14
Austin McDaniel <amcdaniel2@gmail.com>
2012-09-29 18:26:52 -0500

    adding initial plug for helpers in mustache and stubs w/ reqs for each

--- view/mustache/mustache.js
+++ view/mustache/mustache.js
@@ -53,7 +53,7 @@ function( can ){
 		object = object || {};
 		return this.template.fn.call(object, object, new Mustache.Helpers(object, extraHelpers || {}));
 	};
-	
+
 	extend(Mustache.prototype, {
 		/**
 		 * Singleton scanner instance for parsing templates.
@@ -71,7 +71,75 @@ function( can ){
 				cmntLeft: "{{!", // Comment
 				left: "{{#", // Run --- this is hack for now
 				right: "}}" // Right -> All have same FOR Mustache ...
-			}
+			},
+
+			helpers:[
+				/**
+				 * {{# evalvariable }}
+				 * ## Sections
+				 * Sections render blocks of text one or more times, depending on the value 
+				 * of the key in the current context.
+				 *
+				 * A section begins with a pound and ends with a slash. That is, {{#person}} 
+				 * begins a "person" section while {{/person}} ends it.
+				 *
+				 * The behavior of the section is determined by the value of the key.
+				 *
+				 * ### False Values or Empty Lists
+				 * If the person key exists and has a value of false or an empty list, the HTML 
+				 * between the pound and slash will not be displayed.
+				 *
+				 * ### Non-Empty Lists
+				 * If the person key exists and has a non-false value, the HTML between the pound 
+				 * and slash will be rendered and displayed one or more times.
+				 *
+				 * When the value is a non-empty list, the text in the block will be displayed once for 
+				 * each item in the list. The context of the block will be set to the current item for each 
+				 * iteration. In this way we can loop over collections.
+				 *
+				 * ### Lambdas
+				 * When the value is a callable object, such as a function or lambda, the object will be 
+				 * invoked and passed the block of text. The text passed is the literal block, unrendered. 
+				 * {{tags}} will not have been expanded - the lambda should do that on its own. In this way you 
+				 * can implement filters or caching.
+				 *
+				 * @param {String} content
+				 */
+				function(content){
+					return content;
+				},
+
+				/**
+				 * {{#person?}}
+				 * ## Non-False Values
+				 * When the value is non-false but not a list, it will be used as the context 
+				 * for a single rendering of the block.
+				 * @param {String} content
+				 */
+				function(content){
+					return content;
+				},
+
+				/**
+				 * {{^ evalvariable }}
+				 * ## Inverted Sections
+				 * An inverted section begins with a caret (hat) and ends with a slash. 
+				 * That is {{^person}} begins a "person" inverted section while {{/person}} ends it.
+				 * @param {String} content
+				 */
+				function(content){
+					return content;
+				},
+
+				/**
+				 * {{/ evalvariable }}
+				 * Closes sections.
+				 * @param {String} content
+				 */
+				function(content){
+					return content;
+				}
+			].concat(can.view.Scanner.prototype.helpers)
 		})
 	});
 
