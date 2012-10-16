09bce91556a04c52a8f6bdc064331d34ac24618d
David Luecke <daff@neyeon.com>
2012-10-16 12:58:37 -0600

    CI update

--- build/ci/init.sh
+++ build/ci/init.sh
@@ -2,10 +2,9 @@
 BASEDIR=$(dirname $0)
 mkdir $BASEDIR/can
 git archive HEAD | tar -x -C $BASEDIR/can
-node_modules/.bin/http-server -p 8000 &
 cd $BASEDIR
-git clone https://github.com/jupiterjs/funcunit.git
+../../node_modules/.bin/http-server -p 8000 &
+git clone https://github.com/jupiterjs/steal.git --depth=1 --quiet
+git clone https://github.com/jupiterjs/funcunit.git --depth=1 --quiet
 cd funcunit
 git submodule update --init --recursive
-cd ..
-git clone https://github.com/jupiterjs/steal.git
