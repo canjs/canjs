9d83efa04494b95bce442b219477d6265b5a757d
David Luecke <daff@neyeon.com>
2012-11-02 15:27:31 -0600

    Make can.esc better, return empty strings for NaN

--- util/string/string_test.js
+++ util/string/string_test.js
@@ -33,14 +33,23 @@ test("can.getObject", function(){
 	equals(obj,0, 'got 0 (falsey stuff)')
 });
 
-test("can.esc 0 value",function(){
+test("can.esc",function(){
 	var text = can.esc(0);
 	equal(text, "0", "0 value properly rendered");
+
 	text = can.esc(null);
 	deepEqual(text, "", "null value returns empty string");
+
+	text = can.esc();
+	deepEqual(text, "", "undefined returns empty string");
+
+	text = can.esc(NaN);
+	deepEqual(text, "", "NaN returns empty string");
+
 	text = can.esc("<div>&nbsp;</div>");
 	equal(text, "&lt;div&gt;&amp;nbsp;&lt;/div&gt;", "HTML escaped properly");
 });
+
 /*
 test("$.String.niceName", function(){
 	var str = "some_underscored_string";
