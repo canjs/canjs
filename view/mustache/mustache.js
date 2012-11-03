abe8f7d52c63187d04c984522baa1ebe999e27ed
Andy Kant <andy@andykant.com>
2012-11-03 14:36:10 -0500

    Fixing typos

--- view/mustache/mustache.js
+++ view/mustache/mustache.js
@@ -99,6 +99,7 @@ function( can ){
 					name: /^>[\s|\w]\w*/,
 					fn:function(content, cmd){
 						return "can.view.render('" + content.replace(/^>\s?/, '').trim() + "', _CONTEXT)";
+					}
 				},
 
 				/**
