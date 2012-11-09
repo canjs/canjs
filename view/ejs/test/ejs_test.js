1ababe093aae0d2b10b87b93b395a7e9102fce47
Eli Morris-Heft <eli.morris.heft@gmail.com>
2012-11-08 22:06:38 -0600

    Fixed event binding on options test.

--- view/ejs/test/ejs_test.js
+++ view/ejs/test/ejs_test.js
@@ -279,41 +279,26 @@ test("attribute single unescaped, html single unescaped", function(){
 });
 
 
-test("event binding / triggering on options", function(){
+test("event binding / triggering on options", 1, function(){
 	var frag = can.buildFragment("<select><option>a</option></select>",[document]);
 	var qta = document.getElementById('qunit-test-area');
 	qta.innerHTML = "";
 	qta.appendChild(frag);
 	
-	/*qta.addEventListener("foo", function(){
-		ok(false, "event handler called")
-	},false)*/
-	
 
 	// destroyed events should not bubble
-	
-	
-	qta.getElementsByTagName("option")[0].addEventListener("foo", function(ev){
+	can.addEvent.call(qta.getElementsByTagName("option")[0], 'foo', function(event) {
 		ok(true,"option called");
-		ev.stopPropagation();
-		//ev.cancelBubble = true;
-	}, false);
-	
-	qta.getElementsByTagName("select")[0].addEventListener("foo", function(){
+		event.stopPropagation();
+	});
+
+	can.addEvent.call(qta.getElementsByTagName("select")[0], 'foo', function(event) {
 		ok(true,"select called")
-	}, false)
-	
-	var ev = document.createEvent("HTMLEvents");
-	ev.initEvent("foo", true , true);
-	qta.getElementsByTagName("option")[0].dispatchEvent(ev); 
-	
-	//can.trigger(qta,"foo")
-	
-	stop();
-	setTimeout(function(){
-		start();
-		ok(true);
-	},100)
+	});
+
+	can.trigger(qta.getElementsByTagName('option')[0], 'foo');
+
+	qta.removeChild(qta.firstChild);
 })
 
 test("select live binding", function() {
