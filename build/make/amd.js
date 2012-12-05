62b2637fabfec56b2459def52eccede274ef7c21
David Luecke <daff@neyeon.com>
2012-12-05 11:27:48 -0700

    Remove control/view - Deprecated

--- build/make/amd.js
+++ build/make/amd.js
@@ -1,5 +1,5 @@
 steal('can/can.js', 'can/construct/proxy', 'can/construct/super', 'can/control', 'can/control/plugin',
-    'can/control/route', 'can/control/view', 'can/view/modifiers', 'can/model',
+    'can/control/route', 'can/view/modifiers', 'can/model',
     'can/view/ejs', 'can/view/mustache', 'can/observe/attributes', 'can/observe/delegate', 'can/observe/list',
     'can/observe/setter', 'can/observe/validations', 'can/route', 'can/view/modifiers', 'can/observe/backup',
     'can/util/fixture', 'can/util/library.js', 'can/util/mootools', 'can/util/dojo', 'can/util/yui',
