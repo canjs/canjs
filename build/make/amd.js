7e078997b06ee55b984798ba6d5631181a3964d1
Saad Tazi <saad@saadtazi.com>
2012-12-04 22:27:44 -0500

    removed non-official plugins / added can/control/route, control/plugin

--- build/make/amd.js
+++ build/make/amd.js
@@ -1,13 +1,9 @@
-steal('can/can.js', 'can/construct/proxy', 'can/construct/super', 'can/control',
-    'can/control/modifier', 'can/control/modifier/key', 'can/control/plugin', 'can/route',
-	'can/control/view', 'can/view/modifiers', 'can/model', 'can/model/cached', 'can/model/list',
-    'can/model/list/cookie', 'can/model/list/local','can/model/local', 'can/model/pages',
-    'can/model/service', 'can/model/store', 'can/view/ejs', 'can/view/mustache', 'can/observe/elements',
-	'can/observe/attributes', 'can/observe/delegate', 'can/observe/setter', 'can/route/pushstate',
-    'can/observe/list', 'can/observe/sort', 'can/observe/transaction', 'can/util/function',
-    'can/util/string/rsplit', 'can/util/string/classize.js', 'can/observe/validations',
-	'can/view/modifiers', 'can/observe/backup', 'can/util/fixture', 'can/util/library.js',
-	'can/util/mootools', 'can/util/dojo', 'can/util/yui','can/util/zepto', 'can/util/jquery',
+steal('can/can.js', 'can/construct/proxy', 'can/construct/super', 'can/control', 'can/control/plugin',
+    'can/control/route', 'can/control/view', 'can/view/modifiers', 'can/model',
+    'can/view/ejs', 'can/view/mustache', 'can/observe/attributes', 'can/observe/delegate', 'can/observe/list',
+    'can/observe/setter', 'can/observe/validations', 'can/route', 'can/view/modifiers', 'can/observe/backup',
+    'can/util/fixture', 'can/util/library.js', 'can/util/mootools', 'can/util/dojo', 'can/util/yui',
+    'can/util/zepto', 'can/util/jquery',
 	
 function(can) {
 	return can;
