b558c9b3fb2e45bf0d73608f8fa89114211c21e2
Brian Moschel <brian.moschel@gmail.com>
2012-12-06 21:51:27 -0600

    if control or model is loaded after proxy, proxy will still work on static methods

--- construct/proxy/proxy_test.js
+++ construct/proxy/proxy_test.js
@@ -1,20 +1,35 @@
-steal('can/util', 'can/construct/proxy', function(can) {
+steal('can/util', 'can/control').then('can/construct/proxy', function(can) {
 	var isDist = (/dist/.test(location.href));
 
 	module("can/construct/proxy");
 
 
+	test("static proxy if control is loaded first", function(){
+		var curVal = 0;
+		expect(2);
+		can.Control("Car",{
+			show: function( value ) {
+				equals(curVal, value)
+			}
+		},{})
+		var cb = Car.proxy('show');
+		curVal = 1;
+		cb(1)
+
+		curVal = 2;
+		var cb2 = Car.proxy('show',2)
+		cb2();
+	});
+
+
 	test("proxy", function(){
 		var curVal = 0;
+		expect(2);
 		can.Construct("Car",{
 			show: function( value ) {
 				equals(curVal, value)
 			}
-		},{
-			show: function( value ) {
-
-			}
-		})
+		},{})
 		var cb = Car.proxy('show');
 		curVal = 1;
 		cb(1)
