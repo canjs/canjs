6b8752da787ba1879c27e11b068e5deafc8a209b
Dejan Velimirovic <dvsoftware@gmail.com>
2012-12-14 12:56:46 +0100

    Cannot call method "getItem" of undefined when packing

--- model/cached/cached.js
+++ model/cached/cached.js
@@ -4,7 +4,6 @@ steal('can/model','can/util/object', 'can/util/json.js', function(){
 can.Model("can.Model.Cached", {
 	setup: function(){
 		can.Model.setup.apply(this, arguments);
-		console.log(this._shortName)
 		// setup data
 		this._cached = can.evalJSON(window.localStorage.getItem(this.cachedKey())) || {};
 	},
