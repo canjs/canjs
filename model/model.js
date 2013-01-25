e5f1c2622701d955720fa6558b9e63e67cd85eda
David Luecke <daff@neyeon.com>
2013-01-25 16:32:41 -0700

    Added docs for removeAttr

--- model/model.js
+++ model/model.js
@@ -166,6 +166,27 @@ steal('can/util','can/observe', function( can ) {
 		 *       id: "Id"
 		 *     },{});
 		 */
+		/**
+		 * @attribute removeAttr
+		 * Sets whether model conversion should remove non existing attributes or merge with
+		 * the existing attributes. The default is `false`.
+		 * For example, if `Task.findOne({ id: 1 })` returns
+		 *
+		 *      { id: 1, name: 'Do dishes', index: 1 }
+		 *
+         * for the first request and
+		 *
+		 *      { id: 1, name: 'Really do dishes' }
+		 *
+		 *  for the next request, the actual model attributes would look like:
+		 *
+		 *      { id: 1, name: 'Really do dishes', index: 1 }
+		 *
+		 *  Because the attributes of the original model and the updated model will
+		 *  be merged. Setting `removeAttr` to `true` will result in model attributes like
+		 *
+		 *      { id: 1, name: 'Really do dishes' }
+		 */
 	ajaxMethods = {
 		/**
 		 * @function create
