e8a42e4f93b99941889a3e0b67ebe0345f60c04e
David Luecke <daff@neyeon.com>
2013-01-27 23:00:22 -0700

    Added example for merging arrays

--- model/model.js
+++ model/model.js
@@ -172,20 +172,21 @@ steal('can/util','can/observe', function( can ) {
 		 * the existing attributes. The default is `false`.
 		 * For example, if `Task.findOne({ id: 1 })` returns
 		 *
-		 *      { id: 1, name: 'Do dishes', index: 1 }
+		 *      { id: 1, name: 'Do dishes', index: 1, color: ['red', 'blue'] }
 		 *
          * for the first request and
 		 *
-		 *      { id: 1, name: 'Really do dishes' }
+		 *      { id: 1, name: 'Really do dishes', color: ['green'] }
 		 *
 		 *  for the next request, the actual model attributes would look like:
 		 *
-		 *      { id: 1, name: 'Really do dishes', index: 1 }
+		 *      { id: 1, name: 'Really do dishes', index: 1, color: ['green', 'blue'] }
 		 *
 		 *  Because the attributes of the original model and the updated model will
 		 *  be merged. Setting `removeAttr` to `true` will result in model attributes like
 		 *
-		 *      { id: 1, name: 'Really do dishes' }
+		 *      { id: 1, name: 'Really do dishes', color: ['green'] }
+		 *
 		 */
 	ajaxMethods = {
 		/**
