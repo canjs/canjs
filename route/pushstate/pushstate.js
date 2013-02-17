10c10eaf1fa534127663cac0f610d804878d2d69
DVSoftware <dvsoftware@gmail.com>
2013-02-17 19:55:33 +0100

    Don't treat links to "#" as "/"
    Breaks many places where "#" is placed as a dummy href and are handled through click events. It can be prevented with stopPropagation() but then it won't propagate to its parents. This fixes my case, but i'm not sure if it's desirable for others.

--- route/pushstate/pushstate.js
+++ route/pushstate/pushstate.js
@@ -34,14 +34,16 @@ steal('can/util', 'can/route', function(can) {
             _setup: function() {
                 // intercept routable links
                 can.$('body').on('click', 'a', function(e) {
-                    // Fix for ie showing blank host, but blank host means current host.
-                    if(!this.host) {
-                      this.host = window.location.host;
-                    }
-                    // HTML5 pushstate requires host to be the same. Don't prevent default for other hosts.
-                    if(can.route.updateWith(this.pathname+this.search) && window.location.host == this.host) {
-                        e.preventDefault();
-                    }
+                	if($(this).attr('href') != '#') {
+	                    // Fix for ie showing blank host, but blank host means current host.
+	                    if(!this.host) {
+	                      this.host = window.location.host;
+	                    }
+	                    // HTML5 pushstate requires host to be the same. Don't prevent default for other hosts.
+	                    if(can.route.updateWith(this.pathname+this.search) && window.location.host == this.host) {
+	                        e.preventDefault();
+	                    }
+                	}
                 });
                 can.route.history.bind('path',can.route.setState);
             },
