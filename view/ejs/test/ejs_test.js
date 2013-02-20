37b56b80bc1d0bad90d956af169d7f816e0fb41e
Austin McDaniel <amcdaniel2@gmail.com>
2013-02-20 16:36:05 -0600

    Hookup proxy 'this' correctly

--- view/ejs/test/ejs_test.js
+++ view/ejs/test/ejs_test.js
@@ -1263,4 +1263,15 @@ test("Observe with array attributes", function() {
 	equal(div.getElementsByTagName('div')[0].innerHTML, 'Hello again', 'Check updated message');
 })
 
+test('hookups `this` correctly', function(){
+	var milk = { from: "cows" };
+	var html = "<div <%= (el)-> can.append(can.$(el), this.from) %>></div>";
+	var compiled = new can.EJS({ text: html }).render(milk);
+
+	var div = document.createElement('div');
+	div.appendChild(can.view.frag(compiled));
+
+	equal(div.getElementsByTagName('div')[0].innerHTML, 'cows', 'Check html for from');
+});
+
 })();
