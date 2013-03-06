5172329d738afd3ea3210fcbe1f7a0c97749a4ff
Pablo Aguiar <scorphus@gmail.com>
2013-03-06 16:26:11 -0300

    Treat "--" as delimiter of empty element, closes #271

--- view/ejs/test/ejs_test.js
+++ view/ejs/test/ejs_test.js
@@ -1263,4 +1263,24 @@ test("Observe with array attributes", function() {
 	equal(div.getElementsByTagName('div')[0].innerHTML, 'Hello again', 'Check updated message');
 })
 
+//Issue 271
+test('live binding with html comment', function(){
+	var text = '<table><tr><th>Todo</th></tr><!-- do not bother with me -->' +
+			'<% todos.each(function(todo){ %><tr><td><%= todo.name %></td></tr><% }) %></table>',
+		Todos = new can.Observe.List([
+			{id: 1, name: 'Dishes'},
+		]),
+		compiled = new can.EJS({text: text}).render({todos: Todos}),
+		div = document.createElement('div');
+
+	div.appendChild(can.view.frag(compiled));
+	equals(div.getElementsByTagName('table')[0].getElementsByTagName('td').length, 1, '1 item in list');
+
+	Todos.push({id: 2, name: 'Laundry'});
+	equals(div.getElementsByTagName('table')[0].getElementsByTagName('td').length, 2, '2 items in list');
+
+	Todos.splice(0, 2);
+	equals(div.getElementsByTagName('table')[0].getElementsByTagName('td').length, 0, '0 items in list');
+})
+
 })();
