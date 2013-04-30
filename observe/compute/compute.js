559070d9bab71d490a5aa287d7b2895e7decc733
Alexis Abril <dev@alexisabril.com>
2013-04-30 11:09:24 -0500

    Refactoring bind to attach to can.

--- observe/compute/compute.js
+++ observe/compute/compute.js
@@ -461,12 +461,12 @@ steal('can/util', 'can/util/bind', function(can, bind) {
 			 * @function bind
 			 * `compute.bind("change", handler(event, newVal, oldVal))`
 			 */
-			bind: bind.bind,
+			bind: bind.bindAndSetup,
 			/**
 			 * @function unbind
 			 * `compute.unbind("change", handler(event, newVal, oldVal))`
 			 */
-			unbind: bind.unbind
+			unbind: bind.unbindAndTeardown
 		});
 	};
 	can.compute.binder = computeBinder;
