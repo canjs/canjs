2240822968002d87a745f2079d0124e694d645cf
Veljko Dragsic <veljko@proton>
2013-06-14 16:16:17 +0200

    NestedRefrence test and comments.

--- observe/lazy/nested_reference_test.js
+++ observe/lazy/nested_reference_test.js
@@ -1,58 +1,72 @@
 (function(){
 	
-module("can/observe/lazy/nested_reference")
+	module("can/observe/lazy/nested_reference");
 
-test("nested basics",function(){
-	var data = [
+	test("Basics", 3, function(){
+		var data = [
+			{id: 0, items: [{id: "0.0"}, {id: "0.1"}]},
+			{id: 1, items: [{id: "1.0"}, {id: "1.1"}, {id: "1.2"}]}
+		];
+		var nested = new can.NestedReference(data),		
+			ref1 = nested.make("1.items.1"),
+			ref2 = nested.make("1.items.2"),
+			ref3 = nested.make("0.items.1"),
+			count = 0;
+
+		equal( ref1(), "1.items.1" ,"Path created correctly");
+		
+		data[1].items.shift()
+		
+		equal( ref1(), "1.items.0" ,"Path updated correctly after shifting - ref1");
+		equal( ref2(), "1.items.1" ,"Path updated correctly after shifting - ref2");
+				
+	});
+
+	test("Removing children", 5, function() {
+		var data = [
+			{id: 0, items: [{id: "0.0"}, {id: "0.1"}]},
+			{id: 1, items: [{id: "1.0"}, {id: "1.1"}, {id: "1.2"}]}
+		];
+		var nested = new can.NestedReference(data),		
+			ref1 = nested.make("1.items.1"),
+			ref2 = nested.make("1.items.2"),
+			ref3 = nested.make("0.items.1"),
+			count = 0;
+		
+		nested.removeChildren("1.items", function(child, ref){
+			count++;
+			if( count == 1) {
+				equal(child, data[1].items[1], "Reference removed - correct child");
+				equal(ref, '1.items.1', "Reference removed - correct path");
+			} else if (count == 2) {
+				equal(child, data[1].items[2], "Reference removed - correct child");
+				equal(ref, '1.items.2', "Referece removed - correct path");
+			}
+		});
+		
+		equal(nested.references.length, 1, "'1.items*' references removed, '0.items.1' remains.");
+	});
+
+	test("'.each' iterator", 2, function(){		
+		var data = [
 			{id: 0, items: [{id: "0.0"},{id: "0.1"}]},
 			{id: 1, items: [{id: "1.0"},{id: "1.1"}]}
-		]
-	
-	
-	var nested = new can.NestedReference(data);
-	
-	var ref1 = nested.make("1.items.1");
-	
-	
-	data[1].items.shift()
-	
-	equal( ref1(), "1.items.0" ,"path updated correctly");
-	
-	
-	nested.removeChildren("1.items",function(child){
-		equal(child,data[1].items[0])
-	})
-	
-	
-	
-})
-
-test("nested each",function(){
-	
-	var data = [
-			{id: 0, items: [{id: "0.0"},{id: "0.1"}]},
-			{id: 1, items: [{id: "1.0"},{id: "1.1"}]}
-		]
-	
-	
-	var nested = new can.NestedReference(data);
-	
-	nested.make("0.items");
-	
-	nested.make("1.items.0");
-	
-	var callbackCount = 0;
-	nested.each(function(child, ref, path){
-		callbackCount++;
-		if(callbackCount == 1){
-			equal(child,data[0].items)
-		} else {
-			equal(child,data[1].items[0])
-		}
+		],				
+			nested = new can.NestedReference(data);
 		
+		nested.make("0.items");
 		
-	})
-})
-
+		nested.make("1.items.0");
+		
+		var callbackCount = 0;
+		nested.each(function(child, ref, path){
+			callbackCount++;
+			if(callbackCount == 1){
+				equal(child, data[0].items, "First reference exists - '0.items'")
+			} else {
+				equal(child, data[1].items[0], "Second reference exists -'1.items.0'")
+			}			
+		})
+	});
 
 })();
