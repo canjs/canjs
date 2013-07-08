7e51d7c21efd4f64fb3db0a0a414f156b41aa0a4
Justin Meyer <justinbmeyer@gmail.com>
2013-07-08 15:05:03 -0500

    all demos updated to new steal

--- observe/elements/elements_test.js
+++ observe/elements/elements_test.js
@@ -1,15 +1,12 @@
 (function() {
 
-module("jquery/observe/elements")
+module("can/observe/elements")
 
 test("identity uses the real id", function(){
   var Person = can.Model.extend({
     id:'ssn',
     _fullName: 'Person'
   },{
-    ssn: function() {
-      return 'xxx-xx-'+this.attr('ssn').substring(7);
-    }
   })
 
   equal(new Person({ssn:'987-65-4321'}).identity(),'Person_987-65-4321');
