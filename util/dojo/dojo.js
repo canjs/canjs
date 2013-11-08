760deb7e212bd2b7f52c18e1137c0316d6f56c50
Josh Dean <imjoshdean@me.com>
2013-11-08 15:26:35 -0600

    Fix to Dojo events not getting event information.

--- util/dojo/dojo.js
+++ util/dojo/dojo.js
@@ -37,7 +37,9 @@ steal('can/util/can.js', 'dojo', 'can/util/event.js', 'can/util/fragment.js', 'c
 					//						// fireEvent. /me sighs. http://gist.github.com/315318
 					//						throw("janktastic");
 					//					}
-					n.fireEvent(ev);
+					var evObj = document.createEventObject();
+					mix(evObj, a);
+					n.fireEvent(ev, evObj);
 				} catch (er) {
 					// a lame duck to work with. we're probably a 'custom event'
 					var evdata = mix({
