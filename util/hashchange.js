17a6cac8c613d8d514ee769cb62d298a3bd19bb8
Psykoral <psykoral@gmail.com>
2014-01-16 17:32:15 -0800

    defining 'can' as a dependancy
    no longer relying on the global namespaced 'can'. As noted in Issue https://github.com/bitovi/canjs/issues/673

--- util/hashchange.js
+++ util/hashchange.js
@@ -1,4 +1,4 @@
-steal(function() {
+steal('can/util', function(can) {
 	// This is a workaround for libraries that don't natively listen to the window hashchange event
 	!function() {
 		var addEvent = function (el, ev, fn) {
