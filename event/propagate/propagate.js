7f3447eacab12c9b6ec9d836c486f8fa2de79540
Andy Kant <andy@andykant.com>
2014-03-10 15:42:27 -0500

    Tweaked event/propagate logic to avoid unnecessary logic, added isPropagationStopped support

--- event/propagate/propagate.js
+++ event/propagate/propagate.js
@@ -6,24 +6,31 @@ steal('can/event', function(event) {
 		var propagate = (this.__bindEvents && this.__propagate) || false;
 
 		// Inject propagation into event, when applicable
-		if (propagate && typeof ev.__stop === 'undefined') {
-			ev = can.simpleExtend(
-				typeof ev === 'string' ? { type: ev } : ev,
-				{
-					__stop: false,
-					stopPropagation: function() {
-						this.__stop = true;
-					},
-					target: this
-				}
-			);
+		if (propagate && typeof ev.isPropagationStopped === 'undefined') {
+			// Initialize the event into an object
+			if (typeof ev === 'string') {
+				ev = {
+					type: ev
+				};
+			}
+
+			// Add extra event properties
+			// This could be done with can.simpleExtend, but this avoids extra logic execution
+			var stop = false;
+			ev.target = ev.target || this;
+			ev.stopPropagation = function() {
+				stop = true;
+			};
+			ev.isPropagationStopped = function() {
+				return stop;
+			};
 		}
 
 		// Call original dispatch
 		dispatch.call(this, ev, args);
 
 		// Call propagated events
-		if (propagate && !ev.__stop && this[propagate]) {
+		if (propagate && !ev.isPropagationStopped() && this[propagate]) {
 			can.dispatch.call(this[propagate], ev, args);
 		}
 	};
