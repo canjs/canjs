ae6d9f026793193d31e192a032f0065d56f47965
Andy Kant <andy@andykant.com>
2014-03-10 15:44:41 -0500

    Readability tweaks for event/propagate

--- event/propagate/propagate.js
+++ event/propagate/propagate.js
@@ -1,39 +1,39 @@
-steal('can/event', function(event) {
+steal('can/event', function() {
 	// Adds propagation of events
 	// can.extend(Class.prototype, can.event, { __propagate: 'prop' })
-	var dispatch = event.dispatch;
-	event.dispatch = can.dispatch = function (ev, args) {
+	var dispatch = can.dispatch;
+	can.dispatch = can.event.dispatch = function (event, args) {
 		var propagate = (this.__bindEvents && this.__propagate) || false;
 
 		// Inject propagation into event, when applicable
-		if (propagate && typeof ev.isPropagationStopped === 'undefined') {
+		if (propagate && typeof event.isPropagationStopped === 'undefined') {
 			// Initialize the event into an object
-			if (typeof ev === 'string') {
-				ev = {
-					type: ev
+			if (typeof event === 'string') {
+				event = {
+					type: event
 				};
 			}
 
 			// Add extra event properties
 			// This could be done with can.simpleExtend, but this avoids extra logic execution
 			var stop = false;
-			ev.target = ev.target || this;
-			ev.stopPropagation = function() {
+			event.target = event.target || this;
+			event.stopPropagation = function() {
 				stop = true;
 			};
-			ev.isPropagationStopped = function() {
+			event.isPropagationStopped = function() {
 				return stop;
 			};
 		}
 
 		// Call original dispatch
-		dispatch.call(this, ev, args);
+		dispatch.call(this, event, args);
 
 		// Call propagated events
-		if (propagate && !ev.isPropagationStopped() && this[propagate]) {
-			can.dispatch.call(this[propagate], ev, args);
+		if (propagate && !event.isPropagationStopped() && this[propagate]) {
+			can.dispatch.call(this[propagate], event, args);
 		}
 	};
 
-	return event;
+	return can.event;
 });
