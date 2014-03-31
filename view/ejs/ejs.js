eab96d3f9733e874dbafcb952e05cf46ec57c7bd
Alexis Abril <me@alexisabril.com>
2014-03-31 10:02:10 -0500

    fixing ejs lint errors #843

--- view/ejs/ejs.js
+++ view/ejs/ejs.js
@@ -217,7 +217,7 @@ function (can) {
 	// The following helper converts a given string to upper case:
 	//
 	//     can.EJS.Helpers.prototype.toUpper = function(params) {
-	//     	return params.toUpperCase();
+	//       return params.toUpperCase();
 	//     }
 
 	// Use it like this in any EJS template:
@@ -227,9 +227,9 @@ function (can) {
 	// To access the current DOM element return a function that takes the element as a parameter:
 
 	//     can.EJS.Helpers.prototype.upperHtml = function(params) {
-	//     	return function(el) {
-	//     		$(el).html(params.toUpperCase());
-	//     	}
+	//       return function(el) {
+	//         $(el).html(params.toUpperCase());
+	//       }
 	//     }
 
 	// In your EJS view you can then call the helper on an element tag:
