3e6be11f665a7b0ce3c22eff6215194bb66a6cf7
David Luecke <daff@neyeon.com>
2014-04-01 16:20:35 -0600

    Fixing some tests.

--- util/fixture/fixture_test.js
+++ util/fixture/fixture_test.js
@@ -470,7 +470,7 @@ steal('can/util/fixture', 'can/model', 'can/test', function () {
 		
 		can.fixture('GET /presetStore', store.findAll);
 		stop();
-		$.get("/presetStore",{year: 2013, modelId:1}, function(response){
+		can.ajax({ url: "/presetStore", method: "get", data: {year: 2013, modelId:1} }).then(function(response){
 			
 			equal(response.data[0].id, 1, "got the first item");
 			equal(response.data.length, 1, "only got one item");
