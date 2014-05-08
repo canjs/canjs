f8bb3382bbd88d3e979a8f7934197c396b45b413
Brian Moschel <brian.moschel@gmail.com>
2014-05-08 01:20:28 -0500

    better warning text

--- component/component.js
+++ component/component.js
@@ -121,7 +121,8 @@ steal("can/util", "can/view/callbacks","can/control", "can/observe", "can/view/m
 					//!steal-remove-start
 					// user tried to pass something like id="{foo}", so give them a good warning
 					if(ignoreAttributesRegExp.test(name) && value[0] === "{" && value[value.length-1] === "}") {
-						can.dev.warn(name+" is not a supported attribute");
+						can.dev.warn("can/component: looks like you're trying to pass "+name+" as an attribute into a component, "+
+							"but it is not a supported attribute");
 					}
 					//!steal-remove-end
 
