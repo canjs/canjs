ca97d9cb974b0763e0af90f9fc6d5292d3f47c67
Matthew Phillips <matthew@matthewphillips.info>
2014-05-12 14:58:10 -0400

    Failing test for custom element namespace in IE8
    This adds a failing test to the issue where custom elements that have
    been registered with can.view.tag are being rendered in IE8 with a
    preceding `:` because they lose their namespace. This problem does not
    occur in Mustache.
    
    The issue is likely caused by stache using cloneNode internally. There
    are various issues with cloneNode that are solved by jQuery and HTML5
    Shiv includes a note in their readme that they do not address the
    cloneNode problem.

--- view/stache/stache_test.js
+++ view/stache/stache_test.js
@@ -3426,4 +3426,18 @@ steal("can/view/stache", "can/view","can/test","can/view/mustache/spec/specs",fu
 		equal(frag.childNodes[1].innerHTML, '', 'Context is set correctly for falsy values');
 		equal(frag.childNodes[2].innerHTML, '', 'Context is set correctly for falsy values');
 	})
+
+	test("Custom elements created with default namespace in IE8", function(){
+		// Calling can.view.tag so that this tag is shived
+		can.view.tag('my-tag', function(){});
+
+		var tmpl = "<my-tag></my-tag>";
+
+		var frag = can.stache(tmpl)({});
+		var qta = can.$('#qunit-test-area');
+		can.append(qta, frag);
+
+		equal(qta[0].innerHTML.toLowerCase(), "<my-tag></my-tag>", "Element created in default namespace");
+	});
+
 });
