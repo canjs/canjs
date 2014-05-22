c6465404e26e61c96963843c3067a0cb71f3f7ac
Justin Meyer <justinbmeyer@gmail.com>
2014-05-21 19:11:55 -0500

    don't install bower

--- view/ejs/system.js
+++ view/ejs/system.js
@@ -1,8 +1,6 @@
 import can from 'can/view/ejs/ejs';
 
-export
-
-function translate(load) {
+export function translate(load) {
 	return "define(['can/view/ejs/ejs'],function(can){" +
 		"return can.view.preloadStringRenderer('" + load.metadata.pluginArgument + "'," +
 		'can.EJS(function(_CONTEXT,_VIEW) { ' + new can.EJS({
