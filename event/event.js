e80eef170a3c210794b5620ee695e3517f60e300
Andy Kant <andy@andykant.com>
2014-06-06 12:34:09 -0500

    Fixed incorrect scope when using event's delegate/undelegate fallbacks (#1039)

--- event/event.js
+++ event/event.js
@@ -380,7 +380,7 @@ steal('can/util/can.js', function (can) {
 		 * This syntax can be used for objects that don't include the `can.event` mixin.
 		 */
 		delegate: function(selector, event, handler) {
-			return can.addEvent.call(event, handler);
+			return can.addEvent.call(this, event, handler);
 		},
 		/**
 		 * @function can.event.undelegate
@@ -403,7 +403,7 @@ steal('can/util/can.js', function (can) {
 		 * This syntax can be used for objects that don't include the `can.event` mixin.
 		 */
 		undelegate: function(selector, event, handler) {
-			return can.removeEvent.call(event, handler);
+			return can.removeEvent.call(this, event, handler);
 		},
 		/**
 		 * @function can.event.trigger
