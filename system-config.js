e425a326143ff823c357d6f91494867b097af81b
Justin Meyer <justinbmeyer@gmail.com>
2015-04-20 12:41:06 -0500

    adding system-config.js

---
+++ system-config.js
@@ -0,0 +1,17 @@
+"format cjs";
+
+var MySystem = require('@loader');
+
+if(MySystem.env === "development" && typeof window === "undefined") {
+	MySystem.config({
+		meta: {
+			'jquery@1.11.2#dist/jquery': {
+				"format": "global",
+				"exports": "jQuery",
+				"deps": ["can/util/vdom/vdom"]
+			}
+		}
+	});
+} 
+
+
