127450eeece7fda17748ad270c603e32fad61638
Matthew Phillips <matthew@matthewphillips.info>
2015-04-29 10:12:57 -0400

    Updating to use steal.import

--- util/vdom/test/node_test.js
+++ util/vdom/test/node_test.js
@@ -1,21 +1,15 @@
-var steal = require("steal");
+var steal = require("steal").clone();
 var path = require("path");
 
-var localSteal =  steal.clone( steal.addSteal( steal.System.clone() ) );
-
-localSteal.startup({
-	config: path.join(__dirname,"..","..", "..","package.json!npm"),
-	main: "@empty"
-}).then(function(){
-	
-		localSteal.System.import("can/util/vdom/test/main.stache!").then(function(renderer){
-			var res = renderer({});
-			var div = document.createElement("div");
-			div.appendChild(res);
-			console.log("got it!", div.innerHTML);
-		}, function(e){
-			console.log(e);
-		});
-
+steal.config({
+	config: path.join(__dirname,"..","..", "..","package.json!npm")
 });
 
+steal.import("can/util/vdom/test/main.stache!").then(function(renderer){
+	var res = renderer({});
+	var div = document.createElement("div");
+	div.appendChild(res);
+	console.log("got it!", div.innerHTML);
+}, function(e){
+	console.log(e);
+});
