485451463dec20aac77ddc1821118a5ec544f91f
Matthew Phillips <matthew@matthewphillips.info>
2015-05-02 11:23:51 -0400

    Set global.System when loading because of can.import

--- util/vdom/test/node_test.js
+++ util/vdom/test/node_test.js
@@ -2,9 +2,13 @@
 var steal = require("steal").clone();
 var path = require("path");
 
+global.System = steal.System;
+
+/*
 steal.config({
 	config: path.join(__dirname,"..","..", "..","package.json!npm")
 });
+*/
 
 steal.import("can/util/vdom/test/main.stache!").then(function(renderer){
 	var res = renderer({});
