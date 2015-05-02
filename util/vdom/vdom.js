2cc22809b1e72fc17d2a03662d8e497c839766f4
Matthew Phillips <matthew@matthewphillips.info>
2015-05-02 16:18:26 -0400

    Give vdom an outerHTML

--- util/vdom/vdom.js
+++ util/vdom/vdom.js
@@ -1,33 +1,37 @@
 // Everything CanJS+jquery app needs to run to pass
 // if you are doing almost everything with the can.util layer
 steal("can-simple-dom", "./make_parser", function(simpleDOM, makeParser){
-	
+
 	var document = new simpleDOM.Document();
 	var serializer = new simpleDOM.HTMLSerializer(simpleDOM.voidMap);
 	var parser = makeParser(document);
-	
+
 	if(Object.defineProperty) {
-		Object.defineProperty(simpleDOM.Element.prototype, "innerHTML", {
-			get: function(){
-				return serializer.serialize(this.firstChild);
-			},
-			set: function(html){
-				// remove all children
-				var cur = this.firstChild;
-				while(cur) {
-					this.removeChild(cur);
-					cur = this.firstChild;
+		var descriptor = function(outerHtml){
+			return {
+				get: function(){
+					return serializer.serialize(outerHtml ? this : this.firstChild);
+				},
+				set: function(html){
+					// remove all children
+					var cur = this.firstChild;
+					while(cur) {
+						this.removeChild(cur);
+						cur = this.firstChild;
+					}
+					if(""+html) {
+						var frag = parser.parse(""+html);
+						this.appendChild(frag);
+					}
 				}
-				if(""+html) {
-					var frag = parser.parse(""+html);
-					this.appendChild(frag);
-				}
-			}
-		});
+			};
+		};
+		Object.defineProperty(simpleDOM.Element.prototype, "innerHTML", descriptor());
+		Object.defineProperty(simpleDOM.Element.prototype, "outerHTML", descriptor(true));
 	}
 	global.document = document;
 	global.window = global;
 	global.addEventListener = function(){};
 	global.removeEventListener = function(){};
-	
+
 });
