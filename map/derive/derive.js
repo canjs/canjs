872789fce8c5ff980b4d0623c4e51c36987c6f3e
Chris Gomez <chris_gomez@bitovi.com>
2015-06-03 11:00:38 -0400

    Derive plugin - List > List

--- map/derive/derive.js
+++ map/derive/derive.js
@@ -1,104 +1,208 @@
 steal('can/compute', 'can/util', 'can/list', function () {
 	
-	var setup = can.Map.prototype.setup;
+	var mixin = function (proto, props) {
+		return can.extend({
+			_bindsetup: function () {
+				var self = this; 
 
-	var DerivedMap = can.Map.extend({},
-	{
-		_bindsetup: function () {
-			var self = this; 
+				this._indexKeys(can.Map.keys(this._source));
+				
+				this._source.bind('change', function (ev, attr, how) {
+					if (how === 'add') {
+						self._indexKeys([attr]);
+					} else if (how === 'remove') {
+						// TODO: Remove bindings
+					} 
+				});
+			},
+			_indexKeys: function (keys) {
+				var self = this;
+				var _meta = [];
 
-			this._indexKeys(this._source._getKeys());
-			
-			this._source.bind('change', function (ev, attr, how) {
-				if (how === 'add') {
-					self._indexKeys([attr]);
-				} else if (how === 'remove') {
-					// TODO: Remove bindings
+				can.each(keys, function (key) {
+					var meta = self._getMeta(key);
+
+					// Update the derived list when the key/value changes
+					meta.key.bind('change', function (ev, newKey, oldKey) {
+						// self._updateKey(meta, oldKey);
+						self._updateKeyAndLog(meta, oldKey);
+					});
+					meta.value.bind('change', function () {
+						// self._updateValue(meta);
+						self._updateValueAndLog(meta);
+					});
+					
+					// Manually set the initial values
+					// self._updateKey(meta);
+					self._updateKeyAndLog(meta);
+
+					self._saveMeta(meta);
+				});
+
+				return _meta;
+			},
+			_saveMeta: function (meta) {
+				this._meta[meta.sourceKey()] = meta;
+			},
+			_updateKeyAndLog: function (meta, oldKey) {
+				console.log('Update:', oldKey, '>', meta.key(), '=', meta.value());
+				if (meta.key() === meta.value()) {
 				}
-			});
+				this._updateKey(meta, oldKey);
+				console.log('Result:', this.attr());
+			},
+			_updateValueAndLog: function (meta) {
+				console.log('Set:', meta.key(), meta.value());
+				this._updateValue(meta);
+				console.log('Result:', this.attr());
+			},
+			_updateKey: function (meta, oldKey) {
+				var key = meta.key();
+				var value = meta.value();
+
+				if (!! oldKey || oldKey === 0) {
+					this.removeAttr(oldKey);
+				}
+				
+				// Only allow truthy keys to be set, or 0 (int)
+				if (!! key || key === 0) {
+					this.attr(key, value);
+				}
+			},
+			_updateValue: function (meta) {
+				var key = meta.key();
+				
+				if (!! key || key === 0) {
+					this.attr(meta.key(), meta.value());
+				}
+			},
+			_getMeta: function (key) {
+				var sourceKey = can.compute(key);
+
+				// Keep this out of the compute so that changes to the list
+				// don't force the compute to re-evalaute
+				
+				var derivedValue = can.compute(function () {
+					var i = sourceKey();
+					var item;
+
+					// TODO: Find a MUCH more elegant way to do this
+					if (this instanceof can.List) {
+						item = this._source[sourceKey()];
+					} else {
+						item = this._source.attr(sourceKey());
+					}
+
+					var result = this._valueFn(item, i);
+					return result;
+				}, this);
+				
+				var derivedKeyValue = can.compute(function () {
+					var i = sourceKey();
+					var mappedKey = this._keyFn(derivedValue(), i);
+					var derivedKey = this._deriveKey.call(this, mappedKey, i);
+					return derivedKey; 
+				}, this);
+
+				var meta = {
+					sourceKey: sourceKey,
+					key: derivedKeyValue,
+					value: derivedValue
+				};
+				return meta;
+			},
+			_deriveKey: function (mappedKey, sourceKey) {
+				if (typeof mappedKey !== 'boolean') {
+					// Convert to a string
+					return '' + mappedKey;
+				}
+
+				// Only allow setting booleans to `false` on Maps
+				return ! mappedKey ? undefined : sourceKey;
+			},
+			_getMetaObject: function () {
+				return {};
+			},
+			setup: function (source, keyFn, valueFn) {
+				this._source = source;
+				this._meta = this._getMetaObject(); 
+
+				// TODO: Figure out how to make these computes without 
+				// them being considered values in the Map/List. 
+				// Also, how can you change the function a map is based on?
+				this._keyFn = keyFn || function (item, key) { return key; };
+				this._valueFn = valueFn || function (item) { return item; };
+
+				return proto.setup.apply(this);
+			}
+		}, props);
+	};
+
+	var DerivedMap = can.Map.extend({}, mixin(can.Map.prototype));
+
+	var DerivedList = can.List.extend({}, mixin(can.List.prototype, {
+		_saveMeta: function (meta) {
+			this._meta.splice(meta.sourceKey(), 0, meta);
 		},
-		_indexKeys: function (keys) {
+		_deriveKey: function (mappedIndex, sourceIndex) {
+			if (typeof mappedIndex === 'boolean') {
+				return mappedIndex ? this._mapIndex.apply(this, [sourceIndex]) : undefined;
+			} else {
+				return +mappedIndex; // Convert to an int
+			}
+		},
+		_mapIndex: function (comparedSourceIndex) {
+			var self = this;
+			var mapping = {};
+			var maxPreIndex = 0;
+			var preItemMeta;
+			var key; 
+
+			can.each(this._meta, function (itemMeta) {
+				var itemSourceIndex = itemMeta.sourceKey();
+				var val = self._source.attr(itemSourceIndex);
+				if (typeof itemMeta.key() === 'undefined') {
+					return;
+				}
+
+				if (itemSourceIndex < comparedSourceIndex && itemSourceIndex > maxPreIndex) {
+					maxPreIndex = itemSourceIndex;
+				}
+
+				mapping[itemSourceIndex] = itemMeta.key();
+			});
+
+			var index = typeof mapping[maxPreIndex] !== 'undefined' ? mapping[maxPreIndex] + 1 : 0;
+			return index;
+		},
+		_updateKey: function (meta, oldVal) { 
+			var key = meta.key();
+			var value = meta.value(); 
+
+			if (oldVal) {
+				this.splice(oldVal, 1);
+			}
+			
+			if (typeof key !== 'undefined') {
+				this.splice(key, 0, value);
+			}
+		},
+		_bindsetup: function () {
 			var self = this;
 
-			can.each(keys, function (key) {
-				var meta = self._getMeta(key);
-
-				// Update the derived list when the key/value changes
-				meta.value.bind('change', self._makeValueChangeHandler(meta));
-				meta.key.bind('change', self._makeKeyChangeHandler(meta));
-				
-				// Manually set the initial values
-				self.attr(meta.key(), meta.value());
-			});
-		},
-		_makeKeyChangeHandler: function (meta) {
-			return can.proxy(function (ev, newVal, oldVal) {
-				this.removeAttr(oldVal);
-				this.attr(newVal, meta.value());
-			}, this);
-		},
-		_makeValueChangeHandler: function (meta) {
-			return can.proxy(function (ev, newVal) {
-				this.attr(meta.key(), newVal);
-			}, this);
-		},
-		_getMeta: function (key) {
-			var sourceKey = can.compute(key);
-			
-			var meta = {
-				sourceKey: sourceKey,
-				key: can.compute(function () {
-					var key = sourceKey();
-					var result = this._keyFn(this._source.attr(key), key);
-					return result; 
-				}, this),
-				value: can.compute(function () {
-					var key = sourceKey();
-					var result = this._valueFn(this._source.attr(key), key);
-					return result;
-				}, this),
-			};
-
-			return meta;
-		},
-		setup: function (source, keyFn, valueFn) {
-			this._source = source;
-			this._meta = []; 
-
-			// TODO: Figure out how to make these computes without 
-			// them being considered values in the Map/List. 
-			// Also, how can you change the function a map is based on?
-			this._keyFn = keyFn || function (item, key) { return key; };
-			this._valueFn = valueFn || function (item) { return item; };
-
-			return setup.apply(this);
-		}
-	});
-
-	var DerivedList = DerivedMap.extend({},
-	{
-		_bindsetup: function () {
-			var self = this; 
-
-			this._indexKeys(this._source._getKeys());
+			this._indexKeys(this._source.keys());
 			
 			this._source.bind('add', function (ev, newItems, index) {
-				self._indexKeys(this._getKeys.call(newItems));
+				self._indexKeys(this.keys.call(newItems, index));
 			});
 		},
-	});
+		_getMetaObject: function () {
+			return [];
+		},
+	}));
 
-	// Public API
+	// Public API changes
 	can.extend(can.Map.prototype, {
-		_getKeys: function () {
-			var keys = []; 
-
-			this.each(function (value, key) {
-				keys.push(key);
-			});
-			
-			return keys;
-		},
 		deriveMap: function (keyFn, valueFn) {
 			return new DerivedMap(this, keyFn, valueFn);
 		},
@@ -106,4 +210,17 @@ steal('can/compute', 'can/util', 'can/list', function () {
 			return new DerivedList(this, keyFn, valueFn);
 		}
 	}); 
+
+	can.extend(can.List.prototype, {
+		keys: function (offset) {
+			var keys = []; 
+			offset = typeof offset === 'number' ? offset : 0;
+
+			this.forEach(function (item, i) {
+				keys.push(i + offset);
+			});
+
+			return keys;
+		}
+	});
 });
--- map/derive/derive_test.js
+++ map/derive/derive_test.js
@@ -3,45 +3,79 @@ steal("can/map/derive", "can/test", "steal-qunit", function () {
 
 	QUnit.module('can/map/derive');
 
-	test('Map - mapKeys', function () {
-		var source = new can.Map({ 
-			a: 1, 
-			b: 2 
-		});
-
-		var derived = source.deriveMap(function(value, key) {
-			return key + value;
-		}); 
-		derived.bind('change', can.noop);
-		
-		// Initial
-		equal(derived.attr('a1'), 1, 'Initial properties derived');
-		equal(derived.attr('b2'), 2, 'Initial properties derived');
-
-		// Change
-		source.attr('a', 3);
-		equal(derived.attr('a3'), 3, 'Changed property derived');
-
-		// Add
-		source.attr('c', 4);
-		equal(derived.attr('c4'), 4, 'Added property derived');
-
-		// Remove
-		source.removeAttr('c');
-		equal(derived.attr('c4'), undefined, 'Removed property derived');
+test('Map - mapKeys', function () {
+	console.log('test: Map - mapKeys');
+	var source = new can.Map({ 
+		a: 1, 
+		b: 2 
 	});
 
-	test('List - compact', function () {
-		var source = new can.List([0, 1, false, 2, '', 3]);
+	var derived = source.deriveMap(function(value, key) {
+		return key + value;
+	}); 
+	derived.bind('change', can.noop);
+	
+	// Initial
+	console.log(derived.attr()); //-> {a1: 1, b2: 2}
+	equal(derived.attr('a1'), 1, 'Initial properties derived');
+	equal(derived.attr('b2'), 2, 'Initial properties derived');
 
-		var derived = source.deriveList(function (item, index) {
-			return !! item;
-		});
-		derived.bind('change', can.noop);
+	// Change
+	console.log('>>> Change');
+	source.attr('a', 3);
+	
+	console.log(derived.attr()); //-> {b2: 2, a3: 3}
+	equal(derived.attr('a3'), 3, 'Changed property derived');
 
-		derived.each(function (item) {
-			ok(item, 'Derived list contains truthy values only');
-		});
+	// Add
+	console.log('>>> Add');
+	source.attr('c', 4);
+
+	console.log(derived.attr()); //-> {b2: 2, a3: 3, c4: 4}
+	equal(derived.attr('c4'), 4, 'Added property derived');
+
+	// Remove
+	console.log('>>> Remove');
+	source.removeAttr('c');
+
+	console.log(derived.attr()); //-> {b2: 2, a3: 3}
+	equal(derived.attr('c4'), undefined, 'Removed property derived');
+});
+
+test('List - compact', function () {
+	console.log('test: List - compact');
+	var source = new can.List([0, 1, false, 2, '', 3]);
+
+	var derived = source.deriveList(function (item, index) {
+		return !! item;
+	});
+
+	derived.bind('change', can.noop);
+
+	// Initial
+	console.log(derived.attr()); //-> [1, 2, 3] 
+	derived.each(function (item) {
+		ok(item, 'Derived item is truthy');
 	})
+	equal(derived.attr('length'), 3, 'Correct number of items derived');
+
+	// Push
+	console.log('>>> Push');
+	source.push('pushed');
+	console.log(derived.attr()); //-> [1, 2, 3, "pushed"]
+	equal(derived.attr(3), 'pushed', 'Pushed truthy derived at correct index');
+
+	// Unshift
+	console.log('>>> Unshift');
+	source.unshift('unshifted');
+	console.log(derived.attr()); //-> ["unshifted", 1, 2, 3, "pushed"]
+	equal(derived.attr(0), 'unshifted', 'Unshifted truthy derived at correct index');
+
+	// TODO: Remove
+	// console.log('>>> Remove');
+	// source.splice(2, 1);
+	// console.log(derived.attr()); //-> ["unshifted", 1, 3, "pushed"]
+	// equal(derived.attr(2), 3, 'Spliced value removed');
+});
 
 });
