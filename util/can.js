b3c6df9adad34aac4c2825ad9c617af9e4c21940
Matthew Phillips <matthew@matthewphillips.info>
2015-07-30 20:40:07 -0400

    Implement ~ helper
    The ~ helper is used to create urls (for `<img>` for example) that are
    bound to the root of your application or relative to the current module.
    
    If the lookup is relative, such as:
    
    ```
    {{~ "../hello/world.svg"}}
    ```
    
    This will lookup the location relative to the current template module
    (if using Steal).
    
    Otherwise it will lookup the location through, in descending importance:
    
    * can.baseUrl
    * System.baseUrl (if using Steal/System)
    * location.pathname
    
    Fixes #1816
    
    Includes docs

--- util/can.js
+++ util/can.js
@@ -130,6 +130,49 @@ steal(function () {
 		}
 	};
 
+
+	var parseURI = function(url){
+			var m = String(url).replace(/^\s+|\s+$/g, '').match(/^([^:\/?#]+:)?(\/\/(?:[^:@]*(?::[^:@]*)?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/);
+				// authority = '//' + user + ':' + pass '@' + hostname + ':' port
+			return (m ? {
+				href     : m[0] || '',
+				protocol : m[1] || '',
+				authority: m[2] || '',
+				host     : m[3] || '',
+				hostname : m[4] || '',
+				port     : m[5] || '',
+				pathname : m[6] || '',
+				search   : m[7] || '',
+				hash     : m[8] || ''
+			} : null);
+		};
+
+	can.joinURIs = function(base, href) {
+		function removeDotSegments(input) {
+			var output = [];
+			input.replace(/^(\.\.?(\/|$))+/, '')
+				.replace(/\/(\.(\/|$))+/g, '/')
+				.replace(/\/\.\.$/, '/../')
+				.replace(/\/?[^\/]*/g, function (p) {
+					if (p === '/..') {
+						output.pop();
+					} else {
+						output.push(p);
+					}
+				});
+			return output.join('').replace(/^\//, input.charAt(0) === '/' ? '/' : '');
+		}
+
+		href = parseURI(href || '');
+		base = parseURI(base || '');
+
+		return !href || !base ? null : (href.protocol || base.protocol) +
+			(href.protocol || href.authority ? href.authority : base.authority) +
+			removeDotSegments(href.protocol || href.authority || href.pathname.charAt(0) === '/' ? href.pathname : (href.pathname ? ((base.authority && !base.pathname ? '/' : '') + base.pathname.slice(0, base.pathname.lastIndexOf('/') + 1) + href.pathname) : base.pathname)) +
+				(href.protocol || href.authority || href.pathname ? href.search : (href.search || base.search)) +
+				href.hash;
+	};
+
 	can["import"] = function(moduleName, parentName) {
 		var deferred = new can.Deferred();
 
