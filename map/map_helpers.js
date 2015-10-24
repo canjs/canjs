6582f17c8119e4d17cf113191e61264fa1885099
jisutton <jisutton@adobe.com>
2015-10-23 17:59:47 -0700

    maybe I'll get it right one of these commits

--- map/map_helpers.js
+++ map/map_helpers.js
@@ -3,7 +3,7 @@
 // a map, or track the maps created from plain JavaScript objects.
 // `can.Map` handles cycles in objects nicely!
 
-steal('can/util', 'util/object/isplain', function(can){
+steal('can/util', 'can/util/object/isplain', function(can){
 	
 	var mapHelpers = {
 		// ### mapHelpers.attrParts
