edf9c03dcbab5bda1238a371d16c56e9a6aa16e6
David Luecke <daff@neyeon.com>
2016-09-27 09:14:25 -0700

    Enable Safari 9 on Saucelabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function (grunt) {
+<<<<<<< e44290c78b45a021321beecba1186590e1fb2e7d
   var browsers = [
 	  {
 		  browserName: 'firefox',
@@ -16,6 +17,24 @@ module.exports = function (grunt) {
 		  platform: 'Windows 8',
 		  version: '10.0'
 		}
+=======
+  var browsers = [{
+	    browserName: 'firefox',
+	    platform: 'Windows 10',
+	    version: '48.0'
+	  }, {
+	    browserName: 'googlechrome',
+	    platform: 'windows'
+	  }, {
+	    browserName: 'safari',
+	    platform: 'OS X 10.11',
+	    version: '9.0'
+	  }, {
+	    browserName: 'internet explorer',
+	    platform: 'Windows 8',
+	    version: '10.0'
+	  }
+>>>>>>> Enable Safari 9 on Saucelabs
   ];
   
   grunt.initConfig({
