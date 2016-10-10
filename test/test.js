dd305c1382c4e3c5af9691f42d3a1ca0e73102a7
Kevin Phillips <kphillips86@gmail.com>
2016-10-10 09:53:09 -0500

    reorganizing tests by core/legacy/infrastructure

--- test/test.js
+++ test/test.js
@@ -1,19 +1,35 @@
-require('../construct/construct_test');
+// Core tests
 require('../component/component_test');
 require('../compute/compute_test');
-require('../control/control_test');
-require('../event/event_test');
-require('../list/list_test');
-require('../map/map_test');
+// require('can-connect/test');
+require('../construct/construct_test');
+// require('can-define/test');
 // require('../route/route_test');
 // require('../route/pushstate/pushstate_test');
+// require('can-set/src/test');
+require('../view/stache/stache_test');
+
+
+// Infrastructure tests
+require('../event/event_test');
+// require('can-observation/can-observation_test');
 require('../util/util_test');
-// require('../view/autorender/autorender_test');
+// require('can-view-callbacks/test/callbacks-test');
 // require('../view/import/import_test');
 require('../view/live/live_test');
+// require('can-view-model/can-view-model-test');
 require('../view/node_lists/node_lists_test');
 require('../view/parser/parser_test');
 require('../view/scope/scope_test');
-require('../view/stache/stache_test');
+// require('../view/autorender/autorender_test');
 
+
+// Legacy tests
+require('../control/control_test');
+require('../list/list_test');
+require('../map/map_test');
+// require('can-map-define/test/test');
+
+
+// Integration tests
 require('../docs/can-guides/experiment/todomvc/test');
