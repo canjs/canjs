dca2be90c87ee0aca623ad08d2a262bdaaad8348
Chasen Le Hara <chasen@chasenlehara.com>
2016-12-05 16:41:33 -0800

    Remove the old Place My Order guide

--- docs/can-guides/examples/PlaceMyOrder/chapter_8/models/restaurant.js
+++
@@ -1,6 +0,0 @@
-var Restaurant = can.Model.extend({
-	findAll: 'GET /api/restaurants',
-	findOne: 'GET /api/restaurants/:id'
-}, {
-	// Include second, empty parameter object to set instanceProperties
-});
