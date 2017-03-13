bee7a2b3d877d0ce1ad0942800b5e795282e2e17
Kevin Phillips <kevin@bitovi.com>
2017-03-13 10:04:03 -0500

    removing can-connect-feathers tests

--- test/test.js
+++ test/test.js
@@ -54,7 +54,6 @@ require('../view/ejs/ejs_test');
 
 // Ecosystem tests
 require('can-fixture/test/fixture_test');
-require('can-connect-feathers/test/test');
 // require('can-jquery/test/test');
 // require('can-vdom/test/test');
 // require('can-zone/test/test');
