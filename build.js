1b4c2a61763f3d29a6371e564dc6b38730ca1ee3
Kevin Phillips <kevin@bitovi.com>
2017-03-14 10:37:25 -0500

    fixing build with can-connect-signalr

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ var globalJS = require("steal-tools/lib/build/helpers/global").js;
 var baseNormalize = globalJS.normalize();
 var ignoreModules = [function(name){
     if(name.indexOf("jquery") === 0 || name.indexOf("kefir") === 0 || 
-			name.indexOf('validate.js') === 0 || name.indexOf('ms-signalr-client')) {
+			name.indexOf('validate.js') === 0 || name.indexOf('ms-signalr-client') === 0) {
         return true;
     } else {
         return false;
