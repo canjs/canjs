4e424a65e549b7a8ebe3a31ffaefa7346409107e
Obaid Ahmed <obaid@bitovi.com>
2017-03-15 12:10:49 -0400

    Disabling some core tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 // Core tests
 // require('../component/component_test');
 // require('../compute/compute_test');
-// require('can-connect/test/test');
-// require('../construct/construct_test');
+require('can-connect/test/test');
+require('../construct/construct_test');
 require('can-construct-super/test/can-construct-super_test');
 require('can-define/test/test');
 // require('../route/route_test');
