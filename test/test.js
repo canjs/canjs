f99b70f36a57b3ffe84c4cadf8c4a69690a348fb
Obaid Ahmed <obaid@bitovi.com>
2017-03-15 12:46:54 -0400

    disabling component, compute and can-connect tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 // Core tests
-require('../component/component_test');
-require('../compute/compute_test');
+// require('../component/component_test');
+// require('../compute/compute_test');
 // require('can-connect/test/test');
 require('../construct/construct_test');
 require('can-construct-super/test/can-construct-super_test');
