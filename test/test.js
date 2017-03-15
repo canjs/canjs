f50632b6eedbe7a18810918ae563fb7727475433
Obaid Ahmed <obaid@bitovi.com>
2017-03-15 11:56:00 -0400

    Disabling some core tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 // Core tests
-require('../component/component_test');
-require('../compute/compute_test');
-require('can-connect/test/test');
-require('../construct/construct_test');
-require('can-construct-super/test/can-construct-super_test');
-require('can-define/test/test');
-// require('../route/route_test');
-// require('../route/pushstate/pushstate_test');
-require('can-set/test/test');
-require('../view/stache/stache_test');
-require('can-stache-converters/test/test');
+// require('../component/component_test');
+// require('../compute/compute_test');
+// require('can-connect/test/test');
+// require('../construct/construct_test');
+// require('can-construct-super/test/can-construct-super_test');
+// require('can-define/test/test');
+// // require('../route/route_test');
+// // require('../route/pushstate/pushstate_test');
+// require('can-set/test/test');
+// require('../view/stache/stache_test');
+// require('can-stache-converters/test/test');
 
 
 // Infrastructure tests
@@ -28,37 +28,37 @@ require('../view/node_lists/node_lists_test');
 require('../view/parser/parser_test');
 require('../view/scope/scope_test');
 require('../view/autorender/autorender_test');
-// if (!System.isEnv('production')) {
-// 	System.import('can-simple-map/can-simple-map_test');
-// }
-// require('can-view-target/test/test');
-// require('can-simple-dom/test/test');
-// if (!System.isEnv('production')) {
-// 	System.import('can-cid/test/test');
-// }
-// if (!System.isEnv('production')) {
-// 	System.import('can-types/test/test');
-// }
-//
-//
-//
-// // Legacy tests
-// require('../control/control_test');
-// require('../list/list_test');
-// require('../map/map_test');
-// require('can-map-define/can-map-define_test');
-// require('can-view-href/test/test');
-// require('can-map-backup/can-map-backup_test');
-// require('can-validate-legacy/can-validate-test');
-// require('../view/ejs/ejs_test');
-//
-// // Ecosystem tests
-// require('can-fixture/test/fixture_test');
-// // require('can-jquery/test/test');
-// // require('can-vdom/test/test');
-// // require('can-zone/test/test');
-//
-//
-// // Integration tests
+if (!System.isEnv('production')) {
+	System.import('can-simple-map/can-simple-map_test');
+}
+require('can-view-target/test/test');
+require('can-simple-dom/test/test');
+if (!System.isEnv('production')) {
+	System.import('can-cid/test/test');
+}
+if (!System.isEnv('production')) {
+	System.import('can-types/test/test');
+}
+
+
+
+// Legacy tests
+require('../control/control_test');
+require('../list/list_test');
+require('../map/map_test');
+require('can-map-define/can-map-define_test');
+require('can-view-href/test/test');
+require('can-map-backup/can-map-backup_test');
+require('can-validate-legacy/can-validate-test');
+require('../view/ejs/ejs_test');
+
+// Ecosystem tests
+require('can-fixture/test/fixture_test');
+// require('can-jquery/test/test');
+// require('can-vdom/test/test');
+// require('can-zone/test/test');
+
+
+// Integration tests
 require('../docs/can-guides/experiment/todomvc/test');
 require('./integration/all/test');
