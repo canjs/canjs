1c1aef962579269d4f5697413240100aaeb98afb
Obaid Ahmed <obaid@bitovi.com>
2017-03-15 11:34:55 -0400

    Disabling half of the tests to see if they still pass on IE

--- test/test.js
+++ test/test.js
@@ -28,37 +28,37 @@ require('../view/node_lists/node_lists_test');
 require('../view/parser/parser_test');
 require('../view/scope/scope_test');
 require('../view/autorender/autorender_test');
-if (!System.isEnv('production')) {
-	System.import('can-simple-map/can-simple-map_test');
-}
-require('can-view-target/test/test');
-require('can-simple-dom/test/test');
-if (!System.isEnv('production')) {
-	System.import('can-cid/test/test');
-}
-if (!System.isEnv('production')) {
-	System.import('can-types/test/test');
-}
-
-
-
-// Legacy tests
-require('../control/control_test');
-require('../list/list_test');
-require('../map/map_test');
-require('can-map-define/can-map-define_test');
-require('can-view-href/test/test');
-require('can-map-backup/can-map-backup_test');
-require('can-validate-legacy/can-validate-test');
-require('../view/ejs/ejs_test');
-
-// Ecosystem tests
-require('can-fixture/test/fixture_test');
-// require('can-jquery/test/test');
-// require('can-vdom/test/test');
-// require('can-zone/test/test');
-
-
-// Integration tests
-// require('../docs/can-guides/experiment/todomvc/test');
-// require('./integration/all/test');
+// if (!System.isEnv('production')) {
+// 	System.import('can-simple-map/can-simple-map_test');
+// }
+// require('can-view-target/test/test');
+// require('can-simple-dom/test/test');
+// if (!System.isEnv('production')) {
+// 	System.import('can-cid/test/test');
+// }
+// if (!System.isEnv('production')) {
+// 	System.import('can-types/test/test');
+// }
+//
+//
+//
+// // Legacy tests
+// require('../control/control_test');
+// require('../list/list_test');
+// require('../map/map_test');
+// require('can-map-define/can-map-define_test');
+// require('can-view-href/test/test');
+// require('can-map-backup/can-map-backup_test');
+// require('can-validate-legacy/can-validate-test');
+// require('../view/ejs/ejs_test');
+//
+// // Ecosystem tests
+// require('can-fixture/test/fixture_test');
+// // require('can-jquery/test/test');
+// // require('can-vdom/test/test');
+// // require('can-zone/test/test');
+//
+//
+// // Integration tests
+require('../docs/can-guides/experiment/todomvc/test');
+require('./integration/all/test');
