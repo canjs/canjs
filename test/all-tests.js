670c0d4ef0a3ff1b6a9b16ea4a92f0cde2ced974
jlau <jlau@control-tec.com>
2017-05-31 13:56:04 -0400

    More space to tab conversion

--- test/all-tests.js
+++ test/all-tests.js
@@ -34,4 +34,4 @@ steal("can/test/test.js",
 		"can/view/scope/scope_test.js",
 		"can/util/string/string_test.js",
 		"can/util/attr/attr_test.js",
-    "can/util/batch/batch_test.js", function() {});
+		"can/util/batch/batch_test.js", function() {});
