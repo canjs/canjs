ab0193b35fa627a84bbe8243180d85a546fa2ce8
John Gardner <john@ardea.io>
2017-08-10 17:17:03 -0500

    Should run more than two tests

---
+++ test/delete-this-test.js
@@ -0,0 +1,11 @@
+var QUnit = require('steal-qunit');
+
+QUnit.module("IE9 debugging");
+
+test('Simple test for IE9 debugging', function() {
+	QUnit.ok(true, "test ran");
+});
+
+test('Test number two', function() {
+	QUnit.ok(true, "Second test ran");
+});
--- test/test-sauce-labs.js
+++ test/test-sauce-labs.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 6 */
-'use strict';
+// 'use strict';
 
 var testSauceLabs = require('test-saucelabs');
 
@@ -8,40 +8,12 @@ var platforms = [{
 	browserName: 'internet explorer',
 	platform: 'Windows 7',
 	version: '9'
-}, {
-	browserName: 'internet explorer',
-	platform: 'Windows 8',
-	version: '10.0'
-}, {
-	browserName: 'internet explorer',
-	platform: 'Windows 10',
-	version: '11.0'
-}, {
-	browserName: 'safari',
-	platform: 'OS X 10.12',
-	version: 'latest'
-}, {
-	browserName: 'MicrosoftEdge',
-	platform: 'Windows 10'
-}, {
-	browserName: 'firefox',
-	platform: 'Windows 10',
-	version: 'latest'
-}, {
-	browserName: 'googlechrome',
-	platform: 'Windows 10',
-	version: 'latest'
-}, {
-	browserName: 'Safari',
-	'appium-version': '1.6.3',
-	platformName: 'iOS',
-	platformVersion: '10.0',
-	deviceName: 'iPhone 7 Simulator'
 }];
 
 var url = 'http://localhost:3000/test/index.html?hidepassed';
 
 testSauceLabs({
 	urls: [{ name: "canjs", url : url }],
-	platforms: platforms
+	platforms: platforms,
+	zeroAssertionsPass: false
 });
