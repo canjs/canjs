eb7da91fef72d05193a605f6f53ed2f25b03132c
Kevin Phillips <kevin@bitovi.com>
2017-09-27 10:52:38 -0500

    moving attribute-encoder tests

--- test/test-production.js
+++ test/test-production.js
@@ -29,7 +29,6 @@ require('can-simple-observable/can-simple-observable-test');
 require('can-symbol/can-symbol-test');
 require('can-reflect/can-reflect-test');
 require('can-reflect-promise/test/can-reflect-promise_test');
-require('can-attribute-encoder/can-attribute-encoder-test');
 require('can-ajax/can-ajax-test');
 
 
