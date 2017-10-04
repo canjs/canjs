985ddf292e2896e199a9def98e326bea361fe4a3
Kevin Phillips <kevin@bitovi.com>
2017-10-04 09:39:08 -0500

    map-define

--- test/test.js
+++ test/test.js
@@ -36,8 +36,8 @@
 // Legacy tests
 //require('../control/control_test');
 //require('../list/list_test');
-require('../map/map_test');
-//require('can-map-define/can-map-define_test');
+//require('../map/map_test');
+require('can-map-define/can-map-define_test');
 //require('can-view-href/test/test');
 //require('can-map-backup/can-map-backup_test');
 //require('can-validate-legacy/can-validate-test');
