3fc9e9310657af4c19d1bde58893da0979fbfe44
Kevin Phillips <kevin@bitovi.com>
2017-10-04 09:39:30 -0500

    map-backup

--- test/test.js
+++ test/test.js
@@ -38,8 +38,8 @@
 //require('../list/list_test');
 //require('../map/map_test');
 //require('can-map-define/can-map-define_test');
-require('can-view-href/test/test');
-//require('can-map-backup/can-map-backup_test');
+//require('can-view-href/test/test');
+require('can-map-backup/can-map-backup_test');
 //require('can-validate-legacy/can-validate-test');
 //require('../view/ejs/ejs_test');
 
