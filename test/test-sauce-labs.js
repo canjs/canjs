ab1fd213689944954aa38700ffe76ff96cff4c2c
Kevin Phillips <kevin@bitovi.com>
2017-10-06 13:20:34 -0500

    trying iphone 8

--- test/test-sauce-labs.js
+++ test/test-sauce-labs.js
@@ -6,10 +6,10 @@ var testSauceLabs = require('test-saucelabs');
 // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
 var platforms = [{
 	browserName: 'Safari',
-	'appium-version': '1.6.3',
+	'appium-version': '1.7.1',
 	platformName: 'iOS',
-	platformVersion: '10.0',
-	deviceName: 'iPhone 7 Simulator'
+	platformVersion: '11.0',
+	deviceName: 'iPhone 8 Simulator'
 }];
 
 var url = 'http://canjs.test:3000/test/index.html?hidepassed';
