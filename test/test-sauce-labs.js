db5a3bab7d04a0998e8b38c7f56442173cd83206
Kevin Phillips <kevin@bitovi.com>
2017-10-06 13:12:53 -0500

    ios

--- test/test-sauce-labs.js
+++ test/test-sauce-labs.js
@@ -5,9 +5,11 @@ var testSauceLabs = require('test-saucelabs');
 
 // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
 var platforms = [{
-	browserName: 'safari',
-	platform: 'OS X 10.12',
-	version: 'latest'
+	browserName: 'Safari',
+	'appium-version': '1.6.3',
+	platformName: 'iOS',
+	platformVersion: '10.0',
+	deviceName: 'iPhone 7 Simulator'
 }];
 
 var url = 'http://canjs.test:3000/test/index.html?hidepassed';
