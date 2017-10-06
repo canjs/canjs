da699b78463ec5397e020401f7bc42983b08e0d2
Kevin Phillips <kevin@bitovi.com>
2017-10-06 09:37:09 -0500

    testing

--- test/test-sauce-labs.js
+++ test/test-sauce-labs.js
@@ -5,38 +5,9 @@ var testSauceLabs = require('test-saucelabs');
 
 // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
 var platforms = [{
-	browserName: 'internet explorer',
-	platform: 'Windows 7',
-	version: '9'
-}, {
-	browserName: 'internet explorer',
-	platform: 'Windows 8',
-	version: '10.0'
-}, {
-	browserName: 'internet explorer',
-	platform: 'Windows 10',
-	version: '11.0'
-}, {
 	browserName: 'safari',
 	platform: 'OS X 10.12',
 	version: 'latest'
-}, {
-	browserName: 'MicrosoftEdge',
-	platform: 'Windows 10'
-}, {
-	browserName: 'firefox',
-	platform: 'Windows 10',
-	version: 'latest'
-}, {
-	browserName: 'googlechrome',
-	platform: 'Windows 10',
-	version: 'latest'
-}, {
-	browserName: 'Safari',
-	'appium-version': '1.6.3',
-	platformName: 'iOS',
-	platformVersion: '10.0',
-	deviceName: 'iPhone 7 Simulator'
 }];
 
 var url = 'http://canjs.test:3000/test/index.html?hidepassed';
