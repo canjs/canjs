2311de3bce84504f5cedddf63ba145bc58f5fd99
Chasen Le Hara <chasen@chasenlehara.com>
2017-11-30 12:52:30 -0800

    Add can-cid tests

--- test/test-dev-only.js
+++ test/test-dev-only.js
@@ -11,3 +11,4 @@ require('can-attribute-encoder/can-attribute-encoder-test');
 require('can-observation/can-observation_test');
 require('can-view-callbacks/test/callbacks-test');
 require('can-simple-map/can-simple-map_test');
+require('can-cid/tests');
