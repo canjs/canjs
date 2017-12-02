11b9d8133ebff41b5df57623a61a70b03e530c1b
Chasen Le Hara <chasen@chasenlehara.com>
2017-12-01 16:42:48 -0800

    Uncomment-out almost everything in can.js
    can-stache/helpers/route is the one exception.

--- can.js
+++ can.js
@@ -2,24 +2,24 @@ var can = require("can-util/namespace");
 
 require("can-component");
 require("can-compute");
-//require("can-connect/all");
+require("can-connect/all");
 require("can-define/map/map");
 require("can-define/list/list");
-//require("can-route");
-//require("can-set");
-//require("can-simple-observable");
+require("can-route");
+require("can-set");
+require("can-simple-observable");
 require("can-stache");
-//require("can-stache/helpers/route");
+// require("can-stache/helpers/route");
 require("can-stache-bindings");
-//require("can-attribute-encoder");
-//require("can-ajax");
-//require("can-globals");
-//require("can-reflect");
-//require("can-define-lazy-value");
+require("can-attribute-encoder");
+require("can-ajax");
+require("can-globals");
+require("can-reflect");
+require("can-define-lazy-value");
 
 require("can-debug");
 
-//require("can-assign");
+require("can-assign");
 require("can-dom-events");
 require("can-event-dom-enter");
 require("can-event-dom-radiochange");
