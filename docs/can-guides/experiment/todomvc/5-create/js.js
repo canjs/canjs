b9d30f7a23b4f113e7f016382bec1a4ebe2e58aa
Kevin Phillips <kevin@bitovi.com>
2018-02-01 14:38:30 -0600

    using domEventEnter in todomvc demo and guide

--- docs/can-guides/experiment/todomvc/5-create/js.js
+++ docs/can-guides/experiment/todomvc/5-create/js.js
@@ -39,6 +39,8 @@ can.connect.superMap({
   algebra: todoAlgebra
 });
 
+can.domEvents.addEvent( can.domEventEnter );
+
 var TodoCreateVM = can.DefineMap.extend({
     todo: {Default: Todo},
     createTodo: function(){
