6ec96441f12b41222b2941cb20cdfb9a39617536
Justin Meyer <justinbmeyer@gmail.com>
2018-05-29 20:55:52 -0500

    fixing a few deps

--- core.js
+++ core.js
@@ -1,8 +1,16 @@
 var can = require("can-namespace");
 
+require("can-define/map/map");
+require("can-define/list/list");
 
 require("can-component");
 
+require("can-stache");
+require("can-stache-route-helpers");
+require("can-stache-bindings");
 
+require("can-rest-model");
+
+require("can-route");
 
 module.exports = can;
