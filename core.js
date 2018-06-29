3ac2af11ad20e234a1abbb573c391ad98b17ceac
Matthew Phillips <matthew@matthewphillips.info>
2018-06-29 14:00:27 -0400

    Make everything in infrastructure available from core
    Closes #4230

--- core.js
+++ core.js
@@ -14,3 +14,33 @@ export { default as stacheBindings } from "./es/can-stache-bindings";
 
 export { default as stacheRouteHelpers } from "./es/can-stache-route-helpers";
 export { default as route } from "./es/can-route";
+
+// Infrastruture (gets loaded with core)
+// observable
+export { default as SimpleObservable } from "./es/can-simple-observable";
+// utility
+export { default as assign } from "./es/can-assign";
+export { default as attributeEncoder } from "./es/can-attribute-encoder";
+export { default as ajax } from "./es/can-ajax";
+export { default as globals } from "./es/can-globals";
+export { default as Reflect } from "./es/can-reflect";
+export { default as defineLazyValue } from "./es/can-define-lazy-value";
+export { default as domEvents } from "./es/can-dom-events";
+export { default as radioChangeEvent } from "./es/can-event-dom-radiochange";
+export { default as enterEvent } from "./es/can-event-dom-enter";
+export { default as makeInterfaceValidator } from "./es/can-validate-interface";
+export { default as viewModel } from "./es/can-view-model";
+export { default as Observation } from "./es/can-observation";
+export { default as key } from "./es/can-key";
+export {default as diff} from "./es/can-diff";
+export {default as parseURI} from "./es/can-parse-uri";
+export {default as stringToAny} from "./es/can-string-to-any";
+
+export { default as viewCallbacks } from "./es/can-view-callbacks";
+export { default as queues } from "./es/can-queues";
+// data
+export { default as connect } from "./es/can-connect";
+export { default as memoryStore } from "./es/can-memory-store";
+export { default as localStore } from "./es/can-local-store";
+// Typed data
+export { MaybeBoolean, MaybeDate, MaybeNumber, MaybeString } from "./es/can-data-types";
