d4c26c35bac17893b472162c22229b9ac13d5ebd
Chasen Le Hara <git@chasenlehara.com>
2018-11-19 21:10:14 -0800

    Update the “Signup and Login” guide for CanJS 5
    I took some liberties in updating this guide. This includes:
    
    - Switching from showing `can.stache.from` to using a real component
    - Using the new simple setter syntax (e.g. `on:click="this.page = 'login'"`)
    - Using `this.` throughout the template
    - Completely moving the template to a JS string literal; now the HTML only has the custom element
    - Moving the fixture setup into the JS

--- docs/can-guides/commitment/recipes/signup-simple/1-setup.js
+++ docs/can-guides/commitment/recipes/signup-simple/1-setup.js
@@ -1,10 +1,85 @@
-const AppViewModel = can.DefineMap.extend({
+import { ajax, Component, fixture } from "//unpkg.com/can@5/core.mjs";
 
+fixture("GET /api/session", function(req, response) {
+	const session = localStorage.getItem("session");
+	if (localStorage.getItem("session")) {
+		response(JSON.parse(session));
+	} else {
+		response(404, { message: "No session" }, {}, "unauthorized");
+	}
+});
+fixture("POST /api/users", function(request, response) {
+	const session = {
+		user: { email: request.data.email }
+	};
+	localStorage.setItem("user", JSON.stringify(request.data));
+	localStorage.setItem("session", JSON.stringify(session));
+
+	return session.user;
+});
+fixture("DELETE /api/session", function() {
+	localStorage.removeItem("session");
+	return {};
 });
 
-const viewModel = new AppViewModel({});
+fixture("POST /api/session", function(request, response) {
+	const userData = localStorage.getItem("user");
+	if (userData) {
+		const user = JSON.parse(userData);
+		const requestUser = request.data.user;
+		if (
+			user.email === requestUser.email &&
+			user.password === requestUser.password
+		) {
+			return request.data;
+		} else {
+			response(401, { message: "Unauthorized" }, {}, "unauthorized");
+		}
+	}
+	response(401, { message: "Unauthorized" }, {}, "unauthorized");
+});
 
-const view = can.stache.from("app-view");
-const fragment = view(viewModel);
+Component.extend({
+	tag: "signup-login",
+	view: `
+		<p class="welcome-message">
+		  Welcome Someone.
+		  <a href="javascript://">Log out</a>
+		</p>
 
-document.body.appendChild(fragment);
+		<form>
+		  <h2>Sign Up</h2>
+
+		  <input placeholder="email" />
+
+		  <input type="password"
+		       placeholder="password" />
+
+		  <button>Sign Up</button>
+
+		  <aside>
+		    Have an account?
+		    <a href="javascript://">Log in</a>
+		  </aside>
+		</form>
+
+		<form>
+		  <h2>Log In</h2>
+
+		  <input placeholder="email" />
+
+		  <input type="password"
+		     placeholder="password" />
+
+		  <button>Log In</button>
+
+		  <div class="error">error message</div>
+
+		  <aside>
+		    Don’t have an account?
+		    <a href="javascript://">Sign up</a>
+		  </aside>
+		</form>
+	`,
+	ViewModel: {}
+});
