7cdae31c4e6797616df0c7ab359edea3774e43ef
Mohamed Cherif Bouchelaghem <cherif.bouchelaghem@gmail.com>
2019-03-15 12:30:19 -0400

    Update the “Canvas Clock (Simple)” guide for CanJS 5
    Closes https://github.com/canjs/canjs/issues/4621

--- docs/can-guides/commitment/recipes/canvas-clock/1-setup.js
+++ docs/can-guides/commitment/recipes/canvas-clock/1-setup.js
@@ -1,21 +1,18 @@
-can.Component.extend({
+import { Component } from "//unpkg.com/can@5/core.mjs";
+
+Component.extend({
   tag: "clock-controls",
   ViewModel: {
-    time: {
-      value({ resolve }) {
-        const intervalID = setInterval(() => {
-          resolve( new Date() );
-        }, 1000);
-
-        resolve( new Date() );
-
-        return () => clearInterval(intervalID);
-      }
+    time: {Default: Date, Type: Date},
+    connectedCallback() {
+      setInterval(() => {
+        this.time = new Date();
+      }, 1000);
     }
   },
   view: `
-    <p>{{ time }}</p>
-    <digital-clock time:from="time"/>
-    <analog-clock time:from="time"/>
+    <p>{{ this.time }}</p>
+    <digital-clock time:from="this.time"/>
+    <analog-clock time:from="this.time"/>
   `
 });
