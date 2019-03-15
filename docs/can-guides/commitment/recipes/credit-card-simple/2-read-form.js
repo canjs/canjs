2c025ddf76e7935de8fa4a7c6a91071c0016f8fb
Mohamed Cherif Bouchelaghem <cherif.bouchelaghem@gmail.com>
2019-03-15 13:14:06 -0400

    Update the “Credit Card Guide (Simple)” guide for CanJS 5
    Closes https://github.com/canjs/canjs/issues/4623

--- docs/can-guides/commitment/recipes/credit-card-simple/2-read-form.js
+++ docs/can-guides/commitment/recipes/credit-card-simple/2-read-form.js
@@ -1,17 +1,36 @@
+import { Component } from "//unpkg.com/can@5/core.mjs";
+
 Stripe.setPublishableKey("pk_test_zCC2JrO3KSMeh7BB5x9OUe2U");
 
-const PaymentVM = can.DefineMap.extend({
-  amount: { default: 9.99 },
+Component.extend({
+	tag: 'cc-payment',
+	view: `
+		<form>
 
-  userCardNumber: "string",
+			<input type="text" name="number" placeholder="Card Number"
+				value:bind="this.userCardNumber"/>
 
-  userExpiry: "string",
+			<input type="text" name="expiry" placeholder="MM-YY"
+				value:bind="this.userExpiry"/>
 
-  userCVC: "string"
+			<input type="text" name="cvc" placeholder="CVC"
+				value:bind="this.userCVC"/>
+
+			<button>Pay $\{{ this.amount }}</button>
+
+			<p>{{ this.userCardNumber }}, {{ this.userExpiry }}, {{ this.userCVC }}</p>
+			
+		</form>
+	`,
+	ViewModel: {
+		amount: {
+			default: 9.99
+		},
+
+		userCardNumber: "string",
+
+		userExpiry: "string",
+
+		userCVC: "string"
+	}
 });
-
-const viewModel = new PaymentVM();
-
-const paymentView = can.stache.from("payment-view");
-const fragment = paymentView( viewModel );
-document.body.appendChild( fragment );
--- docs/can-guides/commitment/recipes/credit-card-simple/3-format.js
+++ docs/can-guides/commitment/recipes/credit-card-simple/3-format.js
@@ -1,37 +1,57 @@
+import { Component } from "//unpkg.com/can@5/core.mjs";
+
 Stripe.setPublishableKey("pk_test_zCC2JrO3KSMeh7BB5x9OUe2U");
 
-const PaymentVM = can.DefineMap.extend({
-  amount: { default: 9.99 },
+Component.extend({
+	tag: 'cc-payment',
+	view: `
+		<form>
 
-  userCardNumber: "string",
-  get cardNumber() {
-    return this.userCardNumber ? this.userCardNumber.replace(/-/g, ""): null;
-  },
+			<input type="text" name="number" placeholder="Card Number"
+				value:bind="this.userCardNumber"/>
 
-  userExpiry: "string",
-  get expiryParts() {
-    if(this.userExpiry) {
-      return this.userExpiry.split("-").map(function(p){
-        return parseInt(p, 10);
-	  });
-    }
-  },
-  get expiryMonth() {
-    return this.expiryParts && this.expiryParts[0];
-  },
-  get expiryYear() {
-    return this.expiryParts && this.expiryParts[1];
-  },
+			<input type="text" name="expiry" placeholder="MM-YY"
+				value:bind="this.userExpiry"/>
 
-  userCVC: "string",
-  get cvc() {
-    return this.userCVC ?
-      parseInt(this.userCVC, 10) : null;
-  }
+			<input type="text" name="cvc" placeholder="CVC"
+				value:bind="this.userCVC"/>
+
+			<button>Pay $\{{ this.amount }}</button>
+
+			<p>{{ this.userCardNumber }}, {{ this.userExpiry }}, {{ this.userCVC }}</p>
+			<p>{{ this.cardNumber }}, {{ this.expiryMonth }}-{{ this.expiryYear }}, {{ this.cvc }}</p>
+			
+		</form>
+	`,
+	ViewModel: {
+		amount: {
+			default: 9.99
+		},
+
+		userCardNumber: "string",
+		get cardNumber() {
+			return this.userCardNumber ? this.userCardNumber.replace(/-/g, ""): null;
+		},
+
+		userExpiry: "string",
+		get expiryParts() {
+			if(this.userExpiry) {
+				return this.userExpiry.split("-").map(function(p){
+					return parseInt(p, 10);
+				});
+			}
+		},
+		get expiryMonth() {
+			return this.expiryParts && this.expiryParts[0];
+		},
+		get expiryYear() {
+			return this.expiryParts && this.expiryParts[1];
+		},
+
+		userCVC: "string",
+		get cvc() {
+			return this.userCVC ?
+			parseInt(this.userCVC, 10) : null;
+		}
+	}
 });
-
-const viewModel = new PaymentVM();
-
-const paymentView = can.stache.from("payment-view");
-const fragment = paymentView( viewModel );
-document.body.appendChild( fragment );
