7158933a55db0d4b7c20609d4ed821a161b752dc
Chasen Le Hara <git@chasenlehara.com>
2019-07-15 11:25:06 -0700

    Fix loading can-demo-models in Edge
    Edge was not resolving `unpkg.com/can-demo-models@5` to `unpkg.com/can-demo-models@5/index.mjs`, so this commit explicitly imports from that URL.
    
    Fixes https://github.com/canjs/canjs/issues/5125

--- docs/can-guides/experiment/crud-beginner/3.js
+++ docs/can-guides/experiment/crud-beginner/3.js
@@ -1,5 +1,5 @@
 // Creates a mock backend with 3 todos
-import { todoFixture } from "//unpkg.com/can-demo-models@5";
+import { todoFixture } from "//unpkg.com/can-demo-models@5/index.mjs";
 todoFixture(3);
 
 import { Component } from "//unpkg.com/can@5/core.mjs";
