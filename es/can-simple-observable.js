2b0ea7cb5415d99e97c905a1bd01784e37d3d10d
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-08-26 18:15:00 +0100

    Export all SimpleObservables

--- es/can-simple-observable.js
+++ es/can-simple-observable.js
@@ -1,1 +1,5 @@
 export {default} from "can-simple-observable";
+export {default as AsyncObservable} from "can-simple-observable/async/async";
+export {default as keyObservable} from "can-simple-observable/key/key";
+export {default as ResolverObservable} from "can-simple-observable/resolver/resolver";
+export {default as SettableObservable} from "can-simple-observable/settable/settable";
