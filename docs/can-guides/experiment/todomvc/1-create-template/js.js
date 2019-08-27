2ebf6955a56ec380af1be50025d01fb69e9352b8
Manuel Mujica <manuel.expat@gmail.com>
2019-08-27 15:27:52 -0400

    Upgrade TodoMVC guide to CanJS 6

--- docs/can-guides/experiment/todomvc/1-create-template/js.js
+++ docs/can-guides/experiment/todomvc/1-create-template/js.js
@@ -1,66 +1,66 @@
-import { Component } from "//unpkg.com/can@5/core.mjs";
+import { StacheElement } from "//unpkg.com/can@pre/core.mjs";
 
-Component.extend({
-  tag: "todo-mvc",
-  view: `
-      <section id="todoapp">
-        <header id="header">
-          <h1>{{ this.appName }}</h1>
-          <input id="new-todo"
-            placeholder="What needs to be done?"/>
-        </header>
-        <section id="main">
-          <input id="toggle-all" type="checkbox"/>
-          <label for="toggle-all">Mark all as complete</label>
-          <ul id="todo-list">
-            <li class="todo">
-              <div class="view">
-                  <input class="toggle" type="checkbox">
-                  <label>Do the dishes</label>
-                  <button class="destroy"></button>
-              </div>
-              <input class="edit" type="text" value="Do the dishes">
-            </li>
-            <li class="todo completed">
-              <div class="view">
-                  <input class="toggle" type="checkbox">
-                  <label>Mow the lawn</label>
-                  <button class="destroy"></button>
-              </div>
-              <input class="edit" type="text" value="Mow the lawn">
-            </li>
-            <li class="todo editing">
-              <div class="view">
-                  <input class="toggle" type="checkbox">
-                  <label>Pick up dry cleaning</label>
-                  <button class="destroy"></button>
-              </div>
-              <input class="edit" type="text" value="Pick up dry cleaning">
-            </li>
-          </ul>
-        </section>
-        <footer id="footer" class="">
-          <span id="todo-count">
-            <strong>2</strong> items left
-          </span>
-          <ul id="filters">
-            <li>
-              <a href="#!" class="selected">All</a>
-            </li>
-            <li>
-              <a href="#!active">Active</a>
-            </li>
-            <li>
-              <a href="#!complete">Completed</a>
-            </li>
-          </ul>
-          <button id="clear-completed">
-            Clear completed (1)
-          </button>
-        </footer>
+class TodoMVC extends StacheElement {
+  static view = `
+    <section id="todoapp">
+      <header id="header">
+        <h1>{{ this.appName }}</h1>
+        <input id="new-todo" placeholder="What needs to be done?">
+      </header>
+      <section id="main">
+        <input id="toggle-all" type="checkbox"/>
+        <label for="toggle-all">Mark all as complete</label>
+        <ul id="todo-list">
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox">
+              <label>Do the dishes</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" value="Do the dishes">
+          </li>
+          <li class="todo completed">
+            <div class="view">
+              <input class="toggle" type="checkbox">
+              <label>Mow the lawn</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" value="Mow the lawn">
+          </li>
+          <li class="todo editing">
+            <div class="view">
+              <input class="toggle" type="checkbox">
+              <label>Pick up dry cleaning</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" value="Pick up dry cleaning">
+          </li>
+        </ul>
       </section>
-  `,
-  ViewModel: {
-    appName: {default: "TodoMVC"}
-  }
-});
+      <footer id="footer" class="">
+        <span id="todo-count">
+          <strong>2</strong> items left
+        </span>
+        <ul id="filters">
+          <li>
+            <a href="#!" class="selected">All</a>
+          </li>
+          <li>
+            <a href="#!active">Active</a>
+          </li>
+          <li>
+            <a href="#!complete">Completed</a>
+          </li>
+        </ul>
+        <button id="clear-completed">
+          Clear completed (1)
+        </button>
+      </footer>
+    </section>
+  `;
+
+  static props = {
+    appName: { default: "TodoMVC" }
+  };
+}
+customElements.define("todo-mvc", TodoMVC);
--- docs/can-guides/experiment/todomvc/8-routing/js.js
+++ docs/can-guides/experiment/todomvc/8-routing/js.js
@@ -1,29 +1,47 @@
-import { Component, DefineMap, DefineList, fixture, realtimeRestModel, route,
-  domEvents, enterEvent } from "//unpkg.com/can@5/everything.mjs";
+import {
+  StacheElement,
+  ObservableArray,
+  ObservableObject,
+  fixture,
+  realtimeRestModel,
+  type,
+  domEvents,
+  enterEvent,
+  route
+} from "//unpkg.com/can@pre/everything.mjs";
 
 domEvents.addEvent(enterEvent);
 
-const Todo = DefineMap.extend("Todo",{
-  id: {type: "number", identity: true},
-  name: "string",
-  complete: { type: "boolean", default: false }
-});
+class Todo extends ObservableObject {
+  static props = {
+    id: { type: type.convert(Number), identity: true },
+    name: String,
+    complete: { type: type.convert(Boolean), default: false }
+  };
+}
 
-Todo.List = DefineList.extend({
-  "#": Todo,
-  get active() {
-    return this.filter({ complete: false });
-  },
-  get complete() {
-    return this.filter({ complete: true });
-  }
-});
+class TodoList extends ObservableArray {
+  static items = Todo;
 
-const todoStore = fixture.store([
-  { name: "mow lawn", complete: false, id: 5 },
-  { name: "dishes", complete: true, id: 6 },
-  { name: "learn canjs", complete: false, id: 7 }
-], Todo);
+  static props = {
+    get active() {
+      return this.filter({ complete: false });
+    },
+
+    get complete() {
+      return this.filter({ complete: true });
+    }
+  };
+}
+
+const todoStore = fixture.store(
+  [
+    { name: "mow lawn", complete: false, id: 5 },
+    { name: "dishes", complete: true, id: 6 },
+    { name: "learn canjs", complete: false, id: 7 }
+  ],
+  Todo
+);
 
 fixture("/api/todos", todoStore);
 fixture.delay = 200;
@@ -31,126 +49,148 @@ fixture.delay = 200;
 realtimeRestModel({
   url: "/api/todos",
   Map: Todo,
-  List: Todo.List
+  List: TodoList
 });
 
-Component.extend({
-  tag: "todo-create",
-  view: `
+class TodoCreate extends StacheElement {
+  static view = `
     <input id="new-todo"
       placeholder="What needs to be done?"
-      value:bind="todo.name"
-      on:enter="createTodo()"/>
-  `,
-  ViewModel: {
-    todo: { Default: Todo },
-    createTodo: function() {
-      this.todo.save().then(function(){
-        this.todo = new Todo();
-      }.bind(this));
-    }
-  }
-});
+      value:bind="this.todo.name"
+      on:enter="this.createTodo()"
+    >
+  `;
 
-Component.extend({
-  tag: "todo-list",
-  view: `
+  static props = {
+    todo: {
+      get default() {
+        return new Todo();
+      }
+    }
+  };
+
+  createTodo() {
+    this.todo.save().then(() => {
+      this.todo = new Todo();
+    });
+  }
+}
+customElements.define("todo-create", TodoCreate);
+
+class TodoListElement extends StacheElement {
+  static view = `
     <ul id="todo-list">
       {{# for(todo of this.todos) }}
         <li class="todo {{# if(todo.complete) }}completed{{/ if }}
           {{# if(todo.isDestroying()) }}destroying{{/ if }}
           {{# if(this.isEditing(todo)) }}editing{{/ if }}">
           <div class="view">
-            <input class="toggle" type="checkbox"
-               checked:bind="todo.complete" on:change="todo.save()">
+            <input 
+              class="toggle"
+              type="checkbox"
+              checked:bind="todo.complete"
+              on:change="todo.save()"
+            >
             <label on:dblclick="this.edit(todo)">{{ todo.name }}</label>
             <button class="destroy" on:click="todo.destroy()"></button>
           </div>
-          <input class="edit" type="text"
+          <input 
+            class="edit"
+            type="text"
             value:bind="todo.name"
             on:enter="this.updateName()"
             focused:from="this.isEditing(todo)"
-            on:blur="this.cancelEdit()"/>
+            on:blur="this.cancelEdit()"
+          >
         </li>
       {{/ for }}
     </ul>
-  `,
-  ViewModel: {
-    todos: Todo.List,
-    editing: Todo,
-    backupName: "string",
-    isEditing: function(todo) {
-      return todo === this.editing;
-    },
-    edit: function(todo) {
-      this.backupName = todo.name;
-      this.editing = todo;
-    },
-    cancelEdit: function() {
-      if(this.editing) {
-        this.editing.name = this.backupName;
-      }
-      this.editing = null;
-    },
-    updateName: function() {
-      this.editing.save();
-      this.editing = null;
-    }
-  }
-});
+  `;
 
-Component.extend({
-  tag: "todo-mvc",
-  view: `
-      <section id="todoapp">
-        <header id="header">
-          <h1>{{ this.appName }}</h1>
-          <todo-create/>
-        </header>
-        <section id="main" class="">
-          <input id="toggle-all" type="checkbox"/>
-          <label for="toggle-all">Mark all as complete</label>
-          <todo-list todos:from="this.todosPromise.value"/>
-        </section>
-        <footer id="footer" class="">
-          <span id="todo-count">
-            <strong>{{ this.todosPromise.value.active.length }}</strong> items left
-          </span>
-          <ul id="filters">
-            <li>
-              <a href="{{routeUrl(filter=undefined)}}"
-                {{#routeCurrent(filter=undefined)}}class="selected"{{/routeCurrent}}>All</a>
-            </li>
-            <li>
-              <a href="{{routeUrl(filter='active')}}"
-                {{#routeCurrent(filter='active')}}class="selected"{{/routeCurrent}}>Active</a>
-            </li>
-            <li>
-              <a href="{{routeUrl(filter='complete')}}"
-                {{#routeCurrent(filter='complete')}}class="selected"{{/routeCurrent}}>Completed</a>
-            </li>
-          </ul>
-          <button id="clear-completed">
-            Clear completed ({{ this.todosPromise.value.complete.length }})
-          </button>
-        </footer>
+  static props = {
+    todos: type.maybeConvert(TodoListModel),
+    editing: Todo,
+    backupName: String
+  };
+
+  isEditing(todo) {
+    return todo === this.editing;
+  }
+
+  edit(todo) {
+    this.backupName = todo.name;
+    this.editing = todo;
+  }
+
+  cancelEdit() {
+    if (this.editing) {
+      this.editing.name = this.backupName;
+    }
+    this.editing = null;
+  }
+
+  updateName() {
+    this.editing.save();
+    this.editing = null;
+  }
+}
+customElements.define("todo-list", TodoListElement);
+
+class TodoMVC extends StacheElement {
+  static view = `
+    <section id="todoapp">
+      <header id="header">
+        <h1>{{ this.appName }}</h1>
+        <todo-create />
+      </header>
+      <section id="main" class="">
+        <input id="toggle-all" type="checkbox">
+        <label for="toggle-all">Mark all as complete</label>
+        <todo-list todos:from="this.todosPromise.value" />
       </section>
-  `,
-  ViewModel: {
-    appName: {default: "TodoMVC"},
+      <footer id="footer" class="">
+        <span id="todo-count">
+          <strong>{{ this.todosPromise.value.active.length }}</strong> items left
+        </span>
+        <ul id="filters">
+          <li>
+            <a href="{{routeUrl(filter=undefined)}}"
+              {{#routeCurrent(filter=undefined)}}class="selected"{{/routeCurrent}}>All</a>
+          </li>
+          <li>
+            <a href="{{routeUrl(filter='active')}}"
+              {{#routeCurrent(filter='active')}}class="selected"{{/routeCurrent}}>Active</a>
+          </li>
+          <li>
+            <a href="{{routeUrl(filter='complete')}}"
+              {{#routeCurrent(filter='complete')}}class="selected"{{/routeCurrent}}>Completed</a>
+          </li>
+        </ul>
+        <button id="clear-completed">
+          Clear completed ({{ this.todosPromise.value.complete.length }})
+        </button>
+      </footer>
+    </section>
+  `;
+
+  static props = {
+    appName: { default: "TodoMVC" },
     routeData: {
-      default(){
+      get default() {
         route.register("{filter}");
         route.start();
         return route.data;
       }
     },
     get todosPromise() {
-      if(!this.routeData.filter) {
+      if (!this.routeData.filter) {
         return Todo.getList({});
       } else {
-        return Todo.getList({filter: { complete: this.routeData.filter === "complete" }});
+        return Todo.getList({
+          filter: { complete: this.routeData.filter === "complete" }
+        });
       }
     }
-  }
-});
+  };
+}
+customElements.define("todo-mvc", TodoMVC);
