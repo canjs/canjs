a5314ac54f6a7c29526b4bcca84cbeaa45c28de5
Manuel Mujica <manuel.expat@gmail.com>
2019-09-19 10:57:45 -0300

    Upgrade video player recipe to Can 6

--- docs/can-guides/commitment/recipes/video-player/6-play-mutates.js
+++ docs/can-guides/commitment/recipes/video-player/6-play-mutates.js
@@ -1,77 +1,87 @@
-import {Component} from "//unpkg.com/can@5/core.mjs";
+import { fromAttribute, StacheElement } from "//unpkg.com/can@pre/core.mjs";
 
-Component.extend({
-  tag: "video-player",
-  view: `
-    <video
-      on:play="play()"
-      on:pause="pause()"
-      on:timeupdate="updateTimes(scope.element)"
-      on:loadedmetadata="updateTimes(scope.element)">
-      <source src="{{ src }}"/>
-    </video>
-    <div>
-      <button on:click="togglePlay()">
-        {{# if(playing) }} Pause {{ else }} Play {{/ if }}
-      </button>
-      <input type="range" value="0" max="1" step="any"
-             value:bind="percentComplete"/>
-      <span>{{ formatTime(currentTime) }}</span> /
-      <span>{{ formatTime(duration) }} </span>
-    </div>
-  `,
-  ViewModel: {
-    src: "string",
-    playing: "boolean",
-    duration: "number",
-    currentTime: "number",
+class VideoPlayer extends StacheElement {
+	static view = `
+		<video
+			on:play="this.play()"
+			on:pause="this.pause()"
+			on:timeupdate="this.updateTimes(scope.element)"
+			on:loadedmetadata="this.updateTimes(scope.element)"
+		>
+			<source src="{{ this.src }}" >
+		</video>
+		<div>
+			<button on:click="this.togglePlay()">
+				{{# if(this.isPlaying) }} Pause {{ else }} Play {{/ if }}
+			</button>
+			<input type="range" value="0" max="1" step="any" value:bind="this.percentComplete">
+			<span>{{ this.formatTime(this.currentTime) }}</span> /
+			<span>{{ this.formatTime(this.duration) }} </span>
+		</div>
+	`;
 
-    get percentComplete() {
-      return this.currentTime / this.duration;
-    },
-    set percentComplete(newVal) {
-      this.currentTime = newVal * this.duration;
-    },
+	static props = {
+		currentTime: Number,
+		duration: Number,
+		isPlaying: Boolean,
+		src: {
+			bind: fromAttribute,
+			type: String
+		},
 
-    updateTimes(videoElement) {
-      this.currentTime = videoElement.currentTime || 0;
-      this.duration = videoElement.duration;
-    },
-    formatTime(time) {
-      if (time === null || time === undefined) {
-        return "--";
-      }
-      const minutes = Math.floor(time / 60);
-      let seconds = Math.floor(time - minutes * 60);
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-      return minutes + ":" + seconds;
-    },
-    play() {
-      this.playing = true;
-    },
-    pause() {
-      this.playing = false;
-    },
-    togglePlay() {
-      this.playing = !this.playing;
-    },
+		get percentComplete() {
+			return this.currentTime / this.duration;
+		},
 
-    connectedCallback(element) {
-      this.listenTo("playing", function(event, isPlaying) {
-        if (isPlaying) {
-          element.querySelector("video").play();
-        } else {
-          element.querySelector("video").pause();
-        }
-      });
-      this.listenTo("currentTime", function(event, currentTime) {
-        const videoElement = element.querySelector("video");
-        if (currentTime !== videoElement.currentTime) {
-          videoElement.currentTime = currentTime;
-        }
-      });
-    }
-  }
-});
+		set percentComplete(newVal) {
+			this.currentTime = newVal * this.duration;
+		}
+	};
+
+	connected() {
+		this.listenTo("currentTime", ({ value }) => {
+			const videoElement = this.querySelector("video");
+			if (value !== videoElement.currentTime) {
+				videoElement.currentTime = value;
+			}
+		});
+		this.listenTo("isPlaying", ({ value }) => {
+			if (value) {
+				this.querySelector("video").play();
+			} else {
+				this.querySelector("video").pause();
+			}
+		});
+	}
+
+	formatTime(time) {
+		if (time === null || time === undefined) {
+			return "--";
+		}
+		const minutes = Math.floor(time / 60);
+		let seconds = Math.floor(time - minutes * 60);
+		if (seconds < 10) {
+			seconds = "0" + seconds;
+		}
+		return minutes + ":" + seconds;
+	}
+
+	play() {
+		this.isPlaying = true;
+	}
+
+	pause() {
+		this.isPlaying = false;
+	}
+
+	togglePlay() {
+		this.isPlaying = !this.isPlaying;
+	}
+
+	updateTimes(videoElement) {
+		this.currentTime = videoElement.currentTime || 0;
+		this.duration = videoElement.duration;
+	}
+}
+
+customElements.define("video-player", VideoPlayer);
