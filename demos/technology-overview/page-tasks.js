92b6d42bfddac787050c4de9fb7cc94c528a6977
Manuel Mujica <manuel.expat@gmail.com>
2019-09-20 14:57:22 -0300

    Upgrade routing guide to Can 6

--- demos/technology-overview/page-tasks.js
+++ demos/technology-overview/page-tasks.js
@@ -1,58 +1,68 @@
-import { Component } from "can";
+import { StacheElement, type } from "can";
 import "./percent-slider";
 
-const TaskEditor = Component.extend({
-    tag: "task-editor",
-    view: `
-        {{# if logout }}
-            <h1><code>&lt;task-editor&gt;</code></h1>
-            <p>
-                This content is provided by the <code>&lt;task-editor&gt;</code> component.
-                Click <a href="{{ routeUrl(page='home') }}">Home</a> to return to the homepage, or
-                <button on:click="logout()">Logout</button> to log out. Edit the task below:
+class TaskEditor extends StacheElement {
+	static get view() {
+		return `
+			{{# if(this.logout) }}
+				<h1><code>&lt;task-editor&gt;</code></h1>
+				<p>
+					This content is provided by the <code>&lt;task-editor&gt;</code> component.
+					Click <a href="{{ routeUrl(page='home') }}">Home</a> to return to the homepage, or
+					<button on:click="this.logout()">Logout</button> to log out. Edit the task below:
+				</p>
+			{{ else }}
+				<h2>Task Editor</h2>
+			{{/ if }}
 
-            </p>
-        {{/ else }}
-            <h2>Task Editor</h2>
-        {{/ if }}
-        <form on:submit="save(scope.event)">
-            Name: {{ name }}
-            <p>
-                <input value:bind="name"/>
-            </p>
-            Progress: {{ progress }}
-            <p>
-                <percent-slider value:bind="progress"/>
-            </p>
-            <button disabled:from="isSaving">
-                {{# if(isSaving) }}Saving{{ else }}Save{{/ if }}
-            </button>
-        </form>
-    `,
-    ViewModel: {
-        id: "number",
-        name: {
-            default: function(){
-                return "Task " + this.id;
-            }
-        },
-        progress: {
-            // makes progress an integer
-            type(num){
-                return parseInt(num);
-            },
-            default: 0
-        },
-        isSaving: { default: false },
-        save(event){
-            event.preventDefault();
-            this.isSaving = true;
-            // fake ajax request
-            setTimeout(() => {
-                this.isSaving = false;
-            },2000);
-        }
-    }
-});
+			<form on:submit="this.save(scope.event)">
+				Name: {{ this.name }}
+				<p>
+					<input value:bind="this.name">
+				</p>
+				Progress: {{ this.progress }}
+				<p>
+					<percent-slider value:bind="this.progress" />
+				</p>
+				<button disabled:from="this.isSaving">
+					{{# if(this.isSaving) }}Saving{{ else }}Save{{/ if }}
+				</button>
+			</form>
+		`;
+	}
+
+	static get props() {
+		return {
+			id: type.convert(Number),
+
+			name: {
+				get default() {
+					return "Task " + this.id;
+				}
+			},
+
+			progress: {
+				// makes progress an integer
+				type(num) {
+					return parseInt(num, 10);
+				},
+				default: 0
+			},
+
+			isSaving: { default: false }
+		};
+	}
+
+	save(event) {
+		event.preventDefault();
+		this.isSaving = true;
+		// fake ajax request
+		setTimeout(() => {
+			this.isSaving = false;
+		}, 2000);
+	}
+}
+
+customElements.define("task-editor", TaskEditor);
 
 export default TaskEditor;
