2a67179d8d08be55c17431dee353106fb3e5a5b3
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2020-11-04 20:22:18 +0100

    Update ios version

--- test/test-sauce-labs.js
+++ test/test-sauce-labs.js
@@ -60,7 +60,7 @@ var platforms = [{
 	browserName: 'Safari',
 	'appium-version': '1.15.1',
 	platformName: 'iOS',
-	platformVersion: '11.0',
+	platformVersion: '13.0',
 	deviceName: 'iPhone 8 Simulator',
 	maxDuration: maxDuration,
 	commandTimeout: commandTimeout,
